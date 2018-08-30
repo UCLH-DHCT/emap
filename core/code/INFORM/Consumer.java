7aafbe7792087f085d633b8bfd3c1ef7eb9349f4
Matthew Gillman <m.gillman@ucl.ac.uk>
2018-08-30 11:21:33 +0100

    Adding some parsing of generic messages (need to move this to Engine class)

--- core/code/INFORM/Consumer.java
+++ core/code/INFORM/Consumer.java
@@ -14,11 +14,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-import com.sun.org.apache.xerces.internal.impl.dv.DatatypeException;
+//import com.sun.org.apache.xerces.internal.impl.dv.DatatypeException;
 
 import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.util.Hl7InputStreamMessageIterator;
-import ca.uhn.hl7v2.util.Hl7InputStreamMessageStringIterator;
+//import ca.uhn.hl7v2.util.Hl7InputStreamMessageStringIterator;
 import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.parser.Parser;
@@ -27,6 +27,9 @@ import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.DataTypeException;
 import ca.uhn.hl7v2.validation.ValidationException;
 import ca.uhn.hl7v2.validation.impl.ValidationContextFactory;
+import ca.uhn.hl7v2.model.v22.message.ADT_A01;
+import ca.uhn.hl7v2.model.v25.message.ADT_A02;
+//import ca.uhn.hl7v2.model.v25.message.ADT_AXX;
 
 public class Consumer {
 
@@ -50,15 +53,15 @@ public class Consumer {
             //Hl7InputStreamMessageStringIterator iter2 = new Hl7InputStreamMessageStringIterator(is); 
             Hl7InputStreamMessageIterator iter2 = new Hl7InputStreamMessageIterator(is, context); 
             iter2.setIgnoreComments(true);
-            
+            int count = 0;
             while (iter2.hasNext()) {
-                
+                count++;
                 //String next = iter2.next();
-                
+                System.out.println("*************** Next (" + count + ")*****************");
                 // Do something with the message. The iterator strips off the MSH and EVN segments.
                 //System.out.println("\nNext message is:******************\n" + next);
-                Message hapiMsg = iter2.next();
-                String ver = hapiMsg.getVersion();
+                Message msg/*hapiMsg*/ = iter2.next();
+                String ver = msg/*hapiMsg*/.getVersion();
                 System.out.println("version is " + ver);
                 /*try {
                     // The parse method performs the actual parsing
@@ -71,7 +74,21 @@ public class Consumer {
                     return;
                 } */
                 
-                System.exit(1);
+                ////String msgType = hapiMsg.getMessageType().getMessageType().getValue();
+                ////String msgTrigger = hapiMsg.getMessageType().getTriggerEvent().getValue();
+
+                // Prints
+                ///System.out.println("\n\nSpecific: " + msgType + " " + msgTrigger);
+                
+                // From https://hapifhir.github.io/hapi-hl7v2/xref/ca/uhn/hl7v2/examples/ExampleSuperStructures.html
+                if (msg instanceof ADT_A01) {
+                    System.out.println("ADT_A01");//processAdtA01((ADT_A01) msg);
+                } else if (msg instanceof ADT_A02) {
+                    //processAdtA02((ADT_A02) msg);
+                            }
+                else System.out.println("Another message type"); 
+
+                ////System.exit(1);
 
             }
 	    }
@@ -111,21 +128,21 @@ public class Consumer {
         // Not very elegant.
         // Also want a utility to tell us last unid processed (maybe stored as json)
         // which we may (or may not) take notice of.
-        if (arglen > 4) {
+        if (arglen >= 4) {
             if (args[0].equals("-d") && args[2].equals("-i")) {
                 dbconn = args[1];
                 last_unid = Long.parseLong(args[3]);
-                // read records from db
+                // read records from db starting with UNID i
             }
             else if (args[0].equals("-i") && args[2].equals("-d")) {
                 dbconn = args[3];
                 last_unid = Long.parseLong(args[1]);
-                // read records from db
+                // read records from db starting with UNID i
             }
             else HelpMessage();
 
         }
-        else if (arglen > 2) {
+        else if (arglen >= 2) {
             if (args[0].equals("-f")) {
                filename = args[1];
                process_file(filename);
