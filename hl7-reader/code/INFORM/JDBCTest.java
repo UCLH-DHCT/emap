a02262c80a5d84eb6e053195fa2cbf70b9114241
Matthew Gillman <m.gillman@ucl.ac.uk>
2018-10-02 16:53:05 +0100

    Successfully queries dummy IDS and inserts data into dummy UDS, although need to do more on timestamps. NB this works for a single record - need to change to deal with multiple records.

--- hl7-reader/code/INFORM/JDBCTest.java
+++ hl7-reader/code/INFORM/JDBCTest.java
@@ -59,29 +59,90 @@ public class JDBCTest {
 		// Need to add patient id list
 		//////////////////////////////////////////
 		String ids_url = "jdbc:postgresql://localhost/DUMMY_IDS";
+		String uds_url = "jdbc:postgresql://localhost/INFORM_SCRATCH";
+
 		StringBuilder query = new StringBuilder("SELECT PatientName, PatientMiddleName, PatientSurname, ");
 		query.append("DateOfBirth, HospitalNumber, PatientClass, PatientLocation, AdmissionDate, DischargeDate,");
 		query.append("MessageType, MessageVersion ");
 		query.append(" FROM TBL_IDS_MASTER;");
+
+		StringBuilder patient_name = new StringBuilder("J. Doe"); // NB StringBuilder is not thread-safe.
+		String dob = "unknown", hospital_number = "";
+		char patient_class;
+		String location, admit_date, discharge_date, msg_type, msg_version;
+
+		// Extraction of data from IDS step. No HL7 parsing required.
 		try {
 			conn = DriverManager.getConnection(ids_url);
 			st = conn.createStatement();
 			rs = st.executeQuery(query.toString());
 			while (rs.next())
 			{
-				StringBuilder patient_name = new StringBuilder(rs.getString(1));
+				patient_name = new StringBuilder(rs.getString(1));
 				patient_name.append(" ").append(rs.getString(2));
 				patient_name.append(" ").append(rs.getString(3));
 				System.out.println(patient_name.toString());
+
+				dob = new String(rs.getString(4));
+				hospital_number = new String(rs.getString(5));
+				patient_class = (new String(rs.getString(6))).charAt(0);
+				location = new String(rs.getString(7));
+				admit_date = new String(rs.getString(8));
+				discharge_date = new String(rs.getString(9));
+				msg_type = new String(rs.getString(10));
+				msg_version = new String(rs.getString(11));
 			}
 			rs.close();
+			st.close();	
+			conn.close();
+			query.delete(0, query.length()); // Clear query StringBuilder.
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		// Now we write the data to the UDS.
+		query.append("INSERT INTO PERSON (");
+		query.append("set_ID, patient_ID_list, patient_name, birth_date_time, sex, patient_address, ");
+		query.append("patient_death_date_time, patient_death_indicator, identity_unknown_indicator, last_update_date_time");
+
+		// NB May need to parse timestamps to get into correct format for Postgres.
+		query.append(") VALUES (");
+		query.append("NULL,"); // set_ID
+		query.append(hospital_number).append(", ");
+		query.append("'").append(patient_name).append("', ");	
+		query.append("'").append(/*dob*/ "20090304").append("', "); // example: 2018-10-01 14:57:41.090449
+		query.append("'").append("U").append("', "); // sex unknown without parsing HL7
+		query.append("'").append("Address Unknown").append("', "); // address unknown without parsing HL7
+		query.append("NULL").append(", "); // patient death time unknown without parsing HL7 (PID-29)
+		query.append("''").append(", "); // patient death indicator unknown without parsing HL7 (PID-30)
+		query.append("''").append(", "); // identity unknown unknown without parsing HL7 (PID-31)
+		query.append("NULL")/*.append(", ")*/; // last update date/time unknown without parsing HL7 (PID-33)
+
+		query.append(");");
+
+		System.out.println("******* insert statement: *****");
+		System.out.println(query.toString());
+
+
+		try {
+			conn = DriverManager.getConnection(uds_url);
+			st = conn.createStatement();
+			int ret = st.executeUpdate(query.toString());
+			String res = new String("return value was " + ret);
+			System.out.println(res);
+			//rs = st.executeQuery(query.toString());
+			/*while (rs.next())
+			{
+		
+			}*/
+			//rs.close();
 			st.close();	
 			conn.close();
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
 	}	
 
 }
