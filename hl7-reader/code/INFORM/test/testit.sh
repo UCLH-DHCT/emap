aa90e8e94e0560860b1be1fca28027051103ca35
Matthew Gillman <m.gillman@ucl.ac.uk>
2019-01-10 14:01:24 +0000

    Moved tests to their own subdir.

---
+++ hl7-reader/code/INFORM/test/testit.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# *******************************************
+# * This batch file runs example code
+# *
+# * For more info, see: http://hl7api.sourceforge.net/devbyexample.html
+# ******************************************* 
+
+CP="."
+for i in ../../../hapi-dist-2/lib/*.jar;
+do
+		  CP=$i:$CP;
+done
+
+#CP=.:postgresql-42.2.5.jar:json-simple-1.1.1.jar:$CP
+CP=.:../postgresql-42.2.5.jar:../json-simple-1.1.1.jar:$CP
+
+CP=junit-4.10.jar:$CP
+#CP=junit-4.13-beta-1.jar:hamcrest-core-2.1.jar:$CP
+
+echo "Removing old class files"
+#rm ../src/uk/ac/ucl/rits/inform/*.class #HL7Processor.class
+rm *.class
+
+javac -classpath $CP ../src/uk/ac/ucl/rits/inform/Engine.java ../src/uk/ac/ucl/rits/inform/HL7Processor.java TestConvertTimestamp.java TestNull.java JunitTestSuite.java TestRunner.java
+
+java -cp "../src:$CP" TestRunner
