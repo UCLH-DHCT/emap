f132278d245d253fc78f53b73242a1093e428410
Matthew Gillman <m.gillman@ucl.ac.uk>
2019-01-15 11:12:43 +0000

    Refactoring into check_json_value(), with associated unit testing.

--- hl7-reader/code/INFORM/test/TestJson.java
+++ hl7-reader/code/INFORM/test/TestJson.java
@@ -19,6 +19,8 @@ import static org.mockito.Mockito.*;
 	Test class for HL7Processor::interrogate_json_object()
 */	
 
+@SuppressWarnings("unchecked") // see https://stackoverflow.com/questions/53225896/jsonobject-unchecked-call-to-putk-v-as-a-member-of-the-raw-type-java-util-hash
+
 // parameterised tests https://www.tutorialspoint.com/junit/junit_parameterized_test.htm
 //@RunWith(Parameterized.class) // <- doesn't work with our version of junit (4.10)
 public class TestJson extends TestCase {
@@ -28,7 +30,7 @@ public class TestJson extends TestCase {
     @Override
     public void setUp() throws Exception {
         //System.out.println("Setting it up!");
-	mockproc = mock(HL7Processor.class);
+	    mockproc = mock(HL7Processor.class);
     }
 
 
@@ -36,22 +38,58 @@ public class TestJson extends TestCase {
     // Normal operation
     public void testijo1() {
         
-	JSONObject jo = new JSONObject();
-	jo.put("udshost", "192.168.1.1");
-	jo.put("idshost", "192.168.1.1");
-	jo.put("idsusername", "idstestuser");
-	jo.put("udsusername", "udstestuser");
-	jo.put ("idspassword", "password");
-	jo.put("udspassword", "letmein");
-	jo.put("debugging", "true");
+        JSONObject jo = new JSONObject();
+        jo.put("udshost", "192.168.1.1");
+        jo.put("idshost", "192.168.1.1");
+        jo.put("idsusername", "idstestuser");
+        jo.put("udsusername", "udstestuser");
+        jo.put ("idspassword", "password");
+        jo.put("udspassword", "letmein");
+        jo.put("debugging", "true");
 
-	mockproc.interrogate_json_object(jo);	
-
+	    mockproc.interrogate_json_object(jo);	
 
     }
 
+    @Test 
+    // Empty JSONObject
+    // check_json_value (JSONObject jo, String varname, String key)
+    public void testcjv1() {
+        JSONObject jo = new JSONObject();
+        Object[] objarray = mockproc.check_json_value(jo, "udshost", "udshost");
+        boolean b = ((Boolean)objarray[0]).booleanValue();
+        assertFalse(b);
+    }
 
+    @Test
+    // Normal operation
+    public void testcjv2() {
+        JSONObject jo = new JSONObject();
+        jo.put("udshost", "192.168.1.1");
+        Object[] objarray = mockproc.check_json_value(jo, "udshost", "udshost");
+        boolean b = ((Boolean)objarray[0]).booleanValue();
+        assertTrue(b);
+    }
 
+    @Test
+    // Put one key/value into JSONObject and ask for another
+    public void testcjv3() {
+        JSONObject jo = new JSONObject();
+        jo.put("udshost", "192.168.1.1");
+        Object[] objarray = mockproc.(jo, "idsusername", "idsusername");
+        boolean b = ((Boolean)objarray[0]).booleanValue();
+        assertFalse(b);
+    }
+
+    @Test
+    // Empty string query
+    public void testcjv4() {
+        JSONObject jo = new JSONObject();
+        jo.put("udshost", "192.168.1.1");
+        Object[] objarray = mockproc.check_json_value(jo, "", "");
+        boolean b = ((Boolean)objarray[0]).booleanValue();
+        assertFalse(b);
+    }
 
     @Override
     protected void tearDown() throws Exception {
