9bb4827b0470f8c41a7b3b927025e624f1d6b44a
Jeremy Stein <j.stein@ucl.ac.uk>
2019-01-25 16:56:46 +0000

    Basic outline of a JPA class+repository structure.

---
+++ emap-star/Person.java
@@ -0,0 +1,43 @@
+package uk.ac.ucl.rits.inform.informdb;
+
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    public Person(int person_id, Timestamp create_datetime) {
+        super();
+        this.person_id = person_id;
+        this.create_datetime = create_datetime;
+    }
+
+    @Id
+    private int person_id;
+    
+    private Timestamp create_datetime;
+
+    public int getPerson_id() {
+        return person_id;
+    }
+
+    public void setPerson_id(int person_id) {
+        this.person_id = person_id;
+    }
+
+    public Timestamp getCreate_datetime() {
+        return create_datetime;
+    }
+
+    public void setCreate_datetime(Timestamp create_datetime) {
+        this.create_datetime = create_datetime;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [person_id=" + person_id + ", create_datetime=" + create_datetime + "]";
+    }
+}
