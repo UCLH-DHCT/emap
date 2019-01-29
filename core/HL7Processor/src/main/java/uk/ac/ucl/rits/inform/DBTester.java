24f683518d9a95efb94af19a33af3b6e22448206
Jeremy Stein <j.stein@ucl.ac.uk>
2019-01-29 16:52:10 +0000

    Add new MRNs to an existing person

--- core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/DBTester.java
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/DBTester.java
@@ -9,32 +9,42 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import uk.ac.ucl.rits.inform.informdb.Person;
-import uk.ac.ucl.rits.inform.informdb.PersonRepository;
+import uk.ac.ucl.rits.inform.informdb.*;
 
 @Component
 public class DBTester {
     @Autowired
-    private PersonRepository repo;
+    private PersonRepository personRepo;
+    @Autowired
+    private MrnRepository mrnRepo;
     private final static Logger logger = LoggerFactory.getLogger(DBTester.class);
 
     public DBTester() {
     }
 
     public String savePerson() {
-        Person psav = repo.save(new Person(42, Timestamp.from(Instant.now())));
+        Person psav = personRepo.save(new Person(42, Timestamp.from(Instant.now())));
         return String.format("Hello world! %s", psav.toString());
     }
 
+    public Mrn findOrAddMrn(Person person) {
+        Mrn newMrn = new Mrn();
+        newMrn.setStore_datetime(Timestamp.from(Instant.now()));
+        newMrn.setPerson(person);
+        newMrn = mrnRepo.save(newMrn);
+        System.out.println(newMrn.toString());
+        return newMrn;
+    }
+    
     public Person findOrAddPerson() {
-        Optional<Person> pers = repo.findById(42);
+        Optional<Person> pers = personRepo.findById(42);
         if (pers.isPresent()) {
             Person pgot = pers.get();
             System.out.println(pgot.toString());
             return pgot;
 
         } else {
-            Person pnew = repo.save(new Person(42, Timestamp.from(Instant.now())));
+            Person pnew = personRepo.save(new Person(42, Timestamp.from(Instant.now())));
             System.out.println(pnew.toString());
             return pnew;
         }
