e989a371238142499b32f2ccdf93c38a8a6a72f5
Jeremy Stein <j.stein@ucl.ac.uk>
2019-02-04 17:54:06 +0000

    Store a patient fact using the fact/property tables. Set the foreign key column names properly.

---
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/informdb/PatientDemographicPropertyRepository.java
@@ -0,0 +1,7 @@
+package uk.ac.ucl.rits.inform.informdb;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PatientDemographicPropertyRepository extends CrudRepository<PatientDemographicProperty, Integer> {
+
+}
