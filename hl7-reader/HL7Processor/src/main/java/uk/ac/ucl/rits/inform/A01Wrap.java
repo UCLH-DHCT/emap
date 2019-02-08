2faa69a9c4211fb0e33684e4665ed55f7fa315d3
Jeremy Stein <j.stein@ucl.ac.uk>
2019-02-08 18:14:45 +0000

    First attempt at integrating some of Matthew's HL7 code.

--- hl7-reader/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/A01Wrap.java
+++ hl7-reader/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/A01Wrap.java
@@ -1,5 +1,6 @@
 package uk.ac.ucl.rits.inform;
 
+import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.Random;
@@ -7,8 +8,19 @@ import java.util.Random;
 import org.apache.commons.lang.NotImplementedException;
 import org.apache.commons.lang.RandomStringUtils;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.v27.datatype.CX;
+import ca.uhn.hl7v2.model.v27.datatype.ST;
 import ca.uhn.hl7v2.model.v27.message.ADT_A01;
+import ca.uhn.hl7v2.model.v27.segment.MSH;
 import ca.uhn.hl7v2.model.v27.segment.PV1;
+import ca.uhn.hl7v2.parser.CanonicalModelClassFactory;
+import ca.uhn.hl7v2.parser.PipeParser;
+import ca.uhn.hl7v2.validation.ValidationContext;
+import ca.uhn.hl7v2.validation.impl.ValidationContextFactory;
 
 public class A01Wrap {
 
@@ -50,11 +62,36 @@ public class A01Wrap {
      * Populate the data from an HL7 message.
      * 
      * @param fromMsg the passed in HL7 message
+     * @throws HL7Exception
      */
-    public A01Wrap(ADT_A01 fromMsg) {
-        PV1 pv1 = fromMsg.getPV1();
-        pv1.getVisitNumber();
-        throw new NotImplementedException();
+    public A01Wrap(ADT_A01 adt_01) throws HL7Exception {
+
+        // 1. MSH (Message Header) - mostly don't appear to be useful
+        MSH msh = adt_01.getMSH();
+        System.out.println("\n************** MSH segment **************************");
+        // MSH-1 Field Separator
+        // MSH-2 Encoding Characters
+        System.out.println("sending application = " + msh.getSendingApplication().getComponent(0).toString());// MSH-3
+                                                                                                              // Sending
+                                                                                                              // Application
+                                                                                                              // (“CARECAST”)
+        System.out.println("sending facility = " + msh.getSendingFacility().getComponent(0).toString()); // MSH-4
+                                                                                                         // Sending
+                                                                                                         // Facility
+                                                                                                         // (“UCLH”)
+        // MSH-5 Receiving Application (“Receiving system”)
+        System.out.println("messageTimestamp = " + msh.getDateTimeOfMessage().toString()); // MSH-7 Date/Time Of
+                                                                                           // Message
+                                                                                           // YYYYMMDDHHMM
+        System.out.println("message type = " + msh.getMessageType().getMessageCode().toString()); // MSH-9.1 Message
+                                                                                                  // Type (ADT)
+        System.out.println("trigger event = " + msh.getMessageType().getTriggerEvent().getValue()); // MSH-9.2
+                                                                                                    // Trigger
+        PV1 pv1 = adt_01.getPV1();
+        CX visitNumber2 = pv1.getVisitNumber();
+        ST idNumber = visitNumber2.getIDNumber();
+        idNumber.getValue();
+
     }
 
     public String getAdministrativeSex() {
--- hl7-reader/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/App.java
+++ hl7-reader/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/App.java
@@ -1,11 +1,27 @@
 package uk.ac.ucl.rits.inform;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.v27.message.ADT_A01;
+import ca.uhn.hl7v2.parser.CanonicalModelClassFactory;
+import ca.uhn.hl7v2.parser.PipeParser;
+import ca.uhn.hl7v2.util.Hl7InputStreamMessageIterator;
+import ca.uhn.hl7v2.util.Hl7InputStreamMessageStringIterator.ParseFailureError;
+import ca.uhn.hl7v2.validation.ValidationContext;
+import ca.uhn.hl7v2.validation.impl.ValidationContextFactory;
 import uk.ac.ucl.rits.inform.informdb.Encounter;
 
 @SpringBootApplication
@@ -19,18 +35,56 @@ public class App {
     public CommandLineRunner mainLoop(DBTester dbt) {
         return (args) -> {
             System.out.println("hi");
+
             long startTimeMillis = System.currentTimeMillis();
-            for (int i = 0; i < 5e4; i++) {
-                Encounter enc = dbt.addEncounter(new A01Wrap());
-                if (i % 100 == 0) {
-                    System.out.println("[" + i + "]" + enc.toString());
+            System.out.println("hello there");
+            HapiContext context = new DefaultHapiContext();
+
+            ValidationContext vc = ValidationContextFactory.noValidation();
+            context.setValidationContext(vc);
+
+            // https://hapifhir.github.io/hapi-hl7v2/xref/ca/uhn/hl7v2/examples/HandlingMultipleVersions.html
+            CanonicalModelClassFactory mcf = new CanonicalModelClassFactory("2.7");
+            context.setModelClassFactory(mcf);
+            PipeParser parser = context.getPipeParser(); // getGenericParser();
+
+            File file = null;
+            try {
+                file = new File(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println(file.getAbsolutePath() + " " + file.exists());
+            InputStream is = new BufferedInputStream(new FileInputStream(file));
+            Hl7InputStreamMessageIterator hl7iter = new Hl7InputStreamMessageIterator(is, context);
+            hl7iter.setIgnoreComments(true);
+            int count = 0;
+            while (hl7iter.hasNext()) {
+                count++;
+                System.out.println("*************** Next (" + count + ")*****************");
+                // Do something with the message. The iterator strips off the MSH and EVN
+                // segments.
+                Message msg = hl7iter.next();
+
+                System.out.println("Engine: version is " + msg.getVersion());
+
+                if (msg instanceof ADT_A01) {
+                    System.out.println("Got an ADT_A01");
+                    // AO1 message type can be used to represent other message types
+
+                    ADT_A01 adt_01 = (ADT_A01) parser.parse(msg.encode());
+                    Encounter enc = dbt.addEncounter(new A01Wrap(adt_01));
+                    System.out.println("Added: " + enc.toString());
+                } else {
+                    System.out.println("Other message type: " + msg.getClass().toString());
                 }
             }
-            long endcurrentTimeMillis = System.currentTimeMillis();
-            System.out.println(String.format("done, took %.0f secs", (endcurrentTimeMillis - startTimeMillis)/1000.0));
+            long endCurrentTimeMillis = System.currentTimeMillis();
+            System.out.println(String.format("done, %.0f secs", (endCurrentTimeMillis - startTimeMillis) / 1000.0));
+            context.close();
         };
     }
-    
+
     /**
      * Don't want to do any normal HL7 message processing if running test profile
      */
