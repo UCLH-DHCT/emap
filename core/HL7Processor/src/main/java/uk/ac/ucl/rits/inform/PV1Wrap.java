925b1955a3bbdc6d979cd1782fde2ef1a3381451
Matthew Gillman <m.gillman@ucl.ac.uk>
2019-02-26 16:02:47 +0000

    Added TestPV1.java, and getReferringDoctors() method to PV1Wrap.java

--- core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/PV1Wrap.java
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/PV1Wrap.java
@@ -131,6 +131,32 @@ import java.util.Vector;
     }
 
 
+    /**
+     * Get Referring Doctor(s) PV1-8.1 to PV1-8.6
+     * 
+     * I am not sure if Carecast uses PV1-8.7. 
+     * 
+     * @return Vector of Doctor objects
+     * @throws HL7Exception
+     */
+    public Vector<Doctor> getReferringDoctors() throws HL7Exception {
+
+        int reps = _pv1.getReferringDoctorReps();
+        Vector v = new Vector(reps, 1);
+        for (int i = 0; i < reps; i++) {
+            Doctor dr = new Doctor();
+            dr.setConsultantCode(_pv1.getReferringDoctor(i).getPersonIdentifier().toString()); // PV1-8.1
+            dr.setSurname(_pv1.getReferringDoctor(i).getFamilyName().getSurname().toString()); // PV1-8.2
+            dr.setFirstname(_pv1.getReferringDoctor(i).getGivenName().toString()); // PV1-8.3
+            dr.setMiddlenameOrInitial(_pv1.getReferringDoctor(i).getSecondAndFurtherGivenNamesOrInitialsThereof().toString()); // PV1-8.4
+            dr.setTitle(_pv1.getReferringDoctor(i).getPrefixEgDR().toString()); // PV1-8.6
+            //dr.setLocalCode(_pv1.); // PV1-8.7
+
+            v.add(dr);
+        }
+
+        return v;
+    }
 
     /**
      * 
@@ -192,36 +218,4 @@ import java.util.Vector;
 
  }
  
- /*
- int reps = pv1.getAttendingDoctorReps();
- System.out.println("THERE ARE " + reps + " ATTENDING DOCTOR(S):");
- for (int i = 0; i < reps; i++) {
-     // PV1-7.1 (1st repeat) Consultant code. Consultant for the clinic. Attending doctor GMC Code
-     System.out.println("\t(" + i + ") PV1-7.1 consultant code = " + pv1.getAttendingDoctor(i).getPersonIdentifier().toString());
-     // PV1-7.2 (1st repeat) Consultant surname Eg. CASSONI
-     System.out.println("\t(" + i + ") PV1-7.2 consultant surname = " + pv1.getAttendingDoctor(i).getFamilyName().getSurname().toString());
-     // PV1-7.3 (1st repeat) Consultant Fname Eg. M
-     System.out.println("\t(" + i + ") PV1-7.3 consultant firstname = " + pv1.getAttendingDoctor(i).getGivenName().toString());
-     // PV1-7.4 (1st repeat) Consultant mname Eg. A
-     System.out.println("\t(" + i + ") PV1-7.4 Consultant mname = " + pv1.getAttendingDoctor(i).getSecondAndFurtherGivenNamesOrInitialsThereof().toString());
-     // PV1-7.6 (1st repeat) Consultant Title Eg. Dr
-     System.out.println("\t(" + i + ") PV1-7.6 Consultant Title = " + pv1.getAttendingDoctor(i).getPrefixEgDR().toString());
-     // PV1-7.7 (1st repeat) Consultant local code Eg. AC3
-     System.out.println("\t(" + i + ") PV1-7.7 Consultant local code = " + pv1.getAttendingDoctor(i)./*getComponent(7).*//*getDegreeEgMD().toString());
- }
-
- reps = pv1.getReferringDoctorReps();
- System.out.println("THERE ARE " + reps + " REFERRING DOCTOR(S):");
- for (int i = 0; i < reps; i++) {   
-     // PV1-8.1 (1st repeat) Consultant Code	(Registered GP user pointer), 2nd Consultant Code (Referring doctor GMC Code)
-     System.out.println("\t(" + i + ") PV1-8.1 consultant code = " + pv1.getReferringDoctor(i).getPersonIdentifier().toString());
-     // PV1-8.2 (1st repeat) Consultant surname Eg. CASSONI, 2nd Consultant surname Eg. CASSONI
-     System.out.println("\t(" + i + ") PV1-8.2 Consultant surname = " + pv1.getReferringDoctor(i).getFamilyName().getSurname().toString());
-     // PV1-8.3 (1st repeat) Consultant Fname Eg. M, 2nd Consultant Fname Eg. M
-     System.out.println("\t(" + i + ") PV1-8.3 Consultant Fname = " + pv1.getReferringDoctor(i).getGivenName().toString());
-     // PV1-8.4 (1st repeat) Consultant mname Eg. A, 2nd Consultant mname Eg. A
-     System.out.println("\t(" + i + ") PV1-8.4 Consultant mname = " + pv1.getReferringDoctor(i).getSecondAndFurtherGivenNamesOrInitialsThereof().toString());
-     // PV1-8.6 1st repeat Consultant Title Eg. Dr, 2nd Consultant Title	Eg. Dr
-     System.out.println("\t(" + i + ") PV1-8.6 Consultant Title = " + pv1.getReferringDoctor(i).getPrefixEgDR().toString());
- }
- */
+ 
