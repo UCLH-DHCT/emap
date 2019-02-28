3b42d3e6f5a0341c2fb1f02c6afc6505735ce985
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2019-02-28 17:38:25 +0000

    Remove repositories (at least for now)

--- emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/PatientDemographicFactRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ucl.rits.inform.informdb;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientDemographicFactRepository extends CrudRepository<PatientDemographicFact, Integer> {
-
-}
--- emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ucl.rits.inform.informdb;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-
-}
