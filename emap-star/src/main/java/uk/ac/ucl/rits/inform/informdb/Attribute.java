d1f58267f908f709d34e1c19ff096a2ab780ad88
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2019-03-05 10:45:04 +0000

    Add in getters / setters for attributes

--- emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/Attribute.java
+++ emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/Attribute.java
@@ -2,6 +2,7 @@ package uk.ac.ucl.rits.inform.informdb;
 
 import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
@@ -19,18 +20,88 @@ import org.springframework.lang.NonNull;
 public class Attribute {
 
     @Id
-    private long       attributeId;
+    private long            attributeId;
 
+    @Column(unique = true, nullable = false, length = 15)
+    private String          shortName;
     @NonNull
-    private String     shortName;
+    private String          description;
     @NonNull
-    private String     description;
+    private ResultType      resultType;
     @NonNull
-    private ResultType resultType;
-    @NonNull
-    private Instant    addedTime;
+    private Instant         addedTime;
 
     @Transient
     private AttributeKeyMap attributeKey;
+
+    /**
+     * @return the attributeId
+     */
+    public long getAttributeId() {
+        return attributeId;
+    }
+
+    /**
+     * @param attributeId the attributeId to set
+     */
+    public void setAttributeId(long attributeId) {
+        this.attributeId = attributeId;
+    }
+
+    /**
+     * @return the shortName
+     */
+    public String getShortName() {
+        return shortName;
+    }
+
+    /**
+     * @param shortName the shortName to set
+     */
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the resultType
+     */
+    public ResultType getResultType() {
+        return resultType;
+    }
+
+    /**
+     * @param resultType the resultType to set
+     */
+    public void setResultType(ResultType resultType) {
+        this.resultType = resultType;
+    }
+
+    /**
+     * @return the addedTime
+     */
+    public Instant getAddedTime() {
+        return addedTime;
+    }
+
+    /**
+     * @param addedTime the addedTime to set
+     */
+    public void setAddedTime(Instant addedTime) {
+        this.addedTime = addedTime;
+    }
 
 }
