7d2bf0eb80f99253675c47772dc8abda9cc10cf4
Jeremy Stein <j.stein@ucl.ac.uk>
2019-03-21 17:02:29 +0000

    Move IDS operations into a separate class.
    Limit Entity scanning to the Inform-db Entities so that the IDS table
    isn't created in Inform-db.

--- core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/InformDbOperations.java
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/InformDbOperations.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,11 +26,14 @@ import ca.uhn.hl7v2.model.Message;
 import ca.uhn.hl7v2.model.v27.message.ADT_A01;
 import ca.uhn.hl7v2.parser.PipeParser;
 import uk.ac.ucl.rits.inform.ids.IdsMaster;
+import uk.ac.ucl.rits.inform.ids.IdsOperations;
 import uk.ac.ucl.rits.inform.informdb.Attribute;
 import uk.ac.ucl.rits.inform.informdb.AttributeKeyMap;
 import uk.ac.ucl.rits.inform.informdb.AttributeRepository;
 import uk.ac.ucl.rits.inform.informdb.Encounter;
 import uk.ac.ucl.rits.inform.informdb.EncounterRepository;
+import uk.ac.ucl.rits.inform.informdb.IdsProgress;
+import uk.ac.ucl.rits.inform.informdb.IdsProgressRepository;
 import uk.ac.ucl.rits.inform.informdb.Mrn;
 import uk.ac.ucl.rits.inform.informdb.MrnRepository;
 import uk.ac.ucl.rits.inform.informdb.PatientDemographicFact;
@@ -41,6 +45,7 @@ import uk.ac.ucl.rits.inform.informdb.VisitFact;
 import uk.ac.ucl.rits.inform.informdb.VisitProperty;
 
 @Component
+@EntityScan("uk.ac.ucl.rits.inform.informdb")
 public class InformDbOperations {
     @Autowired
     private AttributeRepository attributeRepository;
@@ -57,32 +62,19 @@ public class InformDbOperations {
 
     private final static Logger logger = LoggerFactory.getLogger(InformDbOperations.class);
 
-    private String idsCfgXml;
-    private boolean idsEmptyOnInit;
-
-    private SessionFactory idsFactory;
-
+    private IdsOperations idsOperations;
+    
     public InformDbOperations(
             @Value("${ids.cfg.xml.file}") String idsCfgXml,
             @Autowired Environment environment
         ) {
-        this.idsCfgXml = idsCfgXml;
-        System.out.println("DBTester() 1 - opening config file " + idsCfgXml);
-        String envPrefix = "IDS";
-        if (environment.acceptsProfiles("test")) {
-            envPrefix = null;
-        }
-        idsFactory = makeSessionFactory(idsCfgXml, envPrefix);
-        System.out.println("DBTester() 2");
-        idsEmptyOnInit = getIdsIsEmpty();
-        System.out.println("DBTester() : idsEmptyOnInit = " + idsEmptyOnInit);
+        System.out.println("InformDbOperations 1");
+        idsOperations = new IdsOperations(idsCfgXml, environment);
+        System.out.println("InformDbOperations 2");
     }
 
     public void close() {
-        if (idsFactory != null) {
-            idsFactory.close();
-        }
-        idsFactory = null;
+       idsOperations.close();
     }
 
     /**
@@ -188,7 +180,7 @@ public class InformDbOperations {
         // consider changing to "get next N messages" for more efficient database performance
         // when doing large "catch-up" operations
         // (handle the batching in the caller)
-        Session idsSession = idsFactory.openSession();
+        Session idsSession = idsOperations.openSession();
         idsSession.setDefaultReadOnly(true);
         Query<IdsMaster> qnext = idsSession.createQuery("from IdsMaster where unid > :lastProcessedId order by unid",
                 IdsMaster.class);
@@ -205,43 +197,6 @@ public class InformDbOperations {
         }
     }
 
-    /**
-     * create a session factory from the given config file, overwriting
-     * configurable values from the environment, if specified
-     *
-     * @param configFile the hibernate xml config file
-     * @param envPrefix the prefix for environment variable names, or null if no
-     *        variables should be read from the environment
-     *
-     * @return the SessionFactory thus created
-     */
-    private static SessionFactory makeSessionFactory(String configFile, String envPrefix) {
-        Configuration cfg = new Configuration().configure(configFile);
-        cfg.addAnnotatedClass(IdsMaster.class);
-
-        if (envPrefix != null) {
-            // take the username and password out of the environment
-            // so the config file can safely go into source control
-            String envVarUrl = envPrefix + "_JDBC_URL";
-            String envVarUsername = envPrefix + "_USERNAME";
-            String envVarPassword = envPrefix + "_PASSWORD";
-
-            String url = System.getenv(envVarUrl);
-            String uname = System.getenv(envVarUsername);
-            String pword = System.getenv(envVarPassword);
-            if (url != null) {
-                cfg.setProperty("hibernate.connection.url", url);
-            }
-            if (uname != null) {
-                cfg.setProperty("hibernate.connection.username", uname);
-            }
-            if (pword != null) {
-                cfg.setProperty("hibernate.connection.password", pword);
-            }
-        }
-
-        return cfg.buildSessionFactory();
-    }
 
     /**
      * Create a new encounter using the details given in the A01 message. This may
@@ -343,52 +298,8 @@ public class InformDbOperations {
         return mrn;
     }
 
-
-    /**
-     * Is the IDS currently empty?
-     */
-    private boolean getIdsIsEmpty() {
-        Session idsSession = idsFactory.openSession();
-        idsSession.setDefaultReadOnly(true);
-        // check is empty
-        Query<IdsMaster> qexists = idsSession.createQuery("from IdsMaster", IdsMaster.class);
-        qexists.setMaxResults(1);
-        boolean idsIsEmpty = qexists.list().isEmpty();
-        idsSession.close();
-        return idsIsEmpty;
-    }
-
-    /**
-     * 
-     * @return Was the IDS empty when this object was initialised?
-     */
-    public boolean getIdsEmptyOnInit() {
-        return idsEmptyOnInit;
-    }
-
     public void writeToIds(String hl7message, int id) {
-        // To avoid the risk of accidentally attempting to write into the real
-        // IDS, check that the IDS was empty when we started. Emptiness strongly suggests
-        // that this is a test IDS.
-        if (!getIdsEmptyOnInit()) {
-            throw new RuntimeException("Cannot write into non-empty IDS, are you sure this is a test?");
-        }
-        Session idsSession = idsFactory.openSession();
-        try {
-            Transaction tx = idsSession.beginTransaction();
-            IdsMaster idsrecord = new IdsMaster();
-            // We can't use a sequence to assign ID because it won't exist on the
-            // real IDS, so that will cause Hibernate validation to fail.
-            // However, since we're starting with an empty IDS and populating it
-            // in a single shot, just set the id manually in the client.
-            idsrecord.setUnid(id);
-            idsrecord.setHl7message(hl7message);
-            idsSession.save(idsrecord);
-            tx.commit();
-        }
-        finally {
-            idsSession.close();
-        }
+        idsOperations.writeToIds(hl7message, id);
     }
 
 }
--- core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/informdb/IdsProgressRepository.java
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/informdb/IdsProgressRepository.java
@@ -1,12 +1,14 @@
-package uk.ac.ucl.rits.inform;
+package uk.ac.ucl.rits.inform.informdb;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import uk.ac.ucl.rits.inform.informdb.Mrn;
 
+@EntityScan("uk.ac.ucl.rits.inform.informdb")
 public interface IdsProgressRepository extends CrudRepository<IdsProgress, Integer> {
 
     // currently there is only one row as there is only one number to represent
