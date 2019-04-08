f46cd368a0e3e014775ef9c877493b49b0a01f2e
Jeremy Stein <j.stein@ucl.ac.uk>
2019-04-08 18:24:29 +0100

    Quick SQL script to show visits within the hospital for an MRN

---
+++ hl7-reader/scripts/informdb_showvisits.sql
@@ -0,0 +1,38 @@
+\set ON_ERROR_STOP on
+-- Query movements around the hospital in Inform-db
+
+CREATE TEMPORARY VIEW DEMO_FACT_AGG AS
+SELECT
+pdf.fact_id,
+pdf.attribute_id,
+attrfact.description as attrfact,
+string_agg(attrprop.description, '|') as attrprop,
+string_agg(pdp.value_as_string, '|') as value
+FROM patient_demographic_fact pdf
+LEFT JOIN patient_demographic_property pdp ON pdp.fact = pdf.fact_id
+LEFT JOIN attribute attrfact ON attrfact.attribute_id = pdf.attribute_id
+LEFT JOIN attribute attrprop ON attrprop.attribute_id = pdp.attribute
+group by pdf.fact_id, attrfact.description
+order by fact_id, attribute_id
+;
+
+SELECT
+mrn.mrn,
+dfa.value,
+vf.visit_id,
+attrkey.description,
+vp.value_as_string,
+vp.value_as_datetime,
+attrval.description,
+enc.encounter
+FROM visit_property vp
+LEFT JOIN attribute attrkey ON attrkey.attribute_id = vp.attribute
+LEFT JOIN attribute attrval ON attrval.attribute_id = vp.value_as_attribute
+INNER JOIN visit_fact vf ON vf.visit_id = vp.visit
+INNER JOIN encounter enc ON enc.encounter_id = vf.encounter
+INNER JOIN mrn ON mrn.mrn_id = enc.mrn
+LEFT JOIN patient_demographic_fact pdf ON pdf.encounter = enc.encounter_id
+LEFT JOIN DEMO_FACT_AGG dfa ON pdf.fact_id = dfa.fact_id
+--WHERE mrn.mrn = '21011035'
+;
+
