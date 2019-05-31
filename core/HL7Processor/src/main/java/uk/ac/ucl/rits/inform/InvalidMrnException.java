d37857ffbd7ae86119d5235f1ac919635ae89374
Jeremy Stein <j.stein@ucl.ac.uk>
2019-05-31 13:57:56 +0100

    Implement merges

--- core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/InvalidMrnException.java
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/InvalidMrnException.java
@@ -2,4 +2,11 @@ package uk.ac.ucl.rits.inform;
 
 public class InvalidMrnException extends RuntimeException {
 
+    public InvalidMrnException(String string) {
+        super(string);
+    }
+
+    public InvalidMrnException() {
+    }
+
 }
