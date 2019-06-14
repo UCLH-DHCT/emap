99ed50182b1943f6b4c97826e02f795fe4f5566f
Jeremy Stein <j.stein@ucl.ac.uk>
2019-06-14 17:25:23 +0100

    Convert PIDWrap to interface

--- core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/hl7/PIDWrap.java
+++ core/HL7Processor/src/main/java/uk/ac/ucl/rits/inform/hl7/PIDWrap.java
@@ -26,78 +26,72 @@ import ca.uhn.hl7v2.model.v27.segment.PID;
     Escape Character (normally \)
 
  */
-public class PIDWrap {
-
-    private PID pid;
-
+public interface PIDWrap {
     /**
-     * @param myPID PID segment, obtained by parsing the message to which this segment relates (msg.getPID())
+     * @return the PID segment
      */
-    public PIDWrap(PID myPID) {
-        pid = myPID;
-    }
-
+    PID getPID();
 
     /**
      * @return PID-3.1[1] - in Carecast this is the MRN. Will Epic follow this convention?
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientFirstIdentifier() throws HL7Exception {
-        return pid.getPatientIdentifierList(0).getComponent(0).toString();
+    default String getPatientFirstIdentifier() throws HL7Exception {
+        return getPID().getPatientIdentifierList(0).getComponent(0).toString();
     }
 
     /**
      * @return PID-3.1[2] - in Carecast this is the NHS number. Will Epic follow this convention?
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientSecondIdentifier() throws HL7Exception {
-        return pid.getPatientIdentifierList(1).getComponent(0).toString();
+    default String getPatientSecondIdentifier() throws HL7Exception {
+        return getPID().getPatientIdentifierList(1).getComponent(0).toString();
     }
 
     /**
      * get name functions - these all assume patient has only one name.
      * If more are required then need to get use _pid.getPatientName(1)... etc
      * Certainly in Epic it appears that only one name data (XPN) object is stored.
-     * XPN xpn = pid.getPatientName(0); etc
+     * XPN xpn = getPID().getPatientName(0); etc
      *
      * NB Epic also has Suffix, Prefix, Academic degree, Name Type
      *
      * @return PID-5.1 family name
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientFamilyName() throws HL7Exception {
-        return pid.getPatientName(0).getFamilyName().getSurname().getValue();
+    default String getPatientFamilyName() throws HL7Exception {
+        return getPID().getPatientName(0).getFamilyName().getSurname().getValue();
     }
 
     /**
      * @return PID-5.2 family name
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientGivenName() throws HL7Exception {
-        return pid.getPatientName(0).getGivenName().getValue();
+    default String getPatientGivenName() throws HL7Exception {
+        return getPID().getPatientName(0).getGivenName().getValue();
     }
 
     /**
      * @return PID-5.3 family name
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientMiddleName() throws HL7Exception {
-        return pid.getPatientName(0).getSecondAndFurtherGivenNamesOrInitialsThereof().getValue();
+    default String getPatientMiddleName() throws HL7Exception {
+        return getPID().getPatientName(0).getSecondAndFurtherGivenNamesOrInitialsThereof().getValue();
     }
 
     /**
      * @return PID-5.5 family name
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientTitle() throws HL7Exception {
-        return pid.getPatientName(0).getPrefixEgDR().getValue();
+    default String getPatientTitle() throws HL7Exception {
+        return getPID().getPatientName(0).getPrefixEgDR().getValue();
     }
 
     /**
      * @return full name
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientFullName() throws HL7Exception {
+    default String getPatientFullName() throws HL7Exception {
         String result = this.getPatientTitle() + " " + this.getPatientGivenName() + " "
             + this.getPatientMiddleName() + " " + this.getPatientFamilyName();
         return result;
@@ -116,8 +110,8 @@ public class PIDWrap {
      * @return PID-7.1 birthdatetime
      * @throws HL7Exception if HAPI does
      */
-    public Instant getPatientBirthDate() throws HL7Exception {
-        return HL7Utils.interpretLocalTime(pid.getDateTimeOfBirth());
+    default Instant getPatientBirthDate() throws HL7Exception {
+        return HL7Utils.interpretLocalTime(getPID().getDateTimeOfBirth());
     }
 
 
@@ -125,8 +119,8 @@ public class PIDWrap {
      * @return PID-8 sex
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientSex() throws HL7Exception {
-        return pid.getAdministrativeSex().getIdentifier().getValue();
+    default String getPatientSex() throws HL7Exception {
+        return getPID().getAdministrativeSex().getIdentifier().getValue();
     }
 
 
@@ -151,8 +145,8 @@ public class PIDWrap {
     /**
      * @return the patient postcode (PID-11, first rep, component 5)
      */
-    public String getPatientZipOrPostalCode() {
-        return pid.getPatientAddress(0).getZipOrPostalCode().getValueOrEmpty();
+    default String getPatientZipOrPostalCode() {
+        return getPID().getPatientAddress(0).getZipOrPostalCode().getValueOrEmpty();
     }
 
     /**
@@ -160,8 +154,8 @@ public class PIDWrap {
      * @return Carecast - PID-13.1 (1st repeat) home phone
      * @throws HL7Exception if HAPI does
      */
-    public String getCarecastPatientHomePhoneNumber() throws HL7Exception {
-        return pid.getPhoneNumberHome(0).getTelephoneNumber().getValue();
+    default String getCarecastPatientHomePhoneNumber() throws HL7Exception {
+        return getPID().getPhoneNumberHome(0).getTelephoneNumber().getValue();
     }
 
     /**
@@ -169,8 +163,8 @@ public class PIDWrap {
      * @return Carecast - PID-13.1 (2nd repeat) mobile
      * @throws HL7Exception if HAPI does
      */
-    public String getCarecastPatientMobilePhoneNumber() throws HL7Exception {
-        return pid.getPhoneNumberHome(1).getTelephoneNumber().getValue();
+    default String getCarecastPatientMobilePhoneNumber() throws HL7Exception {
+        return getPID().getPhoneNumberHome(1).getTelephoneNumber().getValue();
     }
 
     /**
@@ -178,15 +172,15 @@ public class PIDWrap {
      * @return Carecast - PID-13.1 (3rd repeat) email
      * @throws HL7Exception if HAPI does
      */
-    public String getCarecastPatientEmail() throws HL7Exception {
-        return pid.getPhoneNumberHome(2).getTelephoneNumber().getValue();
+    default String getCarecastPatientEmail() throws HL7Exception {
+        return getPID().getPhoneNumberHome(2).getTelephoneNumber().getValue();
     }
 
     /**
      * @return patient home phone number
      * @throws HL7Exception if HAPI does
      */
-    public String getEpicPatientHomePhoneNumber() throws HL7Exception {
+    default String getEpicPatientHomePhoneNumber() throws HL7Exception {
         return "Not yet implemented";
     }
 
@@ -194,7 +188,7 @@ public class PIDWrap {
      * @return patient email
      * @throws HL7Exception if HAPI does
      */
-    public String getEpicPatientEmail() throws HL7Exception {
+    default String getEpicPatientEmail() throws HL7Exception {
         return "Not yet implemented";
     }
 
@@ -202,8 +196,8 @@ public class PIDWrap {
      * @return Carecast - PID-14.1 business phone
      * @throws HL7Exception if HAPI does
      */
-    public String getCarecastPatientBusinessPhoneNumber() throws HL7Exception {
-        return pid.getPhoneNumberBusiness(0).getTelephoneNumber().getValue();
+    default String getCarecastPatientBusinessPhoneNumber() throws HL7Exception {
+        return getPID().getPhoneNumberBusiness(0).getTelephoneNumber().getValue();
     }
 
     /**
@@ -220,42 +214,42 @@ public class PIDWrap {
      * @return the patient business phone number
      * @throws HL7Exception if HAPI does
      */
-    public String getEpicPatientBusinessPhoneNumber() throws HL7Exception {
+    default String getEpicPatientBusinessPhoneNumber() throws HL7Exception {
         return "Not yet implemented";
     }
 
     // language - seems different for Carecast and Epic
     // Carecast has PID-15.1 Interpreter code and PID-15.4 Language code.
     // Epic just seems to have one code value. Format: <code>^^^^^^^^~
-    // CWE c = pid.getPid15_PrimaryLanguage()
+    // CWE c = getPID().getPid15_PrimaryLanguage()
     // component 1: c.getCwe1_Identifier() [ST type]
     // component 4: c.getCwe4_AlternateIdentifier() [ST]
 
 
     // PID-16 Marital Status. NOTE: The Carecast spec says that it uses the locally-defined field ZLC.15 instead.
     // Epic: Format: <code>^^^^^^^^
-    // cwe c = pid.getPid16_MaritalStatus()
+    // cwe c = getPID().getPid16_MaritalStatus()
 
     /**
      * @return PID-17 Religion - or should this be PID-17.1?
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientReligion() throws HL7Exception {
-        return pid.getReligion().getComponent(0).toString();
+    default String getPatientReligion() throws HL7Exception {
+        return getPID().getReligion().getComponent(0).toString();
     }
 
     /**
      * @return PID-18.1 (or PID-18?) Patient Account Number.
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientAccountNumber() throws HL7Exception {
-        return pid.getPatientAccountNumber().getComponent(0).toString();
+    default String getPatientAccountNumber() throws HL7Exception {
+        return getPID().getPatientAccountNumber().getComponent(0).toString();
     }
 
     // PID-21 Mother's Identifier (for newborns). This is not mapped in the Carecast spec
     // Epic: This field is used to link a newborn to his or her mother. It should contain the MRN or a visit
     // identifier for the mother of the patient in the PID segment of the message.
-    // CX[] c = pid.getPid21_MotherSIdentifier()
+    // CX[] c = getPID().getPid21_MotherSIdentifier()
 
     // PID-22 Ethnic Group.  In place of PID-22 Carecast has PID-10.1 Ethnicity, although according to the spec it doesn't have PID-10!!!
     // Epic: Patient ethnic group. Only the first component is used
@@ -266,15 +260,15 @@ public class PIDWrap {
      * @return the ethnic group
      * @throws HL7Exception if HAPI does
      */
-    public String getEthnicGroup() throws HL7Exception {
-        return pid.getPid22_EthnicGroup(0).getText().toString(); // ???need data to check this is correct.
+    default String getEthnicGroup() throws HL7Exception {
+        return getPID().getPid22_EthnicGroup(0).getText().toString(); // ???need data to check this is correct.
     }
 
     // PID-23 Birth Place This is not mapped in the Carecast spec.
     // Epic: Birth city and state. HL7 address format is used rather than the string format defined in the standard.
     // State can be mapped using a translation table
     // Need to see real messages to understand what they mean in line above.
-    // Possibly pid.getPid23_BirthPlace() [ST]
+    // Possibly getPID().getPid23_BirthPlace() [ST]
 
     /**
      * PID-24 Multiple Birth Indicator. This is not mapped in the Carecast spec.
@@ -282,8 +276,8 @@ public class PIDWrap {
      * @return PID-24 Multiple Birth Indicator
      * @throws HL7Exception if HAPI does
      */
-    public String getMultipleBirthIndicator() throws HL7Exception {
-        return pid.getPid24_MultipleBirthIndicator().toString(); // ??? need to see data to check
+    default String getMultipleBirthIndicator() throws HL7Exception {
+        return getPID().getPid24_MultipleBirthIndicator().toString(); // ??? need to see data to check
     }
 
     // PID-26 Citizenship. This is not mapped in the Carecast spec. Epic: This field can be mapped using a translation table.
@@ -296,16 +290,16 @@ public class PIDWrap {
      * @return PID-29 (or PID-29.1?) Patient Death Date and Time. Carecast: YYYYMMDDHHmm format though hhmm is mainly 0000
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientDeathDateTime() throws HL7Exception {
-        return pid.getPatientDeathDateAndTime().toString();
+    default String getPatientDeathDateTime() throws HL7Exception {
+        return getPID().getPatientDeathDateAndTime().toString();
     }
 
     /**
      * @return PID-30 Patient Death Indicator. Carcast: Y = dead, N = alive.
      * @throws HL7Exception if HAPI does
      */
-    public String getPatientDeathIndicator() throws HL7Exception {
-        return pid.getPatientDeathIndicator().getValue();
+    default String getPatientDeathIndicator() throws HL7Exception {
+        return getPID().getPatientDeathIndicator().getValue();
     }
 
     // Epic - PID-32 Identity Reliability Code ??
