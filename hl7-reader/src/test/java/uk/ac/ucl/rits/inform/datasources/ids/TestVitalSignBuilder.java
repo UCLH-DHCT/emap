93f53168a0c784e822508cc788f8ba74976e8fd5
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-01-09 12:46:26 +0000

    Update VitalSign example message based off EPIC screenshot
    with added sender application and sender facility

--- hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestVitalSignBuilder.java
+++ hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestVitalSignBuilder.java
@@ -21,64 +21,63 @@ import static junit.framework.TestCase.assertEquals;
 @ActiveProfiles("test")
 @SpringBootTest
 public class TestVitalSignBuilder {
-    List<VitalSigns> vitalSigns;
-    VitalSigns firstVitalSign;
+    private List<VitalSigns> vitalSigns;
+    private VitalSigns firstVitalSign;
 
     @Before
     public void setUp() throws IOException, HL7Exception {
         String hl7 = HL7Utils.readHl7FromResource("VitalSignHL7Message.txt");
         Message hl7Msg = HL7Utils.parseHl7String(hl7);
-        vitalSigns =  new VitalSignBuilder((ORU_R01) hl7Msg, "42").getMessages();
+        vitalSigns = new VitalSignBuilder("42", (ORU_R01) hl7Msg).getMessages();
         firstVitalSign = vitalSigns.get(0);
     }
 
     @Test
-    public void testMRN()  {
+    public void testMRN() {
         String result = firstVitalSign.getMrn();
         assertEquals("6537077", result);
     }
 
     @Test
-    public void testVisitNumber()  {
+    public void testVisitNumber() {
         String result = firstVitalSign.getVisitNumber();
         assertEquals("1234TESTVISITNUM", result);
     }
 
     @Test
-    public void testVitalSignIdentifier()  {
+    public void testVitalSignIdentifier() {
         String result = firstVitalSign.getVitalSignIdentifier();
         assertEquals("EPIC$271649006", result);
     }
 
     @Test
-    public void testNumericValue()  {
+    public void testNumericValue() {
         Double result = firstVitalSign.getNumericValue();
         assertEquals(132.0, result);
     }
 
     @Test
-    public void testStringValue()  {
+    public void testStringValue() {
         String result = vitalSigns.get(6).getStringValue();
         assertEquals("string value", result);
     }
 
     @Test
-    public void testUnit()  {
+    public void testUnit() {
         String result = firstVitalSign.getUnit();
         assertEquals("mm[Hg]", result);
     }
 
     @Test
-    public void testObservationTimeTaken()  {
+    public void testObservationTimeTaken() {
         Instant result = firstVitalSign.getObservationTimeTaken();
         assertEquals(Instant.parse("2010-02-11T22:05:25.00Z"), result);
     }
 
     @Test
-    public void testSourceMessageId()  {
+    public void testSourceMessageId() {
         String result = firstVitalSign.getSourceMessageId();
         assertEquals("42_01", result);
     }
-
 
 }
