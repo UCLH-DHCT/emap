b7162682b64786f1d04c3c069893730e131dcbe6
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-01-10 11:10:25 +0000

    Add equals and hash to Pair class to allow for testing

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/messaging/Pair.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/messaging/Pair.java
@@ -1,8 +1,9 @@
 package uk.ac.ucl.rits.inform.interchange.messaging;
 
+import java.util.Objects;
+
 /**
  * A simple paired value class.
- *
  * @param <T> first item
  * @param <U> second item
  */
@@ -18,7 +19,7 @@ public final class Pair<T, U> {
     public final U second;
 
     /**
-     * @param first item
+     * @param first  item
      * @param second item
      */
     public Pair(T first, U second) {
@@ -29,5 +30,23 @@ public final class Pair<T, U> {
     @Override
     public String toString() {
         return String.format("(%s, %s)", first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return first.equals(pair.first)
+                && second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
     }
 }
