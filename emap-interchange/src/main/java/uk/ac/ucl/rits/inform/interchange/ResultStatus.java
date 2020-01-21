bca1259ea31db2a00abe37b9a905e47ce15efeb2
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-01-21 15:47:39 +0000

    Add javadoc for ResultStatus

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/ResultStatus.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/ResultStatus.java
@@ -1,5 +1,8 @@
 package uk.ac.ucl.rits.inform.interchange;
 
+/**
+ * Result status of a HL7 message defines the action to be taken upon receipt of the message.
+ */
 public enum ResultStatus {
     /**
      * Add or update the record.
