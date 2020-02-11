9b60ac7acf37e1c9c643efa78f0f80104f4733cb
Sarah <s.keating@ucl.ac.uk>
2020-02-11 15:34:17 +0000

    Revert "committed wrong files"
    This reverts commit a5e416e2a00c5ccbe574366e1171d28f9c9bca23.

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PathologyOrder.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PathologyOrder.java
@@ -213,6 +213,19 @@ public class PathologyOrder extends EmapOperationMessage implements Serializable
     }
 
     /**
+     * Get pathology result by index.
+     * @param index int index of result to get
+     * @return PathologyResult at index given or null if out of bounds
+     */
+    public PathologyResult getPathologyResult(int index) {
+        if (index >= getNumPathologyResults() || index < 0) {
+            return null;
+        } else {
+            return pathologyResults.get(index);
+        }
+    }
+
+    /**
      * @return int number of pathology results in list
      */
     public int getNumPathologyResults() {
