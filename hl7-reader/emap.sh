625054acf99bd24af16fc0a0a6f904e9b65fb113
Jeremy Stein <j.stein@ucl.ac.uk>
2020-03-06 19:21:47 +0000

    Can invoke emap script as emap-live.sh to avoid fakeuds and dbfiller.

--- hl7-reader/emap.sh
+++ hl7-reader/emap.sh
@@ -6,13 +6,21 @@ set -a # auto-export all vars so docker-compose can see them for opening ports e
 source "$EMAP_CONFIG_FILE"
 set +a
 >&2 echo "Emap in a box config file: $EMAP_CONFIG_FILE"
+# If invoked as emap-live.sh then disable the fake UDS and dbfiller
+if [ "$0" = "emap-live.sh" ]; then
+    FAKEUDS_ARG=""
+    DBFILLER_ARG=""
+else
+    FAKEUDS_ARG="-f $SCRIPT_DIR/docker-compose.fakeuds.yml"
+    DBFILLER_ARG="-f "$(dirname "$SCRIPT_DIR")"/DatabaseFiller/docker-compose.yml"
+fi
 set -x
 # Order of docker-compose files can matter. Eg. env_file location is relative to
 # the directory of the *first* -f value.
 docker-compose \
     -f "$SCRIPT_DIR/docker-compose.yml" \
-    -f "$SCRIPT_DIR/docker-compose.fakeuds.yml" \
-    -f "$(dirname "$SCRIPT_DIR")"/DatabaseFiller/docker-compose.yml \
+    $FAKEUDS_ARG \
+    $DBFILLER_ARG \
     -f "$(dirname "$SCRIPT_DIR")"/Omop-ETL/docker-compose.yml \
     -p $EMAP_PROJECT_NAME \
     "$@"
