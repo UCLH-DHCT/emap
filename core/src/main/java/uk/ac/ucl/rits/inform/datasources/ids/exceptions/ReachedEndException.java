b2d1b6a686f8d89b6e370dc0a2d5ecb3668e6c6c
Jeremy Stein <j.stein@ucl.ac.uk>
2020-04-07 14:53:19 +0100

    Missing file.

---
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/exceptions/ReachedEndException.java
@@ -0,0 +1,11 @@
+package uk.ac.ucl.rits.inform.datasources.ids.exceptions;
+
+/**
+ * Processing has stopped because we've processed up to a configured end point.
+ *
+ * @author Jeremy Stein
+ *
+ */
+public class ReachedEndException extends Exception {
+
+}
