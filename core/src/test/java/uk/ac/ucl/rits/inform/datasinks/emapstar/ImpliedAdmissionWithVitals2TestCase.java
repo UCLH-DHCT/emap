5713666e3ada8480b27da4385a35676f92bdf4de
Jeremy Stein <j.stein@ucl.ac.uk>
2020-04-13 16:19:42 +0100

    Treat a transfer as an admission if there are currently no visits for the patient, even if the encounter already exists (eg. because of having received vitals or pathology).

--- core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/ImpliedAdmissionWithVitals2TestCase.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/ImpliedAdmissionWithVitals2TestCase.java
@@ -1,7 +1,6 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar;
 
 import org.junit.Before;
-import org.junit.Ignore;
 
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
 
@@ -9,13 +8,8 @@ import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException
  * A transfer message for a patient we've never seen before, but where there's
  * previously been a vital signs message (which creates an encounter but no visit).
  *
- * This currently fails because the encounter has been created previously, and
- * the core processor therefore assumes the visit must already exist, and gets
- * upset when it doesn't.
- *
  * @author Jeremy Stein
  */
-@Ignore
 public class ImpliedAdmissionWithVitals2TestCase extends ImpliedAdmissionTestCase {
     public ImpliedAdmissionWithVitals2TestCase() {
     }
