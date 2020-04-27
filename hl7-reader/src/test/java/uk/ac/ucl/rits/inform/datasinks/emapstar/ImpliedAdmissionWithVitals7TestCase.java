2f69b70c10d7f9b8c8a1c3d2db97cb1b90d1a8e5
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-04-27 12:03:57 +0100

    Fix implied admission by adding final update of patient details

--- hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/ImpliedAdmissionWithVitals7TestCase.java
+++ hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/ImpliedAdmissionWithVitals7TestCase.java
@@ -1,14 +1,7 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar;
 
-import org.junit.Assert;
 import org.junit.Before;
-import uk.ac.ucl.rits.inform.interchange.AdtMessage;
-import uk.ac.ucl.rits.inform.interchange.AdtOperationType;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
-
-import java.time.Instant;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * A transfer message for a patient we've never seen before, but where there's
@@ -18,11 +11,12 @@ import static org.junit.Assert.assertEquals;
 public class ImpliedAdmissionWithVitals7TestCase extends ImpliedAdmissionTestCase {
     public ImpliedAdmissionWithVitals7TestCase() {
     }
+
     @Before
     public void setup() throws EmapOperationMessageProcessingException {
         addVitals();
         performUpdatePatientDetails();
         performTransfer();
-        Assert.assertTrue("Test should fail but doesn't -  see VitalsUpdateTransferTestCase", false);
+        performUpdatePatientDetails();
     }
 }
