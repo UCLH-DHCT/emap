b0bde8048fad265801de8e050153163bcae19781
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2020-04-27 14:51:53 +0100

    Updated IDS tests

--- core/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestAdtDeath2.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestAdtDeath2.java
@@ -1,13 +1,12 @@
 package uk.ac.ucl.rits.inform.datasources.ids;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.time.Instant;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import uk.ac.ucl.rits.inform.interchange.AdtMessage;
 
@@ -19,7 +18,7 @@ import uk.ac.ucl.rits.inform.interchange.AdtMessage;
 public class TestAdtDeath2 extends TestHl7MessageStream {
     private AdtMessage msg;
 
-    @Before
+    @BeforeEach
     public void setup() throws Exception {
         msg = processSingleAdtMessage("GenericAdt/A03_death_2.txt");
     }
--- core/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestVitalsIgnore.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestVitalsIgnore.java
@@ -1,11 +1,11 @@
 package uk.ac.ucl.rits.inform.datasources.ids;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessage;
 
@@ -17,7 +17,7 @@ import uk.ac.ucl.rits.inform.interchange.EmapOperationMessage;
 public class TestVitalsIgnore extends TestHl7MessageStream {
     private List<? extends EmapOperationMessage> msgs;
 
-    @Before
+    @BeforeEach
     public void setup() throws Exception {
         msgs = processSingleMessage("GenericAdt/Vitals.txt");
     }
