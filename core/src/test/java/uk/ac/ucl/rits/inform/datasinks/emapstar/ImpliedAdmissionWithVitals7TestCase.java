23c14d87668fecab89b0691451a46c7c6dffa1b8
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-04-28 12:26:23 +0100

    Properly document implied admission test case

--- core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/ImpliedAdmissionWithVitals7TestCase.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/ImpliedAdmissionWithVitals7TestCase.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
 
 /**
- * A transfer message for a patient we've never seen before, but where there's
- * previously been a vital signs message (which creates an encounter but no visit).
- * @author Jeremy Stein
+ * After an implied admission, update the patient details, then transfer.
+ * This should not lead to a duplicate fact id, so another update of patient details
+ * should not throw and exception
+ * @author Stef Piatek
  */
 public class ImpliedAdmissionWithVitals7TestCase extends ImpliedAdmissionTestCase {
     public ImpliedAdmissionWithVitals7TestCase() {
