dd959a6f2534176f15e474ab4d39bb7c489167f9
Jeremy Stein <j.stein@ucl.ac.uk>
2020-06-03 19:01:29 +0100

    Make correct and erroneous locations different in cancel transfer test. Check for slightly different expected states of the db for two different paths (with and without initial admit).

--- hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/CancelTransfer.java
+++ hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/CancelTransfer.java
@@ -44,6 +44,8 @@ public class CancelTransfer extends MessageStreamBaseCase {
         erroneousTransferTime = currentTime;
         queueCancelTransfer();
         cancellationTime = currentTime;
+        // burn a location so the correct location is different to the erroneous one
+        nextLocation();
         queueTransfer();
         correctTransferLocation = currentLocation();
         correctTransferTime = currentTime;
@@ -62,6 +64,8 @@ public class CancelTransfer extends MessageStreamBaseCase {
         erroneousTransferTime = currentTime;
         queueCancelTransfer();
         cancellationTime = currentTime;
+        // burn a location so the correct location is different to the erroneous one
+        nextLocation();
         queueTransfer();
         correctTransferLocation = currentLocation();
         correctTransferTime = currentTime;
@@ -104,26 +108,46 @@ public class CancelTransfer extends MessageStreamBaseCase {
         Map<String, List<PatientFact>> validBedVisitsByLocation =
                 bedVisitsByValidity.get(true).stream().collect(Collectors.groupingBy(
                         vis -> vis.getPropertyByAttribute(AttributeKeyMap.LOCATION).get(0).getValueAsString()));
-        List<PatientProperty> propertiesForCurrentBedVisit =
-                validBedVisitsByLocation.get(correctTransferLocation).get(0).getProperties();
-        PatientFact originalBedVisit = validBedVisitsByLocation.get(originalLocation).get(0);
-        List<PatientProperty> allDischargeTimes = originalBedVisit
-                .getPropertyByAttribute(AttributeKeyMap.DISCHARGE_TIME);
 
-        emapStarTestUtils._testPropertyValuesOverTime(allDischargeTimes,
-                omitAdmit ? null : erroneousTransferTime, correctTransferTime, erroneousTransferTime,
-                        cancellationTime, correctTransferTime);
-
-        assertTrue(!propertiesForCancelledBedVisit.isEmpty());
+        // whatever route we took to get here, the current location should be the correct one
+        List<PatientFact> correctLocations = validBedVisitsByLocation.get(correctTransferLocation);
+        assertEquals(1, correctLocations.size());
+        List<PatientProperty> propertiesForCurrentBedVisit = correctLocations.get(0).getProperties();
         assertTrue(!propertiesForCurrentBedVisit.isEmpty());
-        assertTrue(propertiesForCancelledBedVisit.stream().allMatch(p -> !p.isValid()));
         assertTrue(propertiesForCurrentBedVisit.stream().allMatch(p -> p.isValid()));
-        // all properties except discharge time should be valid
-        assertTrue(originalBedVisit.getProperties().stream()
-                .filter(p -> !p.getPropertyType().getShortName().equals(AttributeKeyMap.DISCHARGE_TIME.getShortname()))
-                .allMatch(p -> p.isValid()));
 
-        // check times and locations are the correct ones
-        assertEquals(cancellationTime, bedVisitsByValidity.get(false).get(0).getValidUntil());
+        // in all paths, the erroneous locations should not be valid (one deleted, one invalid)
+        Map<String, List<PatientFact>> allBedVisitsByLocation =
+                bedVisits.stream().collect(Collectors.groupingBy(
+                        vis -> vis.getPropertyByAttribute(AttributeKeyMap.LOCATION).get(0).getValueAsString()));
+        List<PatientFact> allErroniousBedVisits = allBedVisitsByLocation.get(erroneousTransferLocation);
+        assertTrue(allErroniousBedVisits.stream().allMatch(v -> !v.isValid() && v.getProperties().stream().allMatch(p -> !p.isValid())));
+
+        // the invalidation date on the erroneous bed visit matches the cancellation time
+        List<PatientFact> allInvalidErroniousBedVisits = allErroniousBedVisits.stream().filter(v -> v.getValidUntil() != null).collect(Collectors.toList());        assertEquals(1, correctLocations.size());
+        assertEquals(1, allInvalidErroniousBedVisits.size());
+        assertEquals(cancellationTime, allInvalidErroniousBedVisits.get(0).getValidUntil());
+
+        if (!omitAdmit) {
+            // If we had >= 2 locations before the cancel transfer the original location gets
+            // its discharge property invalidated, and then subsequently re-added when the correct
+            // transfer is entered.
+            List<PatientFact> originalBedVisits = validBedVisitsByLocation.get(originalLocation);
+            assertEquals(1,  originalBedVisits.size());
+            PatientFact originalBedVisit = originalBedVisits.get(0);
+            List<PatientProperty> allDischargeTimes = originalBedVisit
+                    .getPropertyByAttribute(AttributeKeyMap.DISCHARGE_TIME);
+
+            emapStarTestUtils._testPropertyValuesOverTime(allDischargeTimes,
+                    omitAdmit ? null : erroneousTransferTime, correctTransferTime, erroneousTransferTime,
+                            cancellationTime, correctTransferTime);
+            assertTrue(!propertiesForCancelledBedVisit.isEmpty());
+            assertTrue(propertiesForCancelledBedVisit.stream().allMatch(p -> !p.isValid()));
+            // all properties except discharge time should be valid
+            assertTrue(originalBedVisit.getProperties().stream()
+                    .filter(p -> !p.getPropertyType().getShortName().equals(AttributeKeyMap.DISCHARGE_TIME.getShortname()))
+                    .allMatch(p -> p.isValid()));
+        }
+
     }
 }
