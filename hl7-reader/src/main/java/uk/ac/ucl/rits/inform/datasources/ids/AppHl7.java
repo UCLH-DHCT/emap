8a9236c273ded9fd7cc84a853be7c8a02cf89101
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-06-11 13:42:29 +0100

    remove informdb

--- hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/AppHl7.java
+++ hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/AppHl7.java
@@ -23,7 +23,6 @@ import uk.ac.ucl.rits.inform.interchange.messaging.Publisher;
  */
 @SpringBootApplication(scanBasePackages = {
         "uk.ac.ucl.rits.inform.datasources.ids",
-        "uk.ac.ucl.rits.inform.informdb",
         "uk.ac.ucl.rits.inform.interchange"})
 public class AppHl7 {
     private static final Logger logger = LoggerFactory.getLogger(AppHl7.class);
