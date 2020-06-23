9618f29b748bfdfc86010e1ec96bc364bd67ece4
Stefan Piatek <s.piatek@ucl.ac.uk>
2020-06-23 18:15:25 +0100

    Document interchange message factory

--- emap-interchange/src/test/java/uk/ac/ucl/rits/inform/interchange/InterchangeMessageFactory.java
+++ emap-interchange/src/test/java/uk/ac/ucl/rits/inform/interchange/InterchangeMessageFactory.java
@@ -3,7 +3,6 @@ package uk.ac.ucl.rits.inform.interchange;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.util.ResourceUtils;
 
@@ -12,17 +11,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TestPathologyOrderFactory {
+/**
+ * Builds interchange messages from yaml files.
+ * Allows for easier setup for integration testing in hl7 sources and emap star
+ */
+public class InterchangeMessageFactory {
     private ObjectMapper mapper;
 
-    public TestPathologyOrderFactory() {
+    public InterchangeMessageFactory() {
         mapper = new ObjectMapper(new YAMLFactory());
+        // Finds modules so instants can be parsed correctly
         mapper.findAndRegisterModules();
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
 
-    public List<PathologyOrder> pathologyOrderFactory(String fileName) {
+    /**
+     * Builds pathology orders from yaml file given, overriding default values for pathology orders and pathology results
+     * @param fileName filename in test resources, this is also the basename for the defaults
+     *                 e.g. 'ORU_R01.yaml' has pathology result values overwritten from 'ORU_R01_pathology_result_defaults.yaml'
+     *                 and pathology order values overwritten from 'ORU_R01_pathology_order_defaults.yaml'
+     * @return List of pathology orders deserialised from the files
+     */
+    public List<PathologyOrder> getPathologyOrders(String fileName) {
         List<PathologyOrder> pathologyOrders = new ArrayList<>();
         try {
             File file = ResourceUtils.getFile("classpath:" + fileName);
@@ -41,13 +51,11 @@ public class TestPathologyOrderFactory {
 
     private PathologyOrder updatePathologyOrderWithDefaults(PathologyOrder order, String fileName) throws IOException {
         ObjectReader orderReader = mapper.readerForUpdating(order);
-        PathologyOrder updatedOrder = orderReader.readValue(ResourceUtils.getFile("classpath:" + fileName));
-        return updatedOrder;
+        return orderReader.readValue(ResourceUtils.getFile("classpath:" + fileName));
     }
 
     private PathologyResult updatePathologyResultWithDefaults(PathologyResult result, String fileName) throws IOException {
         ObjectReader resultReader = mapper.readerForUpdating(result);
-        PathologyResult updatedResult = resultReader.readValue(ResourceUtils.getFile("classpath:" + fileName));
-        return updatedResult;
+        return resultReader.readValue(ResourceUtils.getFile("classpath:" + fileName));
     }
 }
