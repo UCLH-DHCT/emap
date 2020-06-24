e223814163e0268f75fd1f72972073d08c14d052
Roma Klapaukh <r.klapaukh@ucl.ac.uk>
2020-06-24 11:21:05 +0100

    Change PersonMrn to MrnToLive

---
+++ emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/MrnToLive.java
@@ -0,0 +1,87 @@
+package uk.ac.ucl.rits.inform.informdb;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Over time Mrns are merged into others as more is found out about a patient.
+ * This table stores a mapping from every MRN known to the system, to its
+ * currently live (in use) MRN.
+ *
+ * @author UCL RITS
+ *
+ */
+@Entity
+@Table(name = "mrn_to_live")
+public class MrnToLive extends TemporalCore implements Serializable {
+
+    private static final long serialVersionUID = 7019692664925413320L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long              mrnToLiveId;
+
+    @ManyToOne
+    @JoinColumn(name = "mrn_id", nullable = false)
+    private Mrn               mrnId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "live_mrn_id", nullable = false)
+    private Mrn               liveMrnId;
+
+    /**
+     * @return the mrnToLiveId
+     */
+    public Long getMrnToLiveId() {
+        return mrnToLiveId;
+    }
+
+    /**
+     * @param mrnToLiveId the mrnToLiveId to set
+     */
+    public void setMrnToLiveId(Long mrnToLiveId) {
+        this.mrnToLiveId = mrnToLiveId;
+    }
+
+    /**
+     * @return the mrnId
+     */
+    public Mrn getMrnId() {
+        return mrnId;
+    }
+
+    /**
+     * @param mrnId the mrnId to set
+     */
+    public void setMrnId(Mrn mrnId) {
+        this.mrnId = mrnId;
+    }
+
+    /**
+     * @return the liveMrnId
+     */
+    public Mrn getLiveMrnId() {
+        return liveMrnId;
+    }
+
+    /**
+     * @param liveMrnId the liveMrnId to set
+     */
+    public void setLiveMrnId(Mrn liveMrnId) {
+        this.liveMrnId = liveMrnId;
+    }
+
+    @Override
+    public String toString() {
+        return "MrnToLive [mrn_id=" + mrnId + ", live_mrn_id=" + liveMrnId + ", current=" + this.isCurrent() + "]";
+    }
+
+}
