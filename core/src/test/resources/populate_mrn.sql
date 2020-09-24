1c5fccb259fd0ef52b81cb5861bc55db16e1c2e6
Stef Piatek <s.piatek@ucl.ac.uk>
2020-09-24 16:25:29 +0100

    Update test sql files

--- core/src/test/resources/populate_mrn.sql
+++ core/src/test/resources/populate_mrn.sql
@@ -8,11 +8,11 @@ INSERT INTO public.mrn (mrn_id, mrn, nhs_number, source_system, stored_from) VAL
 
 -- Add mrn_to_live
 
-INSERT INTO public.mrn_to_live (mrn_to_live_id, stored_from, stored_until, valid_from, valid_until, live_mrn_id, mrn_id) VALUES
-    (2001, '2010-09-01 11:04:04.794000', null, '2010-09-01 11:04:04.794000', null, 1001, 1001),
-    (2002, '2010-09-03 10:05:04.794000', null, '2010-09-03 11:04:04.794000', null, 1003, 1002),
-    (2003, '2010-09-10 16:01:05.371000', null, '2010-09-03 13:06:05.794000', null, 1003, 1003),
-    (2004, '2010-09-10 17:02:08.000000', null, '2010-09-05 14:05:04.794000', null, 1004, 1004);
+INSERT INTO public.mrn_to_live (mrn_to_live_id, stored_from, valid_from, live_mrn_id, mrn_id) VALUES
+    (2001, '2010-09-01 11:04:04.794000', '2010-09-01 11:04:04.794000', 1001, 1001),
+    (2002, '2010-09-03 10:05:04.794000', '2010-09-03 11:04:04.794000', 1003, 1002),
+    (2003, '2010-09-10 16:01:05.371000', '2010-09-03 13:06:05.794000', 1003, 1003),
+    (2004, '2010-09-10 17:02:08.000000', '2010-09-05 14:05:04.794000', 1004, 1004);
 
 
 -- Add to core_demographic
