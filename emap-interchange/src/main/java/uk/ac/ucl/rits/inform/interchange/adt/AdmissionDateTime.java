627f6e49a255718846ee5e1e6559b4c190295978
Stef Piatek <s.piatek@ucl.ac.uk>
2020-10-01 16:19:04 +0100

    Document use of AdmissionDateTime

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/AdmissionDateTime.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/AdmissionDateTime.java
@@ -6,6 +6,11 @@ import java.time.Instant;
 
 /**
  * Ensuring admission date time is used in a class.
+ * <p>
+ * Only used in specific circumstances because it can be used to mean either presentation time or admission time.
+ * Doesn't seem to be used consistently by all message types so only use when you're sure that it's giving the correct value.
+ * A01 and A03 (admit and discharge) seem to consistently use it for Admission time
+ * A04 uses it for presentation time.
  */
 public interface AdmissionDateTime {
     Hl7Value<Instant> getAdmissionDateTime();
