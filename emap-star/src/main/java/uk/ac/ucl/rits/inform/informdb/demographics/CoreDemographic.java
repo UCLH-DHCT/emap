20dc698857982caad13c89dd0000fce15f4d6d34
Stef Piatek <s.piatek@ucl.ac.uk>
2020-10-05 14:41:03 +0100

    Delete all core demographics upon DeletePatientInformation message

--- emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/demographics/CoreDemographic.java
+++ emap-star/src/main/java/uk/ac/ucl/rits/inform/informdb/demographics/CoreDemographic.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.Instant;
 
 /**
  * A core demographic represents the main demographics stored around patients.
