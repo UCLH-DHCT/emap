2546a149631b49fb5acf32886bfa5de356b0ca2d
Stef Piatek <s.piatek@ucl.ac.uk>
2020-10-06 13:06:51 +0100

    Parse ADT A45 and A47

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/ChangePatientIdentifiers.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/ChangePatientIdentifiers.java
@@ -7,18 +7,16 @@ import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
 
 /**
- * Merge the entire record of two patients.
- * HL7 messages: A40
+ * Change patient identifiers
+ * HL7 messages: A47
+ * The new MRN should not already exist in the system. Therefore not a merge, but instead change the MRN string value to the new value.
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class MergePatient extends AdtMessage implements PreviousIdentifiers {
-    private static final long serialVersionUID = -2500473433999508161L;
+public class ChangePatientIdentifiers extends AdtMessage implements PreviousIdentifiers {
+    private static final long serialVersionUID = 2764828119416263690L;
 
-    /**
-     * MRN to be merged and retired.
-     */
     private String previousMrn;
     private String previousNhsNumber;
 
--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/MoveVisitInformation.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/MoveVisitInformation.java
@@ -7,20 +7,19 @@ import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
 
 /**
- * Merge the entire record of two patients.
- * HL7 messages: A40
+ * Move visit from previous MRN and visit number to current.
+ * HL7 messages: A45
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class MergePatient extends AdtMessage implements PreviousIdentifiers {
-    private static final long serialVersionUID = -2500473433999508161L;
+public class MoveVisitInformation extends AdtMessage implements PreviousIdentifiers {
 
-    /**
-     * MRN to be merged and retired.
-     */
+    private static final long serialVersionUID = 8612053846611150031L;
+
     private String previousMrn;
     private String previousNhsNumber;
+    private String previousVisitNumber;
 
     @Override
     public String processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
