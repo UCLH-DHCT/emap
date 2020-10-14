020e6b54637ea34589e5b4da209341ab81c3a67c
Stef Piatek <s.piatek@ucl.ac.uk>
2020-10-14 09:09:05 +0100

    Fix PV2 Wrap documentation

--- hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/PV2Wrap.java
+++ hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/PV2Wrap.java
@@ -4,17 +4,17 @@ import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.v26.segment.PV2;
 
 /**
- * Wrapper around the HAPI parser's PV1 segment object, to make it easier to use.
- * Reference page: https://hapifhir.github.io/hapi-hl7v2/v27/apidocs/ca/uhn/hl7v2/model/v27/segment/PV1.html
+ * Wrapper around the HAPI parser's PV2 segment object, to make it easier to use.
+ * Reference page: https://hapifhir.github.io/hapi-hl7v2/v27/apidocs/ca/uhn/hl7v2/model/v27/segment/PV2.html
  */
 interface PV2Wrap {
     /**
-     * @return the PV1 object
+     * @return the PV2 object
      */
     PV2 getPV2();
 
     /**
-     * @return whether the PV1 segment exists
+     * @return whether the PV2 segment exists
      */
     default boolean pv2SegmentExists() {
         return getPV2() != null;
