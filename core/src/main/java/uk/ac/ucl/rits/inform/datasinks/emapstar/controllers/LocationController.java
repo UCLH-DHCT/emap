d554ad8fb2c324580952a8f8d1174bbfc4135632
Stef Piatek <s.piatek@ucl.ac.uk>
2020-10-19 13:20:46 +0100

    Comment private methods for locations

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/LocationController.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/LocationController.java
@@ -9,10 +9,12 @@ import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.LocationRepository;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.LocationVisitRepository;
 import uk.ac.ucl.rits.inform.informdb.identity.HospitalVisit;
 import uk.ac.ucl.rits.inform.informdb.movement.AuditLocationVisit;
+import uk.ac.ucl.rits.inform.informdb.movement.Location;
 import uk.ac.ucl.rits.inform.informdb.movement.LocationVisit;
 import uk.ac.ucl.rits.inform.interchange.adt.AdtMessage;
 
 import java.time.Instant;
+import java.util.Optional;
 
 /**
  * Controls interaction with Locations.
@@ -50,13 +52,17 @@ public class LocationController {
             return;
         }
         String locationString = msg.getFullLocationString().get();
-        if (locationRepo.findByLocationStringEquals(locationString).isPresent()) {
+        Optional<Location> potentialLocation = locationRepo.findByLocationStringEquals(locationString);
+        if (potentialLocation.isEmpty()) {
             throw new IllegalArgumentException(
                     String.format("Location string '%s' was not found in location table for message: %s", locationString, msg));
         }
 
         Instant validFrom = getValidFrom(msg);
-        RowState<LocationVisit> locationState = getOrCreateLocationVisit(visit, msg, validFrom, storedFrom);
+        RowState<LocationVisit> locationState = getOrCreateParentLocationVisit(visit, potentialLocation.get(), msg, validFrom, storedFrom);
+        if (locationVisitHasParent(msg)) {
+            locationState = getOrCreateChildLocationVisit(locationState, potentialLocation.get(), msg, validFrom, storedFrom);
+        }
         final LocationVisit originalLocation = locationState.getEntity().copy();
 
         if (locationVisitShouldBeUpdated(locationState, msg)) {
@@ -76,19 +82,39 @@ public class LocationController {
         return (msg.getEventOccurredDateTime() == null) ? msg.getRecordedDateTime() : msg.getEventOccurredDateTime();
     }
 
-    private RowState<LocationVisit> getOrCreateLocationVisit(HospitalVisit visit, AdtMessage msg, Instant validFrom, Instant storedFrom) {
-        // TODO
-        LocationVisit location = new LocationVisit();
-        return new RowState<>(location, Instant.now(), Instant.now(), true);
+    private RowState<LocationVisit> getOrCreateParentLocationVisit(HospitalVisit visit, Location location, AdtMessage msg,
+                                                                   Instant validFrom, Instant storedFrom) {
+        // get locations by the hospital visit with no parent visit_location
+        // otherwise create visit_location
+        LocationVisit locationVisit = new LocationVisit();
+        return new RowState<>(locationVisit, Instant.now(), Instant.now(), true);
+    }
+
+
+    private boolean locationVisitHasParent(AdtMessage msg) {
+        // message is for a transient visit, e.g. where a bed visit is not given up while they go to the MRI scanner.
+        return false;
+    }
+
+    private RowState<LocationVisit> getOrCreateChildLocationVisit(RowState<LocationVisit> parentLocationState, Location location, AdtMessage msg,
+                                                                  Instant validFrom, Instant storedFrom) {
+        // get location by parent location visit id
+        // otherwise create the child visit
+        // if created location_visit and it's meant to have a parent, throw an invalid state error because the parent doesn't exist
+        LocationVisit locationVisit = new LocationVisit();
+        return new RowState<>(locationVisit, Instant.now(), Instant.now(), true);
     }
 
     private boolean locationVisitShouldBeUpdated(RowState<LocationVisit> locationState, AdtMessage msg) {
-        // TODO
+        // message valid from is the same or newer than the current locationState
         return false;
     }
 
     private void updateLocation(AdtMessage msg, RowState<LocationVisit> locationState) {
-        //TODO
+        // if the message has a parent and the message moves back to parent location, then remove the child location
+        // otherwise update the location fk to the new location fk
+        // on discharge, do we just delete the row?
+        // if using, update the admission and discharge datetime here too
     }
 
     private void manuallySaveLocationOrAuditIfRequired(LocationVisit originalLocation, RowState<LocationVisit> locationState,
