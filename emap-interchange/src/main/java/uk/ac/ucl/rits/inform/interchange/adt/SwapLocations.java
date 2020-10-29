0a8f5a2bf836abba6f4ee0de4550789120d67696
Stef Piatek <s.piatek@ucl.ac.uk>
2020-10-29 16:42:01 +0000

    Add admission datetime to swap location message

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/SwapLocations.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/SwapLocations.java
@@ -7,24 +7,26 @@ import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
 import uk.ac.ucl.rits.inform.interchange.Hl7Value;
 
+import java.time.Instant;
+
 /**
  * Swap two patients' locations.
- * Contains information about two patients locations, the other patient should already exist in the system.
+ * Contains information about two patients locations that should be swapped.
+ * From looking at epic messages, only applies to open messages and always has admission date time.
  * HL7 messages: A17
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class SwapLocations extends AdtMessage {
+public class SwapLocations extends AdtMessage implements AdmissionDateTime {
     private static final long serialVersionUID = 1811745798929900545L;
 
+    private Hl7Value<Instant> admissionDateTime;
     private String otherMrn;
     private String otherNhsNumber;
     private String otherVisitNumber;
-    private Hl7Value<String> otherCurrentBed = Hl7Value.unknown();
-    private Hl7Value<String> otherCurrentRoomCode = Hl7Value.unknown();
-    private Hl7Value<String> otherCurrentWardCode = Hl7Value.unknown();
     private Hl7Value<String> otherFullLocationString = Hl7Value.unknown();
+    private Hl7Value<Instant> otherAdmissionDateTime;
 
     @Override
     public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
