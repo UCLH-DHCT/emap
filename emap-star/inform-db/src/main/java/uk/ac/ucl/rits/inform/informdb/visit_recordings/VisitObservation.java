e495e8ea78d277ac46deca9428d914f495aac90c
Stef Piatek <s.piatek@ucl.ac.uk>
2020-11-16 15:41:58 +0000

    Update VisitObservation tables

--- emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/visit_recordings/VisitObservation.java
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/visit_recordings/VisitObservation.java
@@ -1,175 +1,80 @@
 package uk.ac.ucl.rits.inform.informdb.visit_recordings;
 
-import uk.ac.ucl.rits.inform.informdb.AuditCore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import uk.ac.ucl.rits.inform.informdb.TemporalCore;
+import uk.ac.ucl.rits.inform.informdb.annotation.AuditTable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.Instant;
 
 /**
  * VisitObservations represent discrete nurse (or machine) recoded observations
  * about patients at specific time points.
- * @author Roma Klapaukh
+ * @author Roma Klapaukh & Stef Piatek
  */
+@SuppressWarnings("serial")
 @Entity
-public class VisitObservation extends TemporalCore<VisitObservation, AuditCore> {
+@Table
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@AuditTable
+public class VisitObservation extends TemporalCore<VisitObservation, VisitObservationAudit> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long visitObservationId;
-    private long visitObservationDurableId;
 
-    private long visitObservationTypeId;
+    @ManyToOne
+    @JoinColumn(name = "visitObservationTypeId", nullable = false)
+    private VisitObservationType visitObservationTypeId;
 
     private String valueAsText;
-    private double valueAsReal;
+    private Double valueAsReal;
     private String unit;
 
     /**
-     * The time this indvidiual observation was made.
+     * The time this individual observation was made.
      */
-    @Column(columnDefinition = "timestamp with time zone")
+    @Column(columnDefinition = "timestamp with time zone", nullable = false)
     private Instant recordingDatetime;
 
     /**
      * The time the set of observations (observations are often taken in groups) is
      * associated with.
      */
-    @Column(columnDefinition = "timestamp with time zone")
+    @Column(columnDefinition = "timestamp with time zone", nullable = false)
     private Instant panelDatetime;
 
+    /**
+     * Default constructor.
+     */
     public VisitObservation() {}
 
+    /**
+     * Build a new Visit observation from an existing one.
+     * @param other existing visit observation
+     */
     public VisitObservation(VisitObservation other) {
         super(other);
-
-        this.visitObservationDurableId = other.visitObservationDurableId;
-
         this.visitObservationTypeId = other.visitObservationTypeId;
-
         this.valueAsText = other.valueAsText;
         this.valueAsReal = other.valueAsReal;
-
         this.unit = other.unit;
-
         this.recordingDatetime = other.recordingDatetime;
         this.panelDatetime = other.panelDatetime;
-    }
-
-    /**
-     * @return the visitObservationId
-     */
-    public long getVisitObservationId() {
-        return visitObservationId;
-    }
-
-    /**
-     * @param visitObservationId the visitObservationId to set
-     */
-    public void setVisitObservationId(long visitObservationId) {
-        this.visitObservationId = visitObservationId;
-    }
-
-    /**
-     * @return the visitObservationDurableId
-     */
-    public long getVisitObservationDurableId() {
-        return visitObservationDurableId;
-    }
-
-    /**
-     * @param visitObservationDurableId the visitObservationDurableId to set
-     */
-    public void setVisitObservationDurableId(long visitObservationDurableId) {
-        this.visitObservationDurableId = visitObservationDurableId;
-    }
-
-    /**
-     * @return the visitObservationTypeId
-     */
-    public long getVisitObservationTypeId() {
-        return visitObservationTypeId;
-    }
-
-    /**
-     * @param visitObservationTypeId the visitObservationTypeId to set
-     */
-    public void setVisitObservationTypeId(long visitObservationTypeId) {
-        this.visitObservationTypeId = visitObservationTypeId;
-    }
-
-    /**
-     * @return the valueAsText
-     */
-    public String getValueAsText() {
-        return valueAsText;
-    }
-
-    /**
-     * @param valueAsText the valueAsText to set
-     */
-    public void setValueAsText(String valueAsText) {
-        this.valueAsText = valueAsText;
-    }
-
-    /**
-     * @return the valueAsReal
-     */
-    public double getValueAsReal() {
-        return valueAsReal;
-    }
-
-    /**
-     * @param valueAsReal the valueAsReal to set
-     */
-    public void setValueAsReal(double valueAsReal) {
-        this.valueAsReal = valueAsReal;
-    }
-
-    /**
-     * @return the unit
-     */
-    public String getUnit() {
-        return unit;
-    }
-
-    /**
-     * @param unit the unit to set
-     */
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    /**
-     * @return the recordingDatetime
-     */
-    public Instant getRecordingDatetime() {
-        return recordingDatetime;
-    }
-
-    /**
-     * @param recordingDatetime the recordingDatetime to set
-     */
-    public void setRecordingDatetime(Instant recordingDatetime) {
-        this.recordingDatetime = recordingDatetime;
-    }
-
-    /**
-     * @return the panelDatetime
-     */
-    public Instant getPanelDatetime() {
-        return panelDatetime;
-    }
-
-    /**
-     * @param panelDatetime the panelDatetime to set
-     */
-    public void setPanelDatetime(Instant panelDatetime) {
-        this.panelDatetime = panelDatetime;
     }
 
     @Override
@@ -178,7 +83,7 @@ public class VisitObservation extends TemporalCore<VisitObservation, AuditCore> 
     }
 
     @Override
-    public AuditCore createAuditEntity(Instant validUntil, Instant storedUntil) {
+    public VisitObservationAudit createAuditEntity(Instant validUntil, Instant storedUntil) {
         throw new UnsupportedOperationException();
     }
 }
--- emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/visit_recordings/VisitObservationType.java
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/visit_recordings/VisitObservationType.java
@@ -1,5 +1,7 @@
 package uk.ac.ucl.rits.inform.informdb.visit_recordings;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,125 +13,40 @@ import javax.persistence.Id;
  * EHR systems these are often coded either with potentially ambiguous short
  * names, or sometimes just numbers. This table maps these system level terms
  * into standardised vocabularies to make their meanings clear.
- *
- * @author Roma Klapaukh
- *
+ * @author Roma Klapaukh & Stef Piatek
  */
+@Data
+@SuppressWarnings("serial")
 @Entity
 public class VisitObservationType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long   visitObservationType;
+    private long visitObservationType;
 
+    /**
+     * The hospital system that the data has been loaded from.
+     */
     @Column(nullable = false)
     private String sourceSystem;
 
+    /**
+     * The hospital application that the data has been loaded from.
+     */
     @Column(nullable = false)
-    private String sourceSystemCode;
-    private String sourceSystemName;
+    private String sourceApplication;
+
+    /**
+     * The code used by the hospital application to identify the observation type.
+     */
+    @Column(nullable = false)
+    private String sourceApplicationCode;
+    /**
+     * Readable name for the hospital application observation type.
+     */
+    private String sourceApplicationName;
     private String standardisedCode;
     private String standardisedVocabulary;
     private String primaryDataType;
-
-    public VisitObservationType() {}
-
-    /**
-     * @return the visitObservationType
-     */
-    public long getVisitObservationType() {
-        return visitObservationType;
-    }
-
-    /**
-     * @param visitObservationType the visitObservationType to set
-     */
-    public void setVisitObservationType(long visitObservationType) {
-        this.visitObservationType = visitObservationType;
-    }
-
-    /**
-     * @return the sourceSystem
-     */
-    public String getSourceSystem() {
-        return sourceSystem;
-    }
-
-    /**
-     * @param sourceSystem the sourceSystem to set
-     */
-    public void setSourceSystem(String sourceSystem) {
-        this.sourceSystem = sourceSystem;
-    }
-
-    /**
-     * @return the sourceSystemCode
-     */
-    public String getSourceSystemCode() {
-        return sourceSystemCode;
-    }
-
-    /**
-     * @param sourceSystemCode the sourceSystemCode to set
-     */
-    public void setSourceSystemCode(String sourceSystemCode) {
-        this.sourceSystemCode = sourceSystemCode;
-    }
-
-    /**
-     * @return the standardisedCode
-     */
-    public String getStandardisedCode() {
-        return standardisedCode;
-    }
-
-    /**
-     * @param standardisedCode the standardisedCode to set
-     */
-    public void setStandardisedCode(String standardisedCode) {
-        this.standardisedCode = standardisedCode;
-    }
-
-    /**
-     * @return the standardisedVocabulary
-     */
-    public String getStandardisedVocabulary() {
-        return standardisedVocabulary;
-    }
-
-    /**
-     * @param standardisedVocabulary the standardisedVocabulary to set
-     */
-    public void setStandardisedVocabulary(String standardisedVocabulary) {
-        this.standardisedVocabulary = standardisedVocabulary;
-    }
-
-    /**
-     * @return the sourceSystemName
-     */
-    public String getSourceSystemName() {
-        return sourceSystemName;
-    }
-
-    /**
-     * @param sourceSystemName the sourceSystemName to set
-     */
-    public void setSourceSystemName(String sourceSystemName) {
-        this.sourceSystemName = sourceSystemName;
-    }
-
-    /**
-     * @return the primaryDataType
-     */
-    public String getPrimaryDataType() {
-        return primaryDataType;
-    }
-
-    /**
-     * @param primaryDataType the primaryDataType to set
-     */
-    public void setPrimaryDataType(String primaryDataType) {
-        this.primaryDataType = primaryDataType;
-    }
 
 }
