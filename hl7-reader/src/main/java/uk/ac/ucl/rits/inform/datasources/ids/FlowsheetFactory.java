2f1c4e79e8ac76d3fbf13ef083d4b94a0b04a0e9
Stef Piatek <s.piatek@ucl.ac.uk>
2020-11-16 15:12:51 +0000

    Parse HL7 flowsheets with refactored interchange format

---
+++ hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/FlowsheetFactory.java
@@ -0,0 +1,234 @@
+package uk.ac.ucl.rits.inform.datasources.ids;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.Type;
+import ca.uhn.hl7v2.model.Varies;
+import ca.uhn.hl7v2.model.v26.datatype.FT;
+import ca.uhn.hl7v2.model.v26.datatype.NM;
+import ca.uhn.hl7v2.model.v26.group.ORU_R01_OBSERVATION;
+import ca.uhn.hl7v2.model.v26.group.ORU_R01_ORDER_OBSERVATION;
+import ca.uhn.hl7v2.model.v26.group.ORU_R01_PATIENT_RESULT;
+import ca.uhn.hl7v2.model.v26.message.ORU_R01;
+import ca.uhn.hl7v2.model.v26.segment.EVN;
+import ca.uhn.hl7v2.model.v26.segment.MSH;
+import ca.uhn.hl7v2.model.v26.segment.NTE;
+import ca.uhn.hl7v2.model.v26.segment.OBX;
+import ca.uhn.hl7v2.model.v26.segment.PID;
+import ca.uhn.hl7v2.model.v26.segment.PV1;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import uk.ac.ucl.rits.inform.datasources.ids.exceptions.Hl7InconsistencyException;
+import uk.ac.ucl.rits.inform.interchange.Flowsheet;
+import uk.ac.ucl.rits.inform.interchange.Hl7Value;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Build one or more flowsheets from HL7 message.
+ * @author Stef Piatek
+ */
+@Component
+public class FlowsheetFactory {
+    private static final Logger logger = LoggerFactory.getLogger(FlowsheetFactory.class);
+
+    /**
+     * Builds Flowsheet messages from an ORU R01 message.
+     * Allows multiple: OBR segments, OBX segments, NTE segments, OBX lines and NTE lines
+     * @param idsUnid Unique id from UDS
+     * @param msg     ORU R01 HL7 flowsheet message from EPIC
+     * @return Flowsheet messages populated from contstuctor.
+     */
+    List<Flowsheet> getMessages(String idsUnid, Message msg) {
+        List<Flowsheet> flowsheets = new ArrayList<>();
+        try {
+            ORU_R01_PATIENT_RESULT patientResult = ((ORU_R01) msg).getPATIENT_RESULT();
+            PID pid = patientResult.getPATIENT().getPID();
+            MSH msh = (MSH) msg.get("MSH");
+            PV1 pv1 = patientResult.getPATIENT().getVISIT().getPV1();
+            EVN evn = (EVN) msg.get("EVN");
+            Instant recordedDateTime = HL7Utils.interpretLocalTime(evn.getRecordedDateTime());
+
+            flowsheets = buildAllFlowsheets(idsUnid, pid, msh, pv1, recordedDateTime, patientResult.getORDER_OBSERVATIONAll());
+        } catch (HL7Exception e) {
+            FlowsheetFactory.logger.error(String.format("HL7 Exception encountered for msg %s", idsUnid), e);
+        }
+        return flowsheets;
+    }
+
+    /**
+     * @param idsUnid          IDS unid
+     * @param msh              MSH segment
+     * @param pid              PID segment
+     * @param pv1              PIV segment
+     * @param recordedDateTime Event datetime of the message
+     * @param orderObs         ORU R01 order observations from the HL7 message
+     * @return
+     * @throws HL7Exception
+     */
+    private List<Flowsheet> buildAllFlowsheets(final String idsUnid, final PID pid, final MSH msh, final PV1 pv1,
+                                               final Instant recordedDateTime, final Iterable<ORU_R01_ORDER_OBSERVATION> orderObs) throws HL7Exception {
+        List<Flowsheet> flowsheets = new ArrayList<>();
+        for (ORU_R01_ORDER_OBSERVATION orderObr : orderObs) {
+            List<ORU_R01_OBSERVATION> observations = orderObr.getOBSERVATIONAll();
+
+            int msgSuffix = 0;
+            for (ORU_R01_OBSERVATION observation : observations) {
+                msgSuffix++;
+                String subMessageSourceId = String.format("%s$%02d", idsUnid, msgSuffix);
+                try {
+                    Flowsheet flowsheet = buildFlowsheet(subMessageSourceId, observation, msh, pid, pv1, recordedDateTime);
+                    flowsheets.add(flowsheet);
+                } catch (Hl7InconsistencyException e) {
+                    FlowsheetFactory.logger.error(String.format("Flowsheet could not be parsed for msg %s", subMessageSourceId), e);
+                }
+            }
+        }
+        return flowsheets;
+    }
+
+    /**
+     * Populate flowsheet message from HL7 message segments.
+     * @param subMessageSourceId Unique ID of message
+     * @param observation        observation object
+     * @param msh                MSH segment
+     * @param pid                PID segment
+     * @param pv1                PIV segment
+     * @param recordedDateTime   recorded date time
+     * @return Flowsheet
+     * @throws HL7Exception              if HL7 message cannot be parsed
+     * @throws Hl7InconsistencyException if message does not have required data
+     */
+    private Flowsheet buildFlowsheet(String subMessageSourceId, ORU_R01_OBSERVATION observation, MSH msh, PID pid, PV1 pv1, Instant recordedDateTime)
+            throws HL7Exception, Hl7InconsistencyException {
+        Flowsheet flowsheet = new Flowsheet();
+
+        OBX obx = observation.getOBX();
+        List<NTE> notes = observation.getNTEAll();
+
+        // set generic information
+        PatientInfoHl7 patientHl7 = new PatientInfoHl7(msh, pid, pv1);
+        flowsheet.setMrn(patientHl7.getMrn());
+        flowsheet.setVisitNumber(patientHl7.getVisitNumber());
+        flowsheet.setSourceMessageId(subMessageSourceId);
+        flowsheet.setSourceSystem(patientHl7.getSendingApplication());
+        flowsheet.setSourceApplication(patientHl7.getSendingApplication());
+        flowsheet.setUpdatedTime(recordedDateTime);
+
+        // set information from obx
+        String observationId = obx.getObx3_ObservationIdentifier().getCwe1_Identifier().getValueOrEmpty();
+        flowsheet.setFlowsheetId(observationId);
+
+        setFlowsheetValue(subMessageSourceId, flowsheet, obx);
+
+        if (!notes.isEmpty()) {
+            String comment = getComments(notes);
+            flowsheet.setComment(Hl7Value.buildFromHl7(comment));
+        }
+        if (flowsheet.getComment().isUnknown() && flowsheet.getStringValue().isUnknown() && flowsheet.getNumericValue().isUnknown()) {
+            throw new IllegalArgumentException(String.format("msg %s has empty value and comment so was discarded", subMessageSourceId));
+        }
+
+        flowsheet.setUnit(Hl7Value.buildFromHl7(getUnits(obx)));
+        flowsheet.setObservationTime(HL7Utils.interpretLocalTime(obx.getObx14_DateTimeOfTheObservation()));
+        return flowsheet;
+    }
+
+    /**
+     * Get units from the OBX segment.
+     * @param obx OBX segment
+     * @return units
+     */
+    private String getUnits(OBX obx) {
+        return obx.getObx6_Units().getCwe1_Identifier().getValueOrEmpty();
+    }
+
+    /**
+     * Sets the string value or numeric value as appropriate, setting to delete if required.
+     * @param subMessageSourceId Message ID along with the sub message Id
+     * @param flowsheet          flowsheet to add the values to
+     * @param obx                OBX segment
+     * @throws Hl7InconsistencyException If the result status is unknown or numeric result can't be parsed
+     */
+    private void setFlowsheetValue(String subMessageSourceId, Flowsheet flowsheet, OBX obx) throws Hl7InconsistencyException {
+        String resultStatus = obx.getObx11_ObservationResultStatus().getValueOrEmpty();
+        if ("D".equals(resultStatus)) {
+            flowsheet.setStringValue(Hl7Value.delete());
+            flowsheet.setNumericValue(Hl7Value.delete());
+            return;
+        }
+        if (!("F".equals(resultStatus) || "C".equals(resultStatus))) {
+            throw new Hl7InconsistencyException(String.format("msg %s result status ('%s') was not recognised.", subMessageSourceId, resultStatus));
+        }
+
+        // Assuming numeric data will be a single value
+        Type singularData = obx.getObservationValue(0).getData();
+        // HAPI can return null so use nullDefault as empty string
+        String value = singularData.toString();
+        value = value == null ? "" : value;
+
+        if (singularData instanceof NM) {
+            try {
+                flowsheet.setNumericValue(Hl7Value.buildFromHl7(Double.parseDouble(value)));
+            } catch (NumberFormatException e) {
+                throw new Hl7InconsistencyException(
+                        String.format("Numeric result expected for msg %s, instead '%s' was found", subMessageSourceId, value));
+            }
+        } else {
+            // Skip empty string values
+            if (!value.isEmpty()) {
+                String stringValue = getStringValue(obx);
+                flowsheet.setStringValue(Hl7Value.buildFromHl7(stringValue.trim()));
+            }
+        }
+    }
+
+    /**
+     * Build comments from list of NTEs, trimmed and lines separated by newlines.
+     * @param notes List of NTE objects
+     * @return String of trimmed comment lines, joined by newlines
+     */
+    private String getComments(List<NTE> notes) {
+        StringBuilder commentBuilder = new StringBuilder();
+        // multiple NTE segments
+        for (NTE note : notes) {
+            FT[] allComments = note.getNte3_Comment();
+            // Multiple lines in field
+            for (FT comment : allComments) {
+                if (commentBuilder.length() > 1) {
+                    commentBuilder.append("\n");
+                }
+                commentBuilder.append(comment.getValueOrEmpty().trim());
+            }
+        }
+        return commentBuilder.toString().trim();
+    }
+
+    /**
+     * Extracts string value from obx. Allows for multiple lines in an OBX (separated by newline characters)
+     * @param obx OBX object
+     * @return String of all whitespace trimmed lines separated by newlines
+     */
+    private String getStringValue(OBX obx) {
+        // Strings can be made of multiple values
+        Varies[] dataVaries = obx.getObx5_ObservationValue();
+        StringBuilder valueBuilder = new StringBuilder();
+        // Allow for multiple results
+        for (Varies resultLine : dataVaries) {
+            Type lineData = resultLine.getData();
+            String lineValue = lineData.toString();
+            if (lineValue != null) {
+                if (valueBuilder.length() > 1) {
+                    valueBuilder.append("\n");
+                }
+                valueBuilder.append(lineValue.trim());
+            }
+        }
+        return valueBuilder.toString().trim();
+    }
+
+
+}
