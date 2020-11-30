67fd44f8f2c1bd91921e77ec1839eeba1a7a8602
Stef Piatek <s.piatek@ucl.ac.uk>
2020-11-30 15:45:58 +0000

    Use specific ImpliedAdtMessage

---
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/ImpliedAdtMessage.java
@@ -0,0 +1,23 @@
+package uk.ac.ucl.rits.inform.interchange.adt;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
+import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
+
+/**
+ * ADT message implied from a non-ADT source (such as Flowsheet or Pathology results).
+ * Demographics and encounter information can be used. Location information from the messages is not trusted.
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class ImpliedAdtMessage extends AdtMessage {
+    private static final long serialVersionUID = 6177251667805777164L;
+
+    @Override
+    public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
+        processor.processMessage(this);
+    }
+}
