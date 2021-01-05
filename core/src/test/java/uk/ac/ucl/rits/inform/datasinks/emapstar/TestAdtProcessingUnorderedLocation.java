4e3380c665334c281008f872daea9cd11a4ea5c3
Stef Piatek <s.piatek@ucl.ac.uk>
2021-01-05 13:44:59 +0000

    Fix message path for CI testing
    Looks like my IDE was too kind to me?

--- core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/TestAdtProcessingUnorderedLocation.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/TestAdtProcessingUnorderedLocation.java
@@ -26,7 +26,7 @@ class TestAdtProcessingUnorderedLocation extends MessageProcessingBase {
     @TestFactory
     public Stream<DynamicTest> testUnorderedMoves() {
         adtFilenames = new String[] {"02_A01", "03_A02", "04_A02", "05_A02", "06_A02", "07_A06", "08_A03"};
-        orderPermutationTestProducer.setMessagePath("location/Moves");
+        orderPermutationTestProducer.setMessagePath("Location/Moves");
         orderPermutationTestProducer.setInitialAdmissionTime(Instant.parse("2013-02-11T11:00:52Z"));
         orderPermutationTestProducer.setLocations(new String[]{
                 "ED^UCHED RAT CHAIR^RAT-CHAIR",
@@ -53,7 +53,7 @@ class TestAdtProcessingUnorderedLocation extends MessageProcessingBase {
     public Stream<DynamicTest> testUnorderedCancelAdmit() {
         adtFilenames = new String[] {"01_A01", "02_A11", "03_A01", "04_A02", "05_A03"};
 
-        orderPermutationTestProducer.setMessagePath("location/CancelAdmit");
+        orderPermutationTestProducer.setMessagePath("Location/CancelAdmit");
         orderPermutationTestProducer.setInitialAdmissionTime(Instant.parse("2013-02-11T13:00:52Z"));
         orderPermutationTestProducer.setLocations(new String[]{
                 "ED^UCHED RAT CHAIR^RAT-CHAIR",
@@ -75,7 +75,7 @@ class TestAdtProcessingUnorderedLocation extends MessageProcessingBase {
     @TestFactory
     public Stream<DynamicTest> testUnorderedCancelDischarge() {
         adtFilenames = new String[] {"01_A01", "02_A02", "03_A03", "04_A13", "05_A03"};
-        orderPermutationTestProducer.setMessagePath("location/CancelDischarge");
+        orderPermutationTestProducer.setMessagePath("Location/CancelDischarge");
         orderPermutationTestProducer.setInitialAdmissionTime(Instant.parse("2013-02-11T11:00:52Z"));
         orderPermutationTestProducer.setLocations(new String[]{
                 "ED^UCHED RAT CHAIR^RAT-CHAIR",
@@ -98,7 +98,7 @@ class TestAdtProcessingUnorderedLocation extends MessageProcessingBase {
     @TestFactory
     public Stream<DynamicTest> testUnorderedCancelTransfer() {
         adtFilenames = new String[] {"01_A01", "02_A02", "03_A02", "04_A12", "05_A02", "06_A03"};
-        orderPermutationTestProducer.setMessagePath("location/CancelTransfer");
+        orderPermutationTestProducer.setMessagePath("Location/CancelTransfer");
         orderPermutationTestProducer.setInitialAdmissionTime(Instant.parse("2013-02-11T11:00:52Z"));
         orderPermutationTestProducer.setLocations(new String[]{
                 "ED^UCHED RAT CHAIR^RAT-CHAIR",
