0baffc64330965ff18630465b6a580788f95ea3e
Stef Piatek <s.piatek@ucl.ac.uk>
2021-01-21 10:57:09 +0000

    Add simple numeric testing of winpath oru r01

--- hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestWinPathLabOruR01.java
+++ hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestWinPathLabOruR01.java
@@ -13,6 +13,7 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import uk.ac.ucl.rits.inform.interchange.InterchangeValue;
 import uk.ac.ucl.rits.inform.interchange.lab.LabOrderMsg;
 import uk.ac.ucl.rits.inform.interchange.lab.LabResultMsg;
 
@@ -35,6 +36,11 @@ public class TestWinPathLabOruR01 extends TestHl7MessageStream {
         return msgs.get(index);
     }
 
+    private Map<String, LabResultMsg> getResultsByItemCode(List<LabResultMsg> labResultMsgs) {
+        return labResultMsgs.stream()
+                .collect(Collectors.toMap(LabResultMsg::getTestItemLocalCode, v -> v));
+    }
+
     /**
      * Test battery code and description are correctly parsed.
      */
@@ -51,22 +57,21 @@ public class TestWinPathLabOruR01 extends TestHl7MessageStream {
     @Test
     public void testStringResultOnlyParsed() {
         LabOrderMsg msg = processLab("LabOrders/covid19test.txt", 0);
-        List<LabResultMsg> LabResultMsgs = msg.getLabResultMsgs();
-        assertEquals(3, LabResultMsgs.size());
-        Map<String, LabResultMsg> resultsByItemCode = LabResultMsgs.stream()
-                .collect(Collectors.toMap(LabResultMsg::getTestItemLocalCode, v -> v));
+        List<LabResultMsg> labResultMsgs = msg.getLabResultMsgs();
+        assertEquals(3, labResultMsgs.size());
+        Map<String, LabResultMsg> resultsByItemCode = getResultsByItemCode(labResultMsgs);
         assertEquals(new HashSet<>(Arrays.asList("NCVS", "NCVP", "NCVL")), resultsByItemCode.keySet());
         LabResultMsg ncvs = resultsByItemCode.get("NCVS");
         LabResultMsg ncvp = resultsByItemCode.get("NCVP");
         LabResultMsg ncvl = resultsByItemCode.get("NCVL");
 
-        Assertions.assertTrue(ncvs.getNumericValue().isUnknown());
+        assertTrue(ncvs.getNumericValue().isUnknown());
         assertEquals("CTNS", ncvs.getStringValue());
 
-        Assertions.assertTrue(ncvp.getNumericValue().isUnknown());
+        assertTrue(ncvp.getNumericValue().isUnknown());
         assertEquals("NOT detected", ncvp.getStringValue());
 
-        Assertions.assertTrue(ncvl.getNumericValue().isUnknown());
+        assertTrue(ncvl.getNumericValue().isUnknown());
         // why are the leading spaces on each repeat (line) being trimmed?
         assertEquals(new StringBuilder()
                 .append("Please note that this test was performed using\n")
@@ -74,5 +79,24 @@ public class TestWinPathLabOruR01 extends TestHl7MessageStream {
                 .append("This new assay is currently not UKAS accredited,\n")
                 .append("but is internally verified. UKAS extension\n")
                 .append("to scope to include this has been submitted.").toString(), ncvl.getStringValue());
+    }
+
+
+    /**
+     * Test that numeric value, units, range are all set in simple calse
+     */
+    @Test
+    public void testNumericSimplePath() {
+        LabOrderMsg msg = processLab("LabOrders/oru_ro1_numeric.txt", 0);
+        List<LabResultMsg> labResultMsgs = msg.getLabResultMsgs();
+        assertEquals(4, labResultMsgs.size());
+        Map<String, LabResultMsg> resultsByItemCode = getResultsByItemCode(labResultMsgs);
+        assertEquals(new HashSet<>(Arrays.asList("ALP", "ALB", "GRE", "ALBR")), resultsByItemCode.keySet());
+        LabResultMsg result = resultsByItemCode.get("ALP");
+        assertEquals( InterchangeValue.buildFromHl7(104.0), result.getNumericValue());
+        assertEquals(InterchangeValue.buildFromHl7("IU/L"), result.getUnits());
+        assertEquals(InterchangeValue.buildFromHl7(104.0), result.getReferenceHigh());
+        assertEquals(InterchangeValue.buildFromHl7(35.0), result.getReferenceLow());
+
     }
 }
