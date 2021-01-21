703c443f65825b19365c2b6e1f0e40e03246760a
Stef Piatek <s.piatek@ucl.ac.uk>
2021-01-21 11:05:11 +0000

    test

--- hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestWinPathLabOruR01.java
+++ hl7-reader/src/test/java/uk/ac/ucl/rits/inform/datasources/ids/TestWinPathLabOruR01.java
@@ -1,8 +1,9 @@
 package uk.ac.ucl.rits.inform.datasources.ids;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import uk.ac.ucl.rits.inform.interchange.InterchangeValue;
+import uk.ac.ucl.rits.inform.interchange.lab.LabOrderMsg;
+import uk.ac.ucl.rits.inform.interchange.lab.LabResultMsg;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -10,16 +11,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import uk.ac.ucl.rits.inform.interchange.InterchangeValue;
-import uk.ac.ucl.rits.inform.interchange.lab.LabOrderMsg;
-import uk.ac.ucl.rits.inform.interchange.lab.LabResultMsg;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Test ORU RO1 from Winpath
- *
  * @author Stef Piatek
  */
 public class TestWinPathLabOruR01 extends TestHl7MessageStream {
@@ -45,7 +42,7 @@ public class TestWinPathLabOruR01 extends TestHl7MessageStream {
      * Test battery code and description are correctly parsed.
      */
     @Test
-    public void testTestCodes()  {
+    public void testTestCodes() {
         LabOrderMsg msg = processLab("LabOrders/covid19test.txt", 0);
         assertEquals("NCOV", msg.getTestBatteryLocalCode());
         assertEquals("COVID19 PCR", msg.getTestBatteryLocalDescription());
@@ -89,14 +86,50 @@ public class TestWinPathLabOruR01 extends TestHl7MessageStream {
     public void testNumericSimplePath() {
         LabOrderMsg msg = processLab("LabOrders/oru_ro1_numeric.txt", 0);
         List<LabResultMsg> labResultMsgs = msg.getLabResultMsgs();
-        assertEquals(4, labResultMsgs.size());
         Map<String, LabResultMsg> resultsByItemCode = getResultsByItemCode(labResultMsgs);
-        assertEquals(new HashSet<>(Arrays.asList("ALP", "ALB", "GRE", "ALBR")), resultsByItemCode.keySet());
         LabResultMsg result = resultsByItemCode.get("ALP");
-        assertEquals( InterchangeValue.buildFromHl7(104.0), result.getNumericValue());
+        assertEquals(InterchangeValue.buildFromHl7(104.0), result.getNumericValue());
         assertEquals(InterchangeValue.buildFromHl7("IU/L"), result.getUnits());
         assertEquals(InterchangeValue.buildFromHl7(104.0), result.getReferenceHigh());
         assertEquals(InterchangeValue.buildFromHl7(35.0), result.getReferenceLow());
+    }
 
+    /**
+     * Test that less than value sets the result operator and numeric value correctly
+     */
+    @Test
+    public void testResultOperatorLessThan() {
+        LabOrderMsg msg = processLab("LabOrders/oru_ro1_numeric.txt", 0);
+        List<LabResultMsg> labResultMsgs = msg.getLabResultMsgs();
+        Map<String, LabResultMsg> resultsByItemCode = getResultsByItemCode(labResultMsgs);
+        LabResultMsg result = resultsByItemCode.get("<VAL");
+        assertEquals("<", result.getResultOperator());
+        assertEquals(InterchangeValue.buildFromHl7(7.0), result.getNumericValue());
+    }
+
+    /**
+     * Test that greater than value sets the result operator and numeric value correctly
+     */
+    @Test
+    public void testResultOperatorGreaterThan() {
+        LabOrderMsg msg = processLab("LabOrders/oru_ro1_numeric.txt", 0);
+        List<LabResultMsg> labResultMsgs = msg.getLabResultMsgs();
+        Map<String, LabResultMsg> resultsByItemCode = getResultsByItemCode(labResultMsgs);
+        LabResultMsg result = resultsByItemCode.get(">VAL");
+        assertEquals(">", result.getResultOperator());
+        assertEquals(InterchangeValue.buildFromHl7(7.0), result.getNumericValue());
+    }
+
+    /**
+     * Test unknown result operator should still build, but with no known numeric result and the string value giving the original value
+     */
+    @Test
+    public void testUnknownResultOperator() {
+        LabOrderMsg msg = processLab("LabOrders/oru_ro1_numeric.txt", 0);
+        List<LabResultMsg> labResultMsgs = msg.getLabResultMsgs();
+        Map<String, LabResultMsg> resultsByItemCode = getResultsByItemCode(labResultMsgs);
+        LabResultMsg result = resultsByItemCode.get("UNKONWN_OPERATOR");
+        assertTrue(result.getNumericValue().isUnknown());
+        assertEquals("?7", result.getStringValue());
     }
 }
