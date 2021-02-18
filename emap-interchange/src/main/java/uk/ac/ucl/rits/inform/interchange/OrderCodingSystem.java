56c3a24b8c8ba83a8afd033a58521f822325611a
Stef Piatek <s.piatek@ucl.ac.uk>
2021-02-18 15:37:36 +0000

    Parse orders and results to allow for copath and bloods

---
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/OrderCodingSystem.java
@@ -0,0 +1,31 @@
+package uk.ac.ucl.rits.inform.interchange;
+
+/**
+ * Coding system for Orders (and results).
+ */
+public enum OrderCodingSystem {
+    /**
+     * WinPath labs.
+     */
+    WIN_PATH,
+    /**
+     * Bio connect POC Labs.
+     */
+    BIO_CONNECT,
+    /**
+     * CoPath Labs (including CoPathPlus).
+     */
+    CO_PATH,
+    /**
+     * Bank Manager Labs (Profiles).
+     */
+    BANK_MANAGER,
+    /**
+     * Bank manager Blood Products.
+     */
+    BLOOD_PRODUCTS,
+    /**
+     * Unknown type.
+     */
+    UNKNOWN
+}
