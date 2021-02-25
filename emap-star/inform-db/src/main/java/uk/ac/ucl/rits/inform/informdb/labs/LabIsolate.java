c8431c67b91586c9609e91cd9b8f707310c75442
Stef Piatek <s.piatek@ucl.ac.uk>
2021-02-25 10:23:49 +0000

    Create specific isolate interchange format

--- emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/labs/LabIsolate.java
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/labs/LabIsolate.java
@@ -13,6 +13,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Isolates identified from culture.
