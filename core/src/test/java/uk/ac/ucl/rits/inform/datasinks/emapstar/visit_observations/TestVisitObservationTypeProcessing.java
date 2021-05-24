5dbd0a77bf267666e9594e43dbe77a636c63a89b
Stef Piatek <s.piatek@ucl.ac.uk>
2021-05-24 15:51:21 +0100

    Test for metadata not updating from older message

--- core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/visit_observations/TestVisitObservationTypeProcessing.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/visit_observations/TestVisitObservationTypeProcessing.java
@@ -16,9 +16,10 @@ import java.time.Instant;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestVisitObservationTypeProcessing extends MessageProcessingBase {
-    List<FlowsheetMetadata> flowsheetMetadata;
+    FlowsheetMetadata flowsheetMetadata;
 
     @Autowired
     VisitObservationTypeRepository visitObservationTypeRepository;
@@ -27,6 +28,7 @@ public class TestVisitObservationTypeProcessing extends MessageProcessingBase {
     private static final String CABOODLE_APPLICATION = "caboodle";
     private static final String FLOWSHEET = "flowsheet";
     private static final String FLOWSHEET_ID = "5";
+    private static final Instant OLDER_TIME = Instant.parse("1991-01-01T00:00:00Z");
     private static final Instant LATER_TIME = Instant.parse("2021-01-01T00:00:00Z");
 
 
@@ -40,7 +42,7 @@ public class TestVisitObservationTypeProcessing extends MessageProcessingBase {
 
     @BeforeEach
     void setup() throws IOException {
-        flowsheetMetadata = messageFactory.getFlowsheetMetadata("flowsheet_metadata.yaml");
+        flowsheetMetadata = messageFactory.getFlowsheetMetadata("flowsheet_metadata.yaml").get(0);
     }
 
     /**
@@ -51,18 +53,24 @@ public class TestVisitObservationTypeProcessing extends MessageProcessingBase {
      */
     @Test
     void testEntityCreated() throws EmapOperationMessageProcessingException {
-        processSingleMessage(flowsheetMetadata.get(0));
+        processSingleMessage(flowsheetMetadata);
         assertEquals(1, visitObservationTypeRepository.count());
 
         VisitObservationType type = visitObservationTypeRepository.findAll().iterator().next();
-        assertMetadataFields(type, flowsheetMetadata.get(0).getLastUpdatedInstant());
+        assertMetadataFields(type, flowsheetMetadata.getLastUpdatedInstant());
     }
 
+    /**
+     * Given flowsheet visit observation type already exists
+     * When a new metadata message is processed
+     * Non-minimal fields should be updated
+     * @throws EmapOperationMessageProcessingException shouldn't happen
+     */
     @Test
     @Sql("/populate_db.sql")
     void testFieldsUpdated() throws EmapOperationMessageProcessingException {
         // Set existing data to be a flowsheet which exists already
-        FlowsheetMetadata metadata = flowsheetMetadata.get(0);
+        FlowsheetMetadata metadata = flowsheetMetadata;
         metadata.setLastUpdatedInstant(LATER_TIME);
         metadata.setFlowsheetRowEpicId(FLOWSHEET_ID);
         processSingleMessage(metadata);
@@ -73,6 +81,32 @@ public class TestVisitObservationTypeProcessing extends MessageProcessingBase {
                 .orElseThrow();
 
         assertMetadataFields(type, LATER_TIME);
+    }
+
+    /**
+     * Given flowsheet visit observation type already exists
+     * When an older metadata message is processed
+     * Non-minimal fields should not be changed
+     * @throws EmapOperationMessageProcessingException shouldn't happen
+     */
+    @Test
+    @Sql("/populate_db.sql")
+    void testOldMessageDoesntUpdate() throws EmapOperationMessageProcessingException {
+        // Set existing data to be a flowsheet which exists already
+        FlowsheetMetadata metadata = flowsheetMetadata;
+        metadata.setLastUpdatedInstant(OLDER_TIME);
+        metadata.setFlowsheetRowEpicId(FLOWSHEET_ID);
+        processSingleMessage(metadata);
+
+
+        VisitObservationType type = visitObservationTypeRepository
+                .findByIdInApplicationAndSourceSystemAndSourceObservationType(FLOWSHEET_ID, CABOODLE_APPLICATION, FLOWSHEET)
+                .orElseThrow();
+
+        assertEquals("blood pressure", type.getName());
+        assertNull(type.getDisplayName());
+        assertNull(type.getDescription());
+        assertNull(type.getPrimaryDataType());
     }
 
 }
