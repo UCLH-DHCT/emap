da09648dd941ade9b8b56620869c08b31ecf049d
AnikaC-git <anika.cawthorn@gmail.com>
2021-05-27 17:00:59 +0100

    Creating repositories to hold information on consultation requests for patients.

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/ConsultRequestAuditRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/ConsultRequestAuditRepository.java
@@ -1,4 +1,13 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 
-public class ConsultRequestAuditRepository {
+
+import org.springframework.data.repository.CrudRepository;
+import uk.ac.ucl.rits.inform.informdb.consults.ConsultRequestAudit;
+
+/**
+ * Consultation request audit repository.
+ * @author Anika Cawthorn
+ */
+public interface ConsultRequestAuditRepository extends CrudRepository<ConsultRequestAudit, Long> {
 }
+
