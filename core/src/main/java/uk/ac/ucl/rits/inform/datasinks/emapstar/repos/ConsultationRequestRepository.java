55170b030bf1a5a9c3d6f903a6f7a9b13e57b7f0
AnikaC-git <anika.cawthorn@gmail.com>
2021-06-07 12:18:14 +0100

    Progressing consultation request messages processing, but not working at the moment. Only committing to be able to pull updates.

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/ConsultationRequestRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/ConsultationRequestRepository.java
@@ -3,16 +3,26 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.consults.ConsultationRequest;
 import uk.ac.ucl.rits.inform.informdb.consults.ConsultationRequestType;
+import uk.ac.ucl.rits.inform.informdb.identity.HospitalVisit;
 import uk.ac.ucl.rits.inform.informdb.identity.Mrn;
 
 import java.util.Optional;
+import java.time.Instant;
 
 /**
  * Consultation request repository.
  * @author: Anika Cawthorn
  */
 public interface ConsultationRequestRepository extends CrudRepository<ConsultationRequest, Long> {
-    Optional<ConsultationRequest> findByMrnIdAndConsultRequestTypeId(
-            Mrn mrn, ConsultationRequestType consultationRequestType
+    /**
+     * Find consultation request by patient, hospital visit, consultation type and request date.
+     * @param mrn                       Patient identifier
+     * @param visit                     Hospital visit
+     * @param consultationRequestType   Consultancy type
+     * @param requestedDateTime         Date when consultant was requested
+     * @return ConsultationRequest that relates to identifiers
+     */
+    Optional<ConsultationRequest> findByMrnAndHospitalVisitAndConsultationRequestTypeNameAndRequestedDateTime(
+            Mrn mrn, HospitalVisit visit, ConsultationRequestType consultationRequestType, Instant requestedDateTime
     );
 }
