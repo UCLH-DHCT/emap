f92344a69bff7f744a4311831c42165e4a51b388
Stef Piatek <s.piatek@ucl.ac.uk>
2021-06-21 17:40:33 +0100

    Delete older hl7 infections when processing hoover infections

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/PatientConditionRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/PatientConditionRepository.java
@@ -1,9 +1,9 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 
 import org.springframework.data.repository.CrudRepository;
-import uk.ac.ucl.rits.inform.informdb.identity.Mrn;
-import uk.ac.ucl.rits.inform.informdb.conditions.PatientCondition;
 import uk.ac.ucl.rits.inform.informdb.conditions.ConditionType;
+import uk.ac.ucl.rits.inform.informdb.conditions.PatientCondition;
+import uk.ac.ucl.rits.inform.informdb.identity.Mrn;
 
 import java.time.Instant;
 import java.util.Optional;
@@ -15,9 +15,9 @@ import java.util.Optional;
 public interface PatientConditionRepository extends CrudRepository<PatientCondition, Long> {
     /**
      * Get patient state, should be unique.
-     * @param mrn              MRN entity
+     * @param mrn           MRN entity
      * @param conditionType type of state
-     * @param addedDateTime    added date time
+     * @param addedDateTime added date time
      * @return possible patient state
      */
     Optional<PatientCondition> findByMrnIdAndConditionTypeIdAndAddedDateTime(
@@ -25,15 +25,12 @@ public interface PatientConditionRepository extends CrudRepository<PatientCondit
     );
 
     /**
-     * Get patient state, should be unique.
-     * @param mrn              MRN entity
-     * @param stateName        name of the state
-     * @param addedDateTime    added date time
-     * @return possible patient state
+     * Delete all that is valid before an instant for types.
+     * @param untilDateTime instant to delete all messages before
+     * @param conditionType      data type
      */
-    Optional<PatientCondition> findByMrnIdAndConditionTypeIdNameAndAddedDateTime(
-            Mrn mrn, String stateName, Instant addedDateTime
-    );
+    void deleteAllByValidFromBeforeAndInternalIdIsNullAndConditionTypeId(Instant untilDateTime, ConditionType conditionType);
+
 
     /**
      * For testing, shortcut without requiring entities to be passed.
@@ -45,4 +42,6 @@ public interface PatientConditionRepository extends CrudRepository<PatientCondit
     Optional<PatientCondition> findByMrnIdMrnAndConditionTypeIdNameAndAddedDateTime(
             String mrn, String stateName, Instant addedDateTime
     );
+
+    Optional<PatientCondition> findByConditionTypeIdAndInternalId(ConditionType conditionType, Long internalId);
 }
