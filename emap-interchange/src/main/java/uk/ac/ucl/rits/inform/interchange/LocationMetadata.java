bf1a62550eb91fef0a1e442a7ab7aad22450973d
Jeremy Stein <j.stein@ucl.ac.uk>
2021-06-25 18:01:30 +0100

    Make record status/state columns consistent: always use the descriptive string value, eg. "Active" rather than 0/NULL.

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/LocationMetadata.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/LocationMetadata.java
@@ -22,15 +22,48 @@ public class LocationMetadata extends EmapOperationMessage {
     private String epicDepartmentName;
     private Instant epicDepartmentMpiFromDate;
     private Instant epicDepartmentMpiToDate;
-    private Long departmentRecordStatusId;
+    /**
+     * The department record status.
+     * Possible values in ZC_PBA_REC_STAT.
+     * https://datahandbook.epic.com/ClarityDictionary/Details?tblName=CLARITY_DEP
+     */
+    private String departmentRecordStatus;
     private Long epicRoomId;
+
+    /**
+     * Room record state.
+     * Possible values in ZC_PBA_REC_STAT.
+     * https://datahandbook.epic.com/ClarityDictionary/Details?tblName=CLARITY_ROM
+     */
     private String roomRecordState;
     private String roomName;
     private String roomAssociatedPoolBedName;
+
+    /**
+     * CLARITY_ROM.BED_POOL_CENSUS_YN Indicates whether the pool beds for the room
+     * record should be included in bed census reports.
+     * https://datahandbook.epic.com/ClarityDictionary/Details?tblName=CLARITY_ROM
+     */
     private Boolean roomPoolBedIsInCensus;
     private Long epicBedId;
+
+    /**
+     * The bed record state.
+     * Possible values in ZC_PBA_REC_STAT.
+     * <p>
+     */
     private String bedRecordState;
+
+    /**
+     * Whether the bed record should be
+     * included in bed census reports.
+     * <p>
+     */
     private Boolean bedIsInCensus;
+
+    /**
+     * Pool beds are transient beds.
+     */
     private Boolean isPoolBed;
 
     @Override
