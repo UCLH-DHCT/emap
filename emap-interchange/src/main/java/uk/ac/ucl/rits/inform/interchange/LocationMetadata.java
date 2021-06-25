2ecceb4d3de605964023ca7ce2c4ad4b59602bac
Jeremy Stein <j.stein@ucl.ac.uk>
2021-06-25 18:37:02 +0100

    Add Roma's explanation of what a census vs. non-census bed is.

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/LocationMetadata.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/LocationMetadata.java
@@ -58,6 +58,21 @@ public class LocationMetadata extends EmapOperationMessage {
      * CLARITY_BED.CENSUS_INCLUSN_YN Indicates whether the bed record should be
      * included in bed census reports.
      * https://datahandbook.epic.com/ClarityDictionary/Details?tblName=CLARITY_BED
+     *
+     * Roma says: Census beds are beds that count as real beds
+     * where you stay (and we get ADT messages for), rather than non-census beds
+     * which are transient and don't trigger ADT messages (at the moment, and when
+     * they do, it's a different type).
+     * E.g. Say you have a patient in ED. They have a location which is the bed in
+     * ED they are assigned to. Let's say that it is ED-1. Then they decide they
+     * want to take the patient to get an X-RAY. They want to record that the
+     * patient is going to X-RAY Room 1 but without freeing up bed ED-1 (so it's
+     * still there for the patient to come back to). So ED-1 is the census bed
+     * (where the patient is considered to be), while X-RAY Room 1 is the non-census
+     * location (where they are at the moment, but moving here doesn't move them out
+     * of their current census location, because it's not freeing up space, but you
+     * do need to track that the non-census location has a person there and so is in
+     * use.)
      */
     private Boolean bedIsInCensus;
 
