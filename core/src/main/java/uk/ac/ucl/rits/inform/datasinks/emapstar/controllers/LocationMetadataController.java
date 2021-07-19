919d654ade3508463e26476f6a82644e41b2ced5
Stef Piatek <s.piatek@ucl.ac.uk>
2021-07-19 14:39:31 +0100

    Create location metadata controller

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/LocationMetadataController.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/LocationMetadataController.java
@@ -3,6 +3,13 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.BedPoolRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.BedRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.BedStateRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.DepartmentAuditRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.DepartmentRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.RoomRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations.RoomStateRepository;
 
 /**
  * Adds or updates location metadata (department, room, bed, etc.).
@@ -12,9 +19,26 @@ import org.springframework.stereotype.Component;
 public class LocationMetadataController {
     private static final Logger logger = LoggerFactory.getLogger(LocationMetadataController.class);
 
+    private final DepartmentRepository departmentRepo;
+    private final DepartmentAuditRepository departmentAuditRepo;
+    private final RoomRepository roomRepo;
+    private final RoomStateRepository roomStateRepo;
+    private final BedPoolRepository bedPoolRepo;
+    private final BedRepository bedRepo;
+    private final BedStateRepository bedStateRepo;
 
 
-    public LocationMetadataController() {
+    public LocationMetadataController(
+            DepartmentRepository departmentRepo, DepartmentAuditRepository departmentAuditRepo,
+            RoomRepository roomRepo, RoomStateRepository roomStateRepo,
+            BedPoolRepository bedPoolRepo, BedRepository bedRepo, BedStateRepository bedStateRepo) {
+        this.departmentRepo = departmentRepo;
+        this.departmentAuditRepo = departmentAuditRepo;
+        this.roomRepo = roomRepo;
+        this.roomStateRepo = roomStateRepo;
+        this.bedPoolRepo = bedPoolRepo;
+        this.bedRepo = bedRepo;
+        this.bedStateRepo = bedStateRepo;
     }
 
 }
--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/locations/BedRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/locations/BedRepository.java
@@ -2,7 +2,6 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations;
 
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.movement.Bed;
-import uk.ac.ucl.rits.inform.informdb.movement.Room;
 
 /**
  * Bed repository.
