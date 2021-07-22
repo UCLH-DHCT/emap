06bb27fd641741326b9704aecb68d757d32f4aae
Stef Piatek <s.piatek@ucl.ac.uk>
2021-07-22 13:05:42 +0100

    Process bed states

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/locations/BedStateRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/locations/BedStateRepository.java
@@ -1,8 +1,10 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.locations;
 
 import org.springframework.data.repository.CrudRepository;
+import uk.ac.ucl.rits.inform.informdb.movement.Bed;
 import uk.ac.ucl.rits.inform.informdb.movement.BedState;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -10,10 +12,13 @@ import java.util.Optional;
  * @author Stef Piatek
  */
 public interface BedStateRepository extends CrudRepository<BedState, Long> {
+    List<BedState> findAllByBedIdOrderByValidFromDesc(Bed bed);
+
     /**
      * For testing.
      * @param csn CSN to find
      * @return potential bed state
      */
     Optional<BedState> findByCsn(long csn);
+
 }
