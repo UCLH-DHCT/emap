d86f5fe5703bf583ec459eb543d37fe044f3d8ed
Anika C <anika.cawthorn@gmail.com>
2021-07-23 16:11:16 +0100

    Changing functionality to search for existing questions as needed by entity creation and tests.

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/QuestionAuditRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/QuestionAuditRepository.java
@@ -1,7 +1,7 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 
 import org.springframework.data.repository.CrudRepository;
-import uk.ac.ucl.rits.inform.informdb.QuestionAudit;
+import uk.ac.ucl.rits.inform.informdb.questions.QuestionAudit;
 
 /**
  * Question audit repository.
--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/QuestionRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/QuestionRepository.java
@@ -6,26 +6,23 @@ import uk.ac.ucl.rits.inform.informdb.questions.Question;
 import java.util.Optional;
 
 /**
- * Question repository to record all questions independent to which data type they belong to. Each question is linked
- * to a parent table identifier and parent table id.
+ * Question repository to record all questions independent to which data type they belong to.
  * @author Stef Piatek
  * @author Anika Cawthorn
  */
 public interface QuestionRepository extends CrudRepository<Question, Long> {
     /**
-     * Find specific question based on its string content and the identifier in the parent table.
+     * Find a specific question based on question object.
      * @param question
-     * @param parentTableId
-     * @return
+     * @return question as defined by question content
      */
-    Optional<Question> findByQuestionAndParentTableIdentifier(Question question, long parentTableId);
+    Optional<Question> findByQuestion(Question question);
 
     /**
      * Find question based on its string content and the identifier in the parent table this question is related to,
      * e.g. a lab sample identifier. This method is for testing purposes only.
      * @param question
-     * @param parentTableId
-     * @return
+     * @return question as defined by question string
      */
-    Optional<Question> findByQuestionAndParentTableIdentifier(String question, String parentTableId);
+    Optional<Question> findByQuestion(String question);
 }
