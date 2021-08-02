32e4c4bdbffa8b79b286b1e272f52e3012b56df7
Stef Piatek <s.piatek@ucl.ac.uk>
2021-08-02 15:29:08 +0100

    Create Bed Facility

---
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/movement/BedFacility.java
@@ -0,0 +1,42 @@
+package uk.ac.ucl.rits.inform.informdb.movement;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import uk.ac.ucl.rits.inform.informdb.TemporalFrom;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+
+@SuppressWarnings("serial")
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+public class BedFacility implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bedFacilityId;
+
+    @ManyToOne
+    @JoinColumn(name = "bedStateId", nullable = false)
+    private BedState bedStateId;
+
+    /**
+     * Type of facility available at bed.
+     * e.g. Cot, oxygen, ICU...
+     */
+    private String type;
+
+
+    public BedFacility(BedState bedStateId, String type, TemporalFrom temporalFrom) {
+        this.bedStateId = bedStateId;
+        this.type = type;
+    }
+}
