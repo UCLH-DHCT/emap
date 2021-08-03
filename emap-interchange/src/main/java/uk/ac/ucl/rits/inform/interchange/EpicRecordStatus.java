fa5092379433eb52673ccf700a07f1feacdd42d2
Stef Piatek <s.piatek@ucl.ac.uk>
2021-08-03 13:42:13 +0100

    Use EpicRecordStatus enum for location metadata

---
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/EpicRecordStatus.java
@@ -0,0 +1,61 @@
+package uk.ac.ucl.rits.inform.interchange;
+
+import uk.ac.ucl.rits.inform.interchange.adt.PatientClass;
+
+/**
+ * Record Status values from EPIC.
+ */
+public enum EpicRecordStatus {
+    /**
+     * Active.
+     */
+    ACTIVE("Active"),
+    /**
+     * Inactive.
+     */
+    INACTIVE("Inactive"),
+    /**
+     * Deleted.
+     */
+    DELETED("Deleted"),
+    /**
+     * Inactive and Deleted.
+     */
+    INACTIVE_AND_DELETED("Inactive and Deleted"),
+    /**
+     * Hidden.
+     */
+    HIDDEN("Hidden"),
+    /**
+     * Inactive and Hidden.
+     */
+    INACTIVE_AND_HIDDEN("Inactive and Hidden"),
+    /**
+     * Deleted and Hidden.
+     */
+    DELETED_AND_HIDDEN("Deleted and Hidden"),
+    /**
+     * Inactive Deleted and Hidden.
+     */
+    INACTIVE_DELETED_AND_HIDDEN("Inactive Deleted and Hidden");
+
+    private final String name;
+
+    EpicRecordStatus(String name) {
+        this.name = name;
+    }
+
+    public static EpicRecordStatus findByName(String name) {
+        for (EpicRecordStatus recordStatus : values()) {
+            if (recordStatus.name.equals(name)) {
+                return recordStatus;
+            }
+        }
+        throw new IllegalArgumentException(String.format("EpicRecordStatus %s is not a known type", name));
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
