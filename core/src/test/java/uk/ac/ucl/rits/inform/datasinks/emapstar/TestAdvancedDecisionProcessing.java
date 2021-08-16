aee68138c3adc546b9a99c3a7a2d95e069acbd73
Anika C <anika.cawthorn@gmail.com>
2021-08-16 12:40:28 +0100

    Adding scaffolding for advanced decision messages.

---
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/TestAdvancedDecisionProcessing.java
@@ -0,0 +1,8 @@
+package uk.ac.ucl.rits.inform.datasinks.emapstar;
+
+/**
+ * Test cases to assert correct processing of Advanced Decision Message processing.
+ * @author Anika Cawthorn
+ */
+public class TestAdvancedDecisionProcessing extends MessageProcessingBase {
+}
