06471e21c5e0d1d1abb466cf6b6eecf36adc2e3a
Anika C <anika.cawthorn@gmail.com>
2021-08-17 13:32:40 +0100

    More scaffolding for advanced decision messages with some additional repos and adding in advanced decision type after re-reading Github issue

---
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/decisions/AdvancedDecisionType.java
@@ -0,0 +1,71 @@
+package uk.ac.ucl.rits.inform.informdb.decisions;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import uk.ac.ucl.rits.inform.informdb.TemporalCore;
+import uk.ac.ucl.rits.inform.informdb.annotation.AuditTable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AuditTable
+public class AdvancedDecisionType extends TemporalCore<AdvancedDecisionType, AdvancedDecisionTypeAudit> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long advancedDecisionTypeId;
+
+    /**
+     * problem list or patient infection.
+     */
+    @Column(nullable = false)
+    private String name;
+
+    /**
+     * Code used within EPIC for condition.
+     */
+    @Column(nullable = false)
+    private String careCode;
+
+    /**
+     * Minimal information constructor.
+     * @param name          Type of patient state type; either patient infection or problem list
+     * @param careCode      EPIC code of the patient state type
+     * @param validFrom     Timestamp from which information valid from
+     * @param storedFrom    Timestamp from which information stored from
+     */
+    public AdvancedDecisionType(String name, String careCode, Instant validFrom, Instant storedFrom) {
+        this.name = name;
+        this.careCode = careCode;
+        setValidFrom(validFrom);
+        setStoredFrom(storedFrom);
+    }
+
+    /**
+     * Build a new PatientStateType from an existing one.
+     * @param other existing PatientStateType
+     */
+    public AdvancedDecisionType(AdvancedDecisionType other) {
+        super(other);
+        this.name = other.name;
+        this.careCode = other.careCode;
+    }
+
+    @Override
+    public AdvancedDecisionType copy() {
+        return new AdvancedDecisionType(this);
+    }
+
+    @Override
+    public AdvancedDecisionTypeAudit createAuditEntity(Instant validUntil, Instant storedUntil) {
+        return new AdvancedDecisionTypeAudit(this, validUntil, storedUntil);
+    }
+}
