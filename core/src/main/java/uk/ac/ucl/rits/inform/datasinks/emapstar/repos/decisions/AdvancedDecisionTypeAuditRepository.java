6a1828aa3179b7ee5b7326120becdd8a41f12b20
Anika C <anika.cawthorn@gmail.com>
2021-09-02 18:56:59 +0100

    Adding in default structure, but no search functionality yet.

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/decisions/AdvancedDecisionTypeAuditRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/decisions/AdvancedDecisionTypeAuditRepository.java
@@ -1,4 +1,11 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.decisions;
 
-public interface AdvancedDecisionTypeAuditRepository {
+import org.springframework.data.repository.CrudRepository;
+import uk.ac.ucl.rits.inform.informdb.decisions.AdvancedDecisionTypeAudit;
+
+/**
+ * Holds information on changes of types of advanced decisions.
+ * @author Anika Cawthorn
+ */
+public interface AdvancedDecisionTypeAuditRepository extends CrudRepository<AdvancedDecisionTypeAudit, Long> {
 }
