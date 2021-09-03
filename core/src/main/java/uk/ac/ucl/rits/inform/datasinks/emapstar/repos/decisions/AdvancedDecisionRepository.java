3b693b923e9196c689283d212e5b2aded1aca0ed
Anika C <anika.cawthorn@gmail.com>
2021-09-03 13:46:54 +0100

    Adding repository search functionality.

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/decisions/AdvancedDecisionRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/decisions/AdvancedDecisionRepository.java
@@ -3,10 +3,17 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.decisions;
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.decisions.AdvancedDecision;
 
+import java.util.Optional;
+
 /**
  * Repository to hold advanced decisions registered by patients.
  * @author Anika Cawthorn
  */
 public interface AdvancedDecisionRepository extends CrudRepository<AdvancedDecision, Long> {
-
+    /**
+     * Find advanced decision by unique identifier.
+     * @param advancedDecisionId   Advanced decision identifier.
+     * @return possible ConsultationRequest
+     */
+    Optional<AdvancedDecision> findByAdvancedDecisionNumber(Long advancedDecisionId);
 }
