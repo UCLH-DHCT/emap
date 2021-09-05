3365833037f4141cf7b4a7ab002ae5a5beeea40d
Anika C <anika.cawthorn@gmail.com>
2021-09-05 15:02:55 +0100

    Adding minimal creation test.

--- core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/TestAdvancedDecisionProcessing.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/TestAdvancedDecisionProcessing.java
@@ -1,10 +1,20 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.*;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.decisions.AdvancedDecisionRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.decisions.AdvancedDecisionTypeRepository;
+import uk.ac.ucl.rits.inform.informdb.identity.Mrn;
+import uk.ac.ucl.rits.inform.interchange.AdvancedDecisionMessage;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test cases to assert correct processing of Advanced Decision Messages.
@@ -13,9 +23,38 @@ import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException
 public class TestAdvancedDecisionProcessing extends MessageProcessingBase {
     @Autowired
     AdvancedDecisionRepository advancedDecisionRepo;
+    @Autowired
+    AdvancedDecisionTypeRepository advancedDecisionTypeRepo;
+    @Autowired
+    MrnRepository mrnRepository;
+    @Autowired
+    HospitalVisitRepository hospitalVisitRepository;
+    @Autowired
+    QuestionRepository questionRepository;
 
+    private AdvancedDecisionMessage minimalNoQuestions;
+    private AdvancedDecisionMessage minimalWithQuestions;
+    private AdvancedDecisionMessage closedAtDischarge;
+    private AdvancedDecisionMessage cancelled;
+
+    @BeforeEach
+    private void setUp() throws IOException {
+        minimalNoQuestions = messageFactory.getAdvancedDecision("new_no_questions.yaml");
+        minimalWithQuestions = messageFactory.getAdvancedDecision("new_with_questions.yaml");
+        closedAtDischarge = messageFactory.getAdvancedDecision("discharge_cancel.yaml");
+        cancelled = messageFactory.getAdvancedDecision("user_cancel.yaml");
+    }
+
+    /**
+     *  Given that no MRNS or hospital visits exist in the database
+     *  When a new AdvancedDecisionMessage arrives
+     *  Then a minimal HospitalVisit, Mrn and AdvancedDecision should be created
+     */
     @Test
     void testMinimalAdvancedDecisionCreated() throws EmapOperationMessageProcessingException {
-        System.out.println("tst");
+        processSingleMessage(minimalNoQuestions);
+
+        List<Mrn> mrns = getAllMrns();
+        assertEquals(1, mrns.size());
     }
 }
