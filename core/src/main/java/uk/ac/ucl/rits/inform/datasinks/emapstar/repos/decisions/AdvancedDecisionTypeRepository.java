444e4840ccfabeae77b7607cfa08e79f9fe85cf4
Anika C <anika.cawthorn@gmail.com>
2021-09-09 17:11:01 +0100

    Renaming parameter to what it is

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/decisions/AdvancedDecisionTypeRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/decisions/AdvancedDecisionTypeRepository.java
@@ -6,14 +6,14 @@ import uk.ac.ucl.rits.inform.informdb.decisions.AdvancedDecisionType;
 import java.util.Optional;
 
 /**
- * Holds information on advanced decision types.
+ * Provides functionality to search advanced decision type table.
  * @author Anika Cawthorn
  */
 public interface AdvancedDecisionTypeRepository extends CrudRepository<AdvancedDecisionType, Long> {
     /**
-     * Searches for a specific advanced decision type based on code provided.
-     * @param advancedDecisionType    Code of advanced decision type recorded for an advanced decision.
+     * Searches for a specific advanced decision type based on care code provided.
+     * @param advancedDecisionCareCode    Code of advanced decision type recorded for an advanced decision.
      * @return AdvancedDecisionType for code provided
      */
-    Optional<AdvancedDecisionType> findByCareCode(String advancedDecisionType);
+    Optional<AdvancedDecisionType> findByCareCode(String advancedDecisionCareCode);
 }
