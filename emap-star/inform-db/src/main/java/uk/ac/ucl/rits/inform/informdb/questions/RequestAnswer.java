0fe824501699228f7bc3154093f1529ebf25d596
Anika C <anika.cawthorn@gmail.com>
2021-09-23 09:47:32 +0100

    Removing summarised import statement as it causes checkstyle error

--- emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/questions/RequestAnswer.java
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/questions/RequestAnswer.java
@@ -6,7 +6,15 @@ import lombok.ToString;
 import uk.ac.ucl.rits.inform.informdb.TemporalCore;
 import uk.ac.ucl.rits.inform.informdb.annotation.AuditTable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.Instant;
 
 /**
