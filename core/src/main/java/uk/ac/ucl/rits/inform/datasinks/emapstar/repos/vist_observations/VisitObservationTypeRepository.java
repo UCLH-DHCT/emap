b3ff18479b5630fec66805b3c24ffab35ddca0ee
Anika C <anika.cawthorn@gmail.com>
2021-11-11 14:05:28 +0000

    Changing repository search functionality according to new way of handling metadata

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/vist_observations/VisitObservationTypeRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/vist_observations/VisitObservationTypeRepository.java
@@ -1,33 +1,49 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.vist_observations;
 
 import org.springframework.data.repository.CrudRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.exceptions.RequiredDataMissingException;
 import uk.ac.ucl.rits.inform.informdb.visit_recordings.VisitObservationType;
 
 import java.util.Optional;
 
 /**
  * Visit Observation repository.
+ *
  * @author Stef Piatek
  */
 public interface VisitObservationTypeRepository extends CrudRepository<VisitObservationType, Long> {
-
     /**
      * Query visit observation type by required fields.
-     * @param id                    Id within the source application
-     * @param sourceSystem          source system
-     * @param sourceObservationType type of observation from the source system
-     * @return optional visit observation type
-     */
-    Optional<VisitObservationType> findByIdInApplicationAndSourceSystemAndSourceObservationType(
-            String id, String sourceSystem, String sourceObservationType);
-
-    /**
-     * Query visit observation type by required fields.
-     * @param interfaceId       Identifier for visit observation type in EPIC
-     * @param idInApplication   Hospital flowsheet identifier
+     *
+     * @param interfaceId     Identifier for visit observation type in EPIC
+     * @param idInApplication Hospital flowsheet identifier
      * @param observationType type of observation from the source system
      * @return optional visit observation type
+     * @throws RequiredDataMissingException Neither identifiers provided
      */
-    Optional<VisitObservationType> findByInterfaceIdAndIdInApplicationAndSourceObservationType(
-            String interfaceId, String idInApplication, String observationType);
+    default Optional<VisitObservationType> findByInterfaceIdAndIdInApplicationAndSourceObservationType(
+            String interfaceId, String idInApplication, String observationType) throws RequiredDataMissingException {
+        if (interfaceId == null && idInApplication == null) {
+            throw new RequiredDataMissingException("Both the interface identifier and the id in application can't be null");
+        }
+        if (idInApplication == null) {
+            return findByInterfaceIdAndSourceObservationType(interfaceId, observationType);
+        }
+
+        return findByIdInApplicationAndSourceObservationType(idInApplication, observationType);
+    }
+
+    /**
+     * @param interfaceId           identifier
+     * @param sourceObservationType obs type
+     * @return VisitObservationType
+     */
+    Optional<VisitObservationType> findByInterfaceIdAndSourceObservationType(String interfaceId, String sourceObservationType);
+
+    /**
+     * @param idInApplication       sdfa
+     * @param sourceObservationType obs type
+     * @return VisitObservationType
+     */
+    Optional<VisitObservationType> findByIdInApplicationAndSourceObservationType(String idInApplication, String sourceObservationType);
 }
