1d2689871665c5e1b01f6b30357e9ae22a8fa1b8
Stef Piatek <s.piatek@ucl.ac.uk>
2022-02-23 16:11:44 +0000

    Create ClimbSequence Repositories

---
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/labs/ClimbSequenceRepository.java
@@ -0,0 +1,12 @@
+package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.labs;
+
+import org.springframework.data.repository.CrudRepository;
+import uk.ac.ucl.rits.inform.informdb.labs.ClimbSequence;
+
+/**
+ * ClimbSequence repository.
+ * @author Stef Piatek
+ */
+public interface ClimbSequenceRepository extends CrudRepository<ClimbSequence, Long> {
+
+}
