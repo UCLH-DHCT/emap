563d39f84f6b96a836ea3a3b8ba464b4f4856ea6
Stef Piatek <s.piatek@ucl.ac.uk>
2022-02-24 10:50:13 +0000

    Allow processing of community samples

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/labs/ClimbSequenceRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/labs/ClimbSequenceRepository.java
@@ -2,11 +2,14 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.labs;
 
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.labs.ClimbSequence;
+import uk.ac.ucl.rits.inform.informdb.labs.LabSample;
+
+import java.util.Optional;
 
 /**
  * ClimbSequence repository.
  * @author Stef Piatek
  */
 public interface ClimbSequenceRepository extends CrudRepository<ClimbSequence, Long> {
-
+    Optional<ClimbSequence> findByPheIdAndLabSampleId(String pheId, LabSample labSample);
 }
