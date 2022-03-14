34a692bf7b3fa4e4b7cb888f5bbede62d819b2bc
tom <tom.young@chem.ox.ac.uk>
2022-03-14 08:57:12 +0000

    Abstract class for a patient condition [skip actions]

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientConditionMessage.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientConditionMessage.java
@@ -1,71 +1,83 @@
 package uk.ac.ucl.rits.inform.interchange;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
 import java.time.Instant;
 import java.time.LocalDate;
 
 /**
- * Interface defining a patient condition message
+ * Abstract class defining a patient condition message, either a 'problem' (aka. problem list) or an infection
  * @author Tom Young
  */
-public interface PatientConditionMessage {
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
+public abstract class PatientConditionMessage extends EmapOperationMessage{
 
-    String getMrn();
-
-    InterchangeValue<String> getVisitNumber();
-
-    /**
-     * Code defining the infection or problem
-     */
-    String getCode();
+    private String mrn;
 
     /**
-     * Human-readable name of the condition
+     * Number of the hospital visit
      */
-    InterchangeValue<String> getName();
+    private InterchangeValue<String> visitNumber = InterchangeValue.unknown();
+
+    /**
+     * Infection or problem abbreviation.
+     */
+    private String conditionCode;
+
+    /**
+     * Human-readable condtion name.
+     */
+    private InterchangeValue<String> conditionName = InterchangeValue.unknown();
 
     /**
      * Time of the update or message carrying this information.
      */
-    Instant getUpdatedDateTime();
+    private Instant updatedDateTime;
 
     /**
-     * Unique Id for problem in EPIC.
+     * Unique Id for a condition in EPIC.
+     * If we can't get this added to the live HL7 interface when we should remove it.
      */
-    InterchangeValue<Long> getEpicId();
+    private InterchangeValue<Long> epicConditionId = InterchangeValue.unknown();
 
     /**
-     * Problem added at...
+     * Status of condition.
      */
-    Instant getAddedTime();
+    private InterchangeValue<String> status = InterchangeValue.unknown();
+
+    private InterchangeValue<String> comment = InterchangeValue.unknown();
 
     /**
-     * Problem resolved at...
+     * Condition added at...
      */
-    InterchangeValue<Instant> getResolvedTime();
+    private Instant addedTime;
 
     /**
-     * Onset of problem known at...
+     * Condition resolved at...
      */
-    InterchangeValue<LocalDate> getOnsetTime();
+    private InterchangeValue<Instant> resolvedTime = InterchangeValue.unknown();
 
     /**
-     * Status of the condition
+     * Onset of condition known at...
      */
-    InterchangeValue<String> getStatus();
+    private InterchangeValue<LocalDate> onsetTime = InterchangeValue.unknown();
 
     /**
-     * Effectively message type, i.e. whether to add, update or delete the patient problem
+     * Effectively message type, i.e. whether to add, update or delete problem list.
      */
-    String getAction();
+    private String action = "AD";
 
     /**
-     * Notes in relation to problem...
+     * Call back to the processor so it knows what type this object is (ie. double dispatch).
+     * @param processor the processor to call back to
+     * @throws EmapOperationMessageProcessingException if message cannot be processed
      */
-    InterchangeValue<String> getComment();
-
-
-    /**
-     * System which this message came from
-     */
-    String getSourceSystem();
+    abstract public void processMessage(EmapOperationMessageProcessor processor)
+            throws EmapOperationMessageProcessingException;
 }
--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientProblem.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientProblem.java
@@ -17,60 +17,8 @@ import java.time.LocalDate;
  * and they can change (be updated or deleted) over time.
  * @author Anika Cawthorn
  */
-@Data
-@EqualsAndHashCode(callSuper = true)
-@ToString(callSuper = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
-public class PatientProblem extends EmapOperationMessage implements Serializable, PatientConditionMessage{
-    private String mrn;
-    private InterchangeValue<String> visitNumber = InterchangeValue.unknown();
-    /**
-     * Problem code.
-     */
-    private String problemCode;
-    /**
-     * Human-readable problem name.
-     */
-    private InterchangeValue<String> problemName = InterchangeValue.unknown();
-
-    /**
-     * Time of the update or message carrying this information.
-     */
-    private Instant updatedDateTime;
-
-    /**
-     * Unique Id for problem in EPIC.
-     */
-    private InterchangeValue<Long> epicProblemId = InterchangeValue.unknown();
-    /**
-     * Problem added at...
-     */
-    private Instant problemAdded;
-
-    /**
-     * Problem resolved at...
-     */
-    private InterchangeValue<Instant> problemResolved = InterchangeValue.unknown();
-
-    /**
-     * Onset of problem known at...
-     */
-    private InterchangeValue<LocalDate> problemOnset = InterchangeValue.unknown();
-
-    /**
-     * Status of problem...
-     */
-    private InterchangeValue<String> status = InterchangeValue.unknown();
-
-    /**
-     * Effectively message type, i.e. whether to add, update or delete problem list.
-     */
-    private String action;
-
-    /**
-     * Notes in relation to problem...
-     */
-    private InterchangeValue<String> comment = InterchangeValue.unknown();
+public class PatientProblem extends PatientConditionMessage implements Serializable{
 
     /**
      * Call back to the processor, so it knows what type this object is (i.e. double dispatch).
@@ -80,35 +28,5 @@ public class PatientProblem extends EmapOperationMessage implements Serializable
     @Override
     public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
         processor.processMessage(this);
-    }
-
-    @Override
-    public String getCode() {
-        return problemCode;
-    }
-
-    @Override
-    public InterchangeValue<String> getName() {
-        return problemName;
-    }
-
-    @Override
-    public InterchangeValue<Long> getEpicId() {
-        return epicProblemId;
-    }
-
-    @Override
-    public Instant getAddedTime() {
-        return problemAdded;
-    }
-
-    @Override
-    public InterchangeValue<Instant> getResolvedTime() {
-        return problemResolved;
-    }
-
-    @Override
-    public InterchangeValue<LocalDate> getOnsetTime() {
-        return problemOnset;
     }
 }
