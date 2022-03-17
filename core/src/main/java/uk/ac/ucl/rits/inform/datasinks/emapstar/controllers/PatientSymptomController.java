b3b119e8361d07b2dcfe73d0155eb2d89fc61489
tom <tom.young@chem.ox.ac.uk>
2022-03-17 09:40:16 +0000

    Unbreak old tests

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/PatientSymptomController.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/PatientSymptomController.java
@@ -5,21 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.RowState;
-import uk.ac.ucl.rits.inform.datasinks.emapstar.exceptions.RequiredDataMissingException;
-import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.PatientSymptomAuditRepository;
-import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.PatientSymptomRepository;
-import uk.ac.ucl.rits.inform.informdb.conditions.PatientCondition;
-import uk.ac.ucl.rits.inform.informdb.identity.Mrn;
-import uk.ac.ucl.rits.inform.informdb.conditions.ConditionSymptom;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.ConditionSymptomAuditRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.ConditionSymptomRepository;
 import uk.ac.ucl.rits.inform.informdb.conditions.ConditionSymptomAudit;
+import uk.ac.ucl.rits.inform.informdb.conditions.PatientCondition;
+import uk.ac.ucl.rits.inform.informdb.conditions.ConditionSymptom;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
 import uk.ac.ucl.rits.inform.interchange.PatientAllergy;
 import uk.ac.ucl.rits.inform.interchange.PatientConditionMessage;
 
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -31,51 +27,72 @@ import java.util.Optional;
 @Component
 public class PatientSymptomController {
     private final Logger logger = LoggerFactory.getLogger(getClass());
-    private final PatientSymptomRepository patientSymptomRepo;
+    private final ConditionSymptomRepository conditionSymptomRepo;
+    private final ConditionSymptomAuditRepository conditionSymptomAuditRepo;
 
 
     /**
-     * @param patientSymptomRepo      autowired PatientSymptomRepository
+     * @param conditionSymptomRepo      autowired PatientSymptomRepository
      */
-    public PatientSymptomController(PatientSymptomRepository patientSymptomRepo) {
-        this.patientSymptomRepo = patientSymptomRepo;
+    public PatientSymptomController(ConditionSymptomRepository conditionSymptomRepo,
+                                    ConditionSymptomAuditRepository conditionSymptomAuditRepo) {
+        this.conditionSymptomRepo = conditionSymptomRepo;
+        this.conditionSymptomAuditRepo = conditionSymptomAuditRepo;
     }
 
 
     @Transactional
-    public List<ConditionSymptom> getOrCreateSymptoms(PatientConditionMessage msg)
+    public void processMessage(PatientConditionMessage msg, PatientCondition condition, Instant storedFrom)
             throws EmapOperationMessageProcessingException {
 
-        if (msg.getClass() == PatientAllergy.class){
-            return getOrCreateAllergyReactions((PatientAllergy) msg);
+        if (msg instanceof PatientAllergy){
+            processAllergyMessage((PatientAllergy) msg, condition, storedFrom);
         }
 
-        throw new RequiredDataMissingException("Could not process a "+msg.getClass()+" message. Not a supported type");
+        // No other conditions have associated symptoms
     }
 
 
-    /**
-     * Create symptoms for an allergic reaction
-     * @param msg          Allergy message
-     */
-    private List<ConditionSymptom> getOrCreateAllergyReactions(PatientAllergy msg){
-
-        List<ConditionSymptom> reactions = new ArrayList<>();
+    private void processAllergyMessage(PatientAllergy msg, PatientCondition condition, Instant storedFrom){
 
         for (String reactionName : msg.getReactions()){
 
-            Optional<ConditionSymptom> symptom = patientSymptomRepo.findByName(reactionName);
+            RowState<ConditionSymptom, ConditionSymptomAudit> symptomState = getOrCreateConditionSymptomState(msg,
+                    reactionName, condition, storedFrom);
 
-            if (symptom.isPresent() && reactions.contains(symptom.get())){
-                logger.debug("Reaction already present in the list of reactions. Not including");
+            if (!symptomShouldBeUpdated(msg, symptomState)) {
                 continue;
             }
 
-            reactions.add(symptom
-                    .orElseGet(() -> new ConditionSymptom(reactionName)));
+            symptomState.saveEntityOrAuditLogIfRequired(conditionSymptomRepo, conditionSymptomAuditRepo);
         }
+    }
 
-        return reactions;
+
+    private RowState<ConditionSymptom, ConditionSymptomAudit> getOrCreateConditionSymptomState(
+            PatientConditionMessage msg, String symptomName, PatientCondition condition, Instant storedFrom){
+
+        Optional<ConditionSymptom> possibleSymptom = conditionSymptomRepo.findByNameAndPatientConditionId(symptomName,
+                condition);
+        Instant updatedTime = msg.getUpdatedDateTime();
+
+        return possibleSymptom
+                .map(obs -> new RowState<>(obs, updatedTime, storedFrom, false))
+                .orElseGet(() -> createMinimalConditionSymptom(symptomName, condition, updatedTime, storedFrom));
+    }
+
+
+    private boolean symptomShouldBeUpdated(PatientConditionMessage msg,
+                                           RowState<ConditionSymptom, ConditionSymptomAudit> symptom) {
+        return symptom.isEntityCreated() || !msg.getUpdatedDateTime().isBefore(symptom.getEntity().getPatientConditionId().getValidFrom());
+    }
+
+
+    private RowState<ConditionSymptom, ConditionSymptomAudit> createMinimalConditionSymptom(String symptomName,
+                            PatientCondition condition,Instant validFrom, Instant storedFrom){
+
+        ConditionSymptom symptom = new ConditionSymptom(symptomName, condition);
+        return new RowState<>(symptom, validFrom, storedFrom, true);
     }
 
 }
