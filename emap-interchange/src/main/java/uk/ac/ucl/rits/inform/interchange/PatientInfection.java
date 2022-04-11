ce5e82cfee63d283ae3c8634945a00ec9f4c18d7
tom <tom.young@chem.ox.ac.uk>
2022-04-11 12:23:39 +0100

    Change from abstract class to interface

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientInfection.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientInfection.java
@@ -1,6 +1,9 @@
 package uk.ac.ucl.rits.inform.interchange;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -9,60 +12,85 @@ import java.time.LocalDate;
 /**
  * Interchange format of a PatientInterchange message.
  * @author Stef Piatek
+ * @author Tom Young
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
-public class PatientInfection extends PatientConditionMessage implements Serializable {
+public class PatientInfection extends EmapOperationMessage implements Serializable, PatientConditionMessage {
+
+    /**
+     * Unique identifier of the patient in the hospital.
+     */
+    private String mrn;
+
+    /**
+     * Number of the hospital visit.
+     */
+    private InterchangeValue<String> visitNumber = InterchangeValue.unknown();
+
+    /**
+     * Infection abbreviation.
+     */
+    private String conditionCode;
+
+    /**
+     * Human-readable infection name.
+     */
+    private InterchangeValue<String> conditionName = InterchangeValue.unknown();
+
+    /**
+     * Time of the update or message carrying this information.
+     */
+    private Instant updatedDateTime;
+
+    /**
+     * Unique Id for a condition in EPIC.
+     * If we can't get this added to the live HL7 interface when we should remove it.
+     */
+    private InterchangeValue<Long> epicConditionId = InterchangeValue.unknown();
+
+    /**
+     * Status of infection.
+     */
+    private String status;
+
+    /**
+     * Comment on an infection.
+     */
+    private InterchangeValue<String> comment = InterchangeValue.unknown();
+
+    /**
+     * Infection added at...
+     */
+    private Instant addedTime;
+
+    /**
+     * Infection resolved at...
+     */
+    private InterchangeValue<Instant> resolvedTime = InterchangeValue.unknown();
+
+    /**
+     * Onset of infection known at...
+     */
+    private InterchangeValue<LocalDate> onsetTime = InterchangeValue.unknown();
+
+    /**
+     * Effectively message type, i.e. whether to add, update or delete the condition.
+     */
+    private String action = "AD";
 
     /**
      * Call back to the processor so it knows what type this object is (ie. double dispatch).
      * @param processor the processor to call back to
      * @throws EmapOperationMessageProcessingException if message cannot be processed
      */
-    @Override
     public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
         processor.processMessage(this);
     }
 
-    // Below getters and setters are to maintain backwards compatibility
-    public String getInfectionCode() {
-        return getConditionCode();
-    }
-    public void setInfectionCode(String infectionCode) {
-        setConditionCode(infectionCode);
-    }
-
-    public InterchangeValue<String> getInfectionName() {
-        return getConditionName();
-    }
-    public void setInfectionName(InterchangeValue<String> infectionName) {
-        setConditionName(infectionName);
-    }
-
-    public InterchangeValue<Long> getEpicInfectionId() {
-        return getEpicConditionId();
-    }
-    public void setEpicInfectionId(InterchangeValue<Long> epicInfectionId) {
-        setEpicConditionId(epicInfectionId);
-    }
-
-    public InterchangeValue<Instant> getInfectionResolved() {
-        return getResolvedTime();
-    }
-    public void setInfectionResolved(InterchangeValue<Instant> resolvedTime) {
-        setResolvedTime(resolvedTime);
-    }
-
-    public Instant getInfectionAdded() {
-        return getAddedTime();
-    }
-    public void setInfectionAdded(Instant addedTime) {
-        setAddedTime(addedTime);
-    }
-
-    public InterchangeValue<LocalDate> getInfectionOnset() {
-        return getOnsetTime();
-    }
-    public void setInfectionOnset(InterchangeValue<LocalDate> date) {
-        setOnsetTime(date);
+    public boolean statusIsActive() {
+        return getStatus().equals("ACTIVE");
     }
 }
--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientProblem.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientProblem.java
@@ -2,6 +2,9 @@ package uk.ac.ucl.rits.inform.interchange;
 
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -13,61 +16,86 @@ import java.time.LocalDate;
  * PatientProblems are similar to PatientInfections in that they have a start date from which they have been diagnosed
  * and they can change (be updated or deleted) over time.
  * @author Anika Cawthorn
+ * @author Tom Young
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
-public class PatientProblem extends PatientConditionMessage implements Serializable {
+public class PatientProblem extends EmapOperationMessage implements Serializable, PatientConditionMessage {
 
     /**
+     * Unique identifier of the patient in the hospital.
+     */
+    private String mrn;
 
+    /**
+     * Number of the hospital visit.
+     */
+    private InterchangeValue<String> visitNumber = InterchangeValue.unknown();
+
+    /**
+     * Problem abbreviation.
+     */
+    private String conditionCode;
+
+    /**
+     * Human-readable condition name.
+     */
+    private InterchangeValue<String> conditionName = InterchangeValue.unknown();
+
+    /**
+     * Time of the update or message carrying this information.
+     */
+    private Instant updatedDateTime;
+
+    /**
+     * Unique Id for a condition in EPIC.
+     * If we can't get this added to the live HL7 interface when we should remove it.
+     */
+    private InterchangeValue<Long> epicConditionId = InterchangeValue.unknown();
+
+    /**
+     * Status of problem.
+     */
+    private String status;
+
+    /**
+     * Comment on a problem.
+     */
+    private InterchangeValue<String> comment = InterchangeValue.unknown();
+
+    /**
+     * Problem added at...
+     */
+    private Instant addedTime;
+
+    /**
+     * Problem resolved at...
+     */
+    private InterchangeValue<Instant> resolvedTime = InterchangeValue.unknown();
+
+    /**
+     * Onset of problem known at...
+     */
+    private InterchangeValue<LocalDate> onsetTime = InterchangeValue.unknown();
+
+    /**
+     * Effectively message type, i.e. whether to add, update or delete the condition.
+     */
+    private String action = "AD";
+
+    /**
      * Call back to the processor, so it knows what type this object is (i.e. double dispatch).
      * @param processor the processor to call back to
      * @throws EmapOperationMessageProcessingException if message cannot be processed
      */
-    @Override
     public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
         processor.processMessage(this);
     }
 
-    // Below getters and setters are to maintain backwards compatibility
-    public String getProblemCode() {
-        return getConditionCode();
-    }
-    public void setProblemCode(String problemCode) {
-        setConditionCode(problemCode);
+    public boolean statusIsActive() {
+        return getStatus().equals("ACTIVE");
     }
 
-    public InterchangeValue<String> getProblemName() {
-        return getConditionName();
-    }
-    public void setProblemName(InterchangeValue<String> problemName) {
-        setConditionName(problemName);
-    }
-
-    public InterchangeValue<Long> getEpicProblemId() {
-        return getEpicConditionId();
-    }
-    public void setEpicProblemId(InterchangeValue<Long> epicProblemId) {
-        setEpicConditionId(epicProblemId);
-    }
-
-    public InterchangeValue<Instant> getProblemResolved() {
-        return getResolvedTime();
-    }
-    public void setProblemResolved(InterchangeValue<Instant> resolvedTime) {
-        setResolvedTime(resolvedTime);
-    }
-
-    public Instant getProblemAdded() {
-        return getAddedTime();
-    }
-    public void setProblemAdded(Instant addedTime) {
-        setAddedTime(addedTime);
-    }
-
-    public InterchangeValue<LocalDate> getProblemOnset() {
-        return getOnsetTime();
-    }
-    public void setProblemOnset(InterchangeValue<LocalDate> date) {
-        setOnsetTime(date);
-    }
 }
