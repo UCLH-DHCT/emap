c1bc80aba566109ba13df5e9631d4193680d131d
tom <tom.young@chem.ox.ac.uk>
2022-04-20 11:46:57 +0100

    Start docker running

---
+++ emap-setup/emap_setup/docker/glowroot_setup.py
@@ -0,0 +1,9 @@
+
+
+
+"""
+0. Inject correct env file to DatabaseFiller/docker-compose
+1. Run casandra
+
+Emap-Core/emap.sh run glowroot-central java -jar "glowroot-central.jar" setup-admin-user "${GLOWROOT_USERNAME}" "${GLOWROOT_PASSWORD}"
+"""
