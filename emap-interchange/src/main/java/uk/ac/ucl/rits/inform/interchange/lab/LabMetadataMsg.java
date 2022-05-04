ad1086f313b7593941f6f61fadbc21ce68680377
Jeremy Stein <j.stein@ucl.ac.uk>
2022-05-04 17:14:47 +0100

    Set the message metadata fields more consistently with other message types.

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/lab/LabMetadataMsg.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/lab/LabMetadataMsg.java
@@ -2,19 +2,29 @@ package uk.ac.ucl.rits.inform.interchange.lab;
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessage;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
 import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
+import uk.ac.ucl.rits.inform.interchange.OrderCodingSystem;
 
 import java.time.Instant;
 
 @Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 public class LabMetadataMsg extends EmapOperationMessage {
     /**
      * Short code for a test or a battery.
      */
     private String shortCode;
+
+    /**
+     * The coding system that this shortCode belongs to.
+     */
+    private OrderCodingSystem codingSystem;
 
     /**
      * Human readable description.
