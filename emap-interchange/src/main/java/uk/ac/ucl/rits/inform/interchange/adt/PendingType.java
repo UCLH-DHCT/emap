c4e71cdd6cf58dd14238165fe68378274e3f4a9d
Stef Piatek <s.piatek@ucl.ac.uk>
2022-05-05 14:11:27 +0100

    Document PendingType

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/PendingType.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/PendingType.java
@@ -1,7 +1,19 @@
 package uk.ac.ucl.rits.inform.interchange.adt;
 
+/**
+ * Type of pending transfer, to allow for a single table to track history of all pending changes.
+ */
 public enum PendingType {
+    /**
+     * Admit.
+     */
     ADMIT,
+    /**
+     * Transfer for ADT messages: A15 and A26 (cancellation).
+     */
     TRANSFER,
+    /**
+     * Discharge.
+     */
     DISCHARGE
 }
