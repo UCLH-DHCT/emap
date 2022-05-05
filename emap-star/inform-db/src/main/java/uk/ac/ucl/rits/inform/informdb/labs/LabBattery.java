3ce810465fb94a35612622fd5ee5bd59a75cd30a
Jeremy Stein <j.stein@ucl.ac.uk>
2022-05-05 16:50:11 +0100

    Remove description field as we are unlikely to ever be able to fill this

--- emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/labs/LabBattery.java
+++ emap-star/inform-db/src/main/java/uk/ac/ucl/rits/inform/informdb/labs/LabBattery.java
@@ -45,11 +45,6 @@ public class LabBattery extends TemporalCore<LabBattery, LabBatteryAudit> {
     private String batteryName;
 
     /**
-     * \brief Desription of this battery of tests.
-     */
-    private String description;
-
-    /**
      * \brief Source system of this batteryCode.
      *
      * What system this code belongs to. Examples could be WinPath, or Epic.
@@ -77,7 +72,6 @@ public class LabBattery extends TemporalCore<LabBattery, LabBatteryAudit> {
         super(other);
         this.batteryCode = other.batteryCode;
         this.batteryName = other.batteryName;
-        this.description = other.description;
         this.labProvider = other.labProvider;
     }
 
