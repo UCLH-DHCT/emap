d594484d7c4b3b9b1ba10d23b4bff5d463d2282d
Stef Piatek <s.piatek@ucl.ac.uk>
2022-05-06 17:03:42 +0100

    Test message flows

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/PlannedMovementRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/PlannedMovementRepository.java
@@ -2,12 +2,12 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import uk.ac.ucl.rits.inform.datasinks.emapstar.concurrent.Functorator;
 import uk.ac.ucl.rits.inform.informdb.identity.HospitalVisit;
 import uk.ac.ucl.rits.inform.informdb.movement.Location;
 import uk.ac.ucl.rits.inform.informdb.movement.PlannedMovement;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -19,21 +19,22 @@ public interface PlannedMovementRepository extends CrudRepository<PlannedMovemen
      * Try and find a matching planned movement from a pending adt request message.
      * <p>
      * Always find by the event type, planned location and hospital visit Id, then:
-     * - For in order messages: find the most recent message by the eventDatetime
+     * - For in order messages: find the most recent message by the eventDatetime which hasn't been cancelled
      * - For cancel message arriving before the original pending adt: find message which has a null eventDatetime and a cancel after current datetime
      * @param eventType       type of the planned movement
      * @param hospitalVisitId hospital visit associated with the movement
      * @param plannedLocation planned location for the movement
      * @param eventDatetime   the datetime that event was created
-     * @return Optional planned movement entity
+     * @return planned movement entities
      */
     @Query("from PlannedMovement "
             + "where eventType = :eventType and hospitalVisitId = :hospitalVisitId and locationId = :plannedLocation "
-            + "and (eventDatetime < :eventDatetime "
-            + "      or (eventDatetime is null and cancelledDatetime >= :eventDatetime))"
-            + "order by eventDatetime, cancelledDatetime"
+            + "and ((eventDatetime <= :eventDatetime and cancelledDatetime is null) "
+            + "      or (cancelledDatetime >= :eventDatetime and eventDatetime is null) "
+            + "     )"
+            + "order by eventDatetime, cancelledDatetime "
     )
-    Optional<PlannedMovement> findFirstThatMatches(
+    List<PlannedMovement> findMatchingMovements(
             String eventType, HospitalVisit hospitalVisitId, Location plannedLocation, Instant eventDatetime
     );
 
@@ -45,4 +46,12 @@ public interface PlannedMovementRepository extends CrudRepository<PlannedMovemen
      * @return optional planned movement
      */
     Optional<PlannedMovement> findByHospitalVisitIdEncounterAndLocationIdLocationString(String encounter, String locationString);
+
+    /**
+     * For testing.
+     * @param encounter      encounter string
+     * @param locationString location string
+     * @return list of planned movements
+     */
+    List<PlannedMovement> findAllByHospitalVisitIdEncounterAndLocationIdLocationString(String encounter, String locationString);
 }
--- core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/adt/TestPendingAdt.java
+++ core/src/test/java/uk/ac/ucl/rits/inform/datasinks/emapstar/adt/TestPendingAdt.java
@@ -2,8 +2,13 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.adt;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.MessageProcessingBase;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.CoreDemographicRepository;
@@ -11,17 +16,25 @@ import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.HospitalVisitRepository;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.MrnRepository;
 import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.PlannedMovementRepository;
 import uk.ac.ucl.rits.inform.informdb.movement.PlannedMovement;
+import uk.ac.ucl.rits.inform.interchange.InterchangeMessageFactory;
+import uk.ac.ucl.rits.inform.interchange.adt.AdtMessage;
 import uk.ac.ucl.rits.inform.interchange.adt.CancelPendingTransfer;
 import uk.ac.ucl.rits.inform.interchange.adt.PendingTransfer;
 import uk.ac.ucl.rits.inform.interchange.adt.PendingType;
 
 import java.io.IOException;
+import java.io.InvalidClassException;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TestPendingAdt extends MessageProcessingBase {
+    private final Logger logger = LoggerFactory.getLogger(TestPendingAdt.class);
     @Autowired
     private MrnRepository mrnRepository;
     @Autowired
@@ -114,5 +127,75 @@ class TestPendingAdt extends MessageProcessingBase {
         assertEquals(EVENT_TIME, plannedMovement.getEventDatetime());
         assertEquals(CANCEL_TIME, plannedMovement.getCancelledDatetime());
     }
+
+
+    @ParameterizedTest
+    @ArgumentsSource(TestMessageStreamProvider.class)
+    void testDuplicateRequestInOrder(Iterable<? extends AdtMessage> pendingMessages, Integer expectedCount) throws Exception {
+        for (var msg : pendingMessages) {
+            if (msg instanceof PendingTransfer) {
+                dbOps.processMessage((PendingTransfer) msg);
+            } else if (msg instanceof CancelPendingTransfer) {
+                dbOps.processMessage((CancelPendingTransfer) msg);
+            } else {
+                throw new InvalidClassException("Unrecognised class of message");
+            }
+        }
+        List<PlannedMovement> plannedMovements = plannedMovementRepository
+                .findAllByHospitalVisitIdEncounterAndLocationIdLocationString(VISIT_NUMBER, LOCATION_STRING);
+        assertEquals(expectedCount, plannedMovements.size());
+//        for (PlannedMovement pm : plannedMovements) {
+//            logger.trace("Testing planned movement has values set correctly");
+//            assertNotNull(pm.getCancelledDatetime(), "Cancelled datetime shouldn't be null");
+//        }
+    }
+
 }
 
+
+class TestMessageStreamProvider implements ArgumentsProvider {
+    private final InterchangeMessageFactory messageFactory = new InterchangeMessageFactory();
+
+    private void changeEventTime(PendingTransfer pendingTransfer, long hourChange) {
+        Instant laterTime = pendingTransfer.getEventOccurredDateTime().plus(hourChange, ChronoUnit.HOURS);
+        pendingTransfer.setEventOccurredDateTime(laterTime);
+    }
+
+    private void changeCancelTime(CancelPendingTransfer cancelPending, long hourChange) {
+        Instant laterTime = cancelPending.getCancelledDateTime().plus(hourChange, ChronoUnit.HOURS);
+        cancelPending.setEventOccurredDateTime(laterTime);
+        cancelPending.setCancelledDateTime(laterTime);
+    }
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws IOException {
+        PendingTransfer pendingTransferEarlier = messageFactory.getAdtMessage("pending/A15.yaml");
+        changeEventTime(pendingTransferEarlier, -1);
+        PendingTransfer pendingTransfer = messageFactory.getAdtMessage("pending/A15.yaml");
+        PendingTransfer pendingTransferLater = messageFactory.getAdtMessage("pending/A15.yaml");
+        changeEventTime(pendingTransferLater, 1);
+
+        CancelPendingTransfer cancelPendingEarlier = messageFactory.getAdtMessage("pending/A26.yaml");
+        changeCancelTime(cancelPendingEarlier, -1);
+        CancelPendingTransfer cancelPending = messageFactory.getAdtMessage("pending/A26.yaml");
+        CancelPendingTransfer cancelPendingLater = messageFactory.getAdtMessage("pending/A26.yaml");
+        changeCancelTime(cancelPendingLater, 1);
+
+        return Stream.of(
+                // simple case of create and cancel, should have single entity
+                Arguments.of(List.of(pendingTransfer, cancelPending), 1),
+                // request created and cancelled twice, in order
+                Arguments.of(List.of(pendingTransfer, cancelPending, pendingTransferLater, cancelPendingLater), 2),
+                // request created and cancelled twice, last pending out of order
+                Arguments.of(List.of(pendingTransfer, cancelPending, cancelPendingLater, pendingTransferLater), 2),
+                // request created and cancelled twice, cancels all received before the pending
+                Arguments.of(List.of(cancelPending, cancelPendingLater, pendingTransfer, pendingTransferLater), 2),
+                // request created twice, will only output once as we're not sure if it's an update
+                Arguments.of(List.of(pendingTransfer, pendingTransferLater), 1),
+                // request created twice, cancelled twice, second will only have cancel datetime set
+                Arguments.of(List.of(pendingTransfer, pendingTransferLater, cancelPending, cancelPendingLater), 2)
+        );
+    }
+
+    ;
+}
