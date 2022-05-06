d64e1b066934ded881bfac6246dce511d608b901
Stef Piatek <s.piatek@ucl.ac.uk>
2022-05-06 14:55:38 +0100

    Process Pending Transfer requests

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/PendingAdtController.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/controllers/PendingAdtController.java
@@ -1,7 +1,13 @@
 package uk.ac.ucl.rits.inform.datasinks.emapstar.controllers;
 
 import org.springframework.stereotype.Component;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.RowState;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.PlannedMovementAuditRepository;
+import uk.ac.ucl.rits.inform.datasinks.emapstar.repos.PlannedMovementRepository;
 import uk.ac.ucl.rits.inform.informdb.identity.HospitalVisit;
+import uk.ac.ucl.rits.inform.informdb.movement.Location;
+import uk.ac.ucl.rits.inform.informdb.movement.PlannedMovement;
+import uk.ac.ucl.rits.inform.informdb.movement.PlannedMovementAudit;
 import uk.ac.ucl.rits.inform.interchange.adt.CancelPendingTransfer;
 import uk.ac.ucl.rits.inform.interchange.adt.PendingTransfer;
 
@@ -16,10 +22,59 @@ import java.time.Instant;
  */
 @Component
 public class PendingAdtController {
+    private LocationController locationController;
+    private PlannedMovementRepository plannedMovementRepo;
+    private PlannedMovementAuditRepository plannedMovementAuditRepo;
+
+    /**
+     * @param locationController       To get the location entity for the planned move
+     * @param plannedMovementRepo      To update the Planned Movement entity
+     * @param plannedMovementAuditRepo To update the Audit log for Planned Movement
+     */
+    public PendingAdtController(LocationController locationController,
+                                PlannedMovementRepository plannedMovementRepo,
+                                PlannedMovementAuditRepository plannedMovementAuditRepo) {
+        this.locationController = locationController;
+        this.plannedMovementRepo = plannedMovementRepo;
+        this.plannedMovementAuditRepo = plannedMovementAuditRepo;
+    }
 
 
+    /**
+     * Process pending ADT requests.
+     * @param visit      associated visit
+     * @param msg        pending adt
+     * @param validFrom  time in the hospital when the message was created
+     * @param storedFrom time that emap core started processing the message
+     */
     public void processMsg(HospitalVisit visit, PendingTransfer msg, Instant validFrom, Instant storedFrom) {
-        return;
+        Location plannedLocation = null;
+        if (msg.getPendingLocation().isSave()) {
+            plannedLocation = locationController.getOrCreateLocation(msg.getPendingLocation().get());
+        }
+
+        var plannedState = getOrCreateFromPlannedMessage(
+                visit, plannedLocation, msg.getPendingEventType().toString(), msg.getEventOccurredDateTime(), validFrom, storedFrom
+        );
+        PlannedMovement plannedMovement = plannedState.getEntity();
+        // If we receive a cancelled message before the original request then add it in
+        if (!plannedState.isEntityCreated() && plannedMovement.getEventDatetime() == null) {
+            plannedState.assignIfDifferent(plannedMovement.getEventDatetime(), msg.getEventOccurredDateTime(), plannedMovement::setEventDatetime);
+        }
+
+        plannedState.saveEntityOrAuditLogIfRequired(plannedMovementRepo, plannedMovementAuditRepo);
+    }
+
+    private RowState<PlannedMovement, PlannedMovementAudit> getOrCreateFromPlannedMessage(
+            HospitalVisit visit, Location plannedLocation, String pendingEventType, Instant eventOccurredDateTime,
+            Instant validFrom, Instant storedFrom) {
+        return plannedMovementRepo.findFirstFromPlannedMovement(pendingEventType, visit, plannedLocation, eventOccurredDateTime)
+                .map(pm -> new RowState<>(pm, validFrom, storedFrom, false))
+                .orElseGet(() -> {
+                    PlannedMovement pm = new PlannedMovement(visit, plannedLocation, pendingEventType);
+                    pm.setEventDatetime(eventOccurredDateTime);
+                    return new RowState<>(pm, validFrom, storedFrom, true);
+                });
     }
 
     public void processMsg(HospitalVisit visit, CancelPendingTransfer msg, Instant validFrom, Instant storedFrom) {
