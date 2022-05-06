97bafd184f829054263ea47d5a07af11574592f5
tom <tom.young@chem.ox.ac.uk>
2022-05-06 15:37:12 +0100

    Lint

--- emap-setup/setup.py
+++ emap-setup/setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 setup(
     name="emap_runner",
     version="0.0.1",
-    packages=["emap_runner",
-              "emap_runner.setup",
-              "emap_runner.docker",
-              "emap_runner.validation"],
+    packages=[
+        "emap_runner",
+        "emap_runner.setup",
+        "emap_runner.docker",
+        "emap_runner.validation",
+    ],
     author="Sarah Keating, Tom Young",
     url="https://github.com/inform-health-informatics/emap-setup",
     entry_points={"console_scripts": ["emap = emap_runner.runner:main"]},
