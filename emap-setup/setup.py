d1ed67645017aeee50169baa628ef14a904f38fd
tom <tom.young@chem.ox.ac.uk>
2022-05-09 12:20:39 +0100

    Use find_packages

--- emap-setup/setup.py
+++ emap-setup/setup.py
@@ -1,15 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="emap_runner",
     version="0.0.1",
-    packages=[
-        "emap_runner",
-        "emap_runner.setup",
-        "emap_runner.docker",
-        "emap_runner.validation",
-    ],
+    packages=find_packages("."),
     author="Sarah Keating, Tom Young",
     url="https://github.com/inform-health-informatics/emap-setup",
     entry_points={"console_scripts": ["emap = emap_runner.runner:main"]},
