278e7b2167058f530e32bedda9e518bd207cccba
tom <tom.young@chem.ox.ac.uk>
2022-05-09 13:46:35 +0100

    Update documentation

--- emap-star/inform-db/src/test/java/uk/ac/ucl/rits/inform/informdb/TestForeignKeyNaming.java
+++ emap-star/inform-db/src/test/java/uk/ac/ucl/rits/inform/informdb/TestForeignKeyNaming.java
@@ -4,7 +4,6 @@ package uk.ac.ucl.rits.inform.informdb;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import javax.persistence.JoinColumn;
@@ -14,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Ensure that all entities use the convention that a foreign key to another entity is <entityClass>Id.
+ * @author Tom Young
  */
 class TestForeignKeyNaming extends DBBaseTest{
 
