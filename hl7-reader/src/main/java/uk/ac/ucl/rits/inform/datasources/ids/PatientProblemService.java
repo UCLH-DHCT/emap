511076b9c93787c9336fd97a14a096262d6b2e5d
Anika C <anika.cawthorn@gmail.com>
2022-05-23 11:12:24 +0100

    Adding back in Epic Condition ID.

--- hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/PatientProblemService.java
+++ hl7-reader/src/main/java/uk/ac/ucl/rits/inform/datasources/ids/PatientProblemService.java
@@ -88,6 +88,8 @@ public class PatientProblemService {
         patientProblem.setResolvedTime(InterchangeValue.buildFromHl7(problemResolved));
         String problemStatus = problemSegment.getPrb13_ProblemConfirmationStatus().getCwe1_Identifier().getValueOrEmpty();
         patientProblem.setStatus(ConditionStatus.findByHl7Value(problemStatus));
+        String problemId = problemSegment.getPrb4_ProblemInstanceID().getEntityIdentifier().getValueOrEmpty();
+        patientProblem.setEpicConditionId(Long.valueOf(problemId));
         DTM problemOnset = problemSegment.getPrb16_ProblemDateOfOnset();
         if (problemOnset.getValue() != null) {
             patientProblem.setOnsetTime(InterchangeValue.buildFromHl7(HL7Utils.interpretDate(problemOnset)));
