aed42fadd2112868e6dd30fa83f43bf7dee2facc
Anika C <anika.cawthorn@gmail.com>
2022-06-23 17:06:31 +0100

    Making changes to extract comments from Clarity too.

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientConditionMessage.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientConditionMessage.java
@@ -74,4 +74,9 @@ public abstract class PatientConditionMessage extends EmapOperationMessage {
      * Identifier for condition as provided in HL7 messages.
      */
     private InterchangeValue<Long> epicConditionId = InterchangeValue.unknown();
+
+    /**
+     * Comment for condition as provided in HL7 messages.
+     */
+    private InterchangeValue<String> comment = InterchangeValue.unknown();
 }
