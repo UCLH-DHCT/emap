91b3b3c72e65921adfcf5f1202f17829146088f4
Anika C <anika.cawthorn@gmail.com>
2022-06-29 17:20:56 +0100

    Moving added date/time and resolution date/time into respective problem list and infection to allow for different time resolution between both

--- emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientProblem.java
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/PatientProblem.java
@@ -7,12 +7,13 @@ import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 /**
  * Interchange format of a PatientProblem message. In hospital terminology they are referred to as problem lists.
  * <p>
  * PatientProblems are similar to PatientInfections in that they have a start date from which they have been diagnosed
- * and they can change (be updated or deleted) over time.
+ * and that they can change (be updated or deleted) over time.
  * @author Anika Cawthorn
  * @author Tom Young
  */
@@ -22,6 +23,15 @@ import java.io.Serializable;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "@class")
 public class PatientProblem extends PatientConditionMessage implements Serializable {
 
+    /**
+     * Time and date condition was added at.
+     */
+    private Instant addedDate;
+
+    /**
+     * Time and date condition was added at.
+     */
+    private Instant resolvedDate;
     /**
      * Call back to the processor, so it knows what type this object is (i.e. double dispatch).
      * @param processor the processor to call back to
