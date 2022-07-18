fe37505c64974c32451f918d6163266f0326b4c8
Stef Piatek <s.piatek@ucl.ac.uk>
2022-07-18 12:06:41 +0100

    Test planned movement deleted in cascade

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/PlannedMovementAuditRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/PlannedMovementAuditRepository.java
@@ -3,9 +3,16 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.movement.PlannedMovementAudit;
 
+import java.util.List;
+
 /**
  * Interaction with the PlannedMovementAudit table.
  * @author Stef Piatek
  */
 public interface PlannedMovementAuditRepository extends CrudRepository<PlannedMovementAudit, Long> {
+    /**
+     * For testing.
+     * @param hospitalVisitId id of the hospital visit
+     */
+    List<PlannedMovementAudit> findAllByHospitalVisitId(long hospitalVisitId);
 }
