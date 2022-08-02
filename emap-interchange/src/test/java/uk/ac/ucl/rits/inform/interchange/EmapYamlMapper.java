78b6ebc66f376e43357295280121dd35c4d0dede
Stef Piatek <s.piatek@ucl.ac.uk>
2022-08-02 08:50:33 +0100

    Assert interchange messages by their yaml string

--- emap-interchange/src/test/java/uk/ac/ucl/rits/inform/interchange/EmapYamlMapper.java
+++ emap-interchange/src/test/java/uk/ac/ucl/rits/inform/interchange/EmapYamlMapper.java
@@ -7,25 +7,31 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.extern.slf4j.Slf4j;
-import uk.ac.ucl.rits.inform.interchange.visit_observations.Flowsheet;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.regex.Pattern;
 
 @Slf4j
-public class InterchangeMapper {
+public class EmapYamlMapper {
     private static final Pattern SOURCE_ID_PATTERN = Pattern.compile("sourceMessageId:.*\n");
     private final ObjectMapper mapper;
 
-    public InterchangeMapper() {
+    /**
+     * Create an EMAP interchange Mapper object, configured to allow for serialising and deserialising YAML.
+     */
+    public EmapYamlMapper() {
         mapper = new ObjectMapper(new YAMLFactory());
         // Finds modules so instants can be parsed correctly
         mapper.findAndRegisterModules();
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
     }
 
-
+    /**
+     * Convert EMAP message to YAML string for easy comparison in an assert statement.
+     * @param message EMAP message to convert to a string
+     * @return YAML string of the message contents
+     */
     public String convertToString(EmapOperationMessage message) {
         try {
             String json = mapper.writeValueAsString(message);
