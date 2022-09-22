65a6e3bba694fe1198373017fe58fb0c43a1bafc
Jeremy Stein <j.stein@ucl.ac.uk>
2022-09-22 18:22:17 +0100

    Flesh out form processor test a lot. Fix metadata updating.

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/FormQuestionRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/FormQuestionRepository.java
@@ -3,8 +3,12 @@ package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.forms.FormQuestion;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface FormQuestionRepository extends CrudRepository<FormQuestion, Long> {
     Optional<FormQuestion> findByInternalId(String formQuestionSourceId);
+
+    List<FormQuestion> findAllByInternalIdIn(Set<String> formQuestionSourceIds);
 }
