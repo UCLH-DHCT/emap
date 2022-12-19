2f1621fce396de89480cb78b6213b86dbd9f3ad6
Stef Piatek <s.piatek@ucl.ac.uk>
2022-12-19 15:02:26 +0000

    Move form repos to separate package and document methods

--- core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/forms/FormAnswerAuditRepository.java
+++ core/src/main/java/uk/ac/ucl/rits/inform/datasinks/emapstar/repos/forms/FormAnswerAuditRepository.java
@@ -1,4 +1,4 @@
-package uk.ac.ucl.rits.inform.datasinks.emapstar.repos;
+package uk.ac.ucl.rits.inform.datasinks.emapstar.repos.forms;
 
 import org.springframework.data.repository.CrudRepository;
 import uk.ac.ucl.rits.inform.informdb.forms.FormAnswerAudit;
@@ -6,7 +6,17 @@ import uk.ac.ucl.rits.inform.informdb.forms.FormAnswerAudit;
 import java.util.List;
 
 public interface FormAnswerAuditRepository extends CrudRepository<FormAnswerAudit, Long> {
+    /**
+     * Used in testing only.
+     * @param id id of the form answer
+     * @return true if the form answer exsits.
+     */
     boolean existsByFormAnswerId(Long id);
 
+    /**
+     * Used in testing only.
+     * @param internalId internal ID to query by
+     * @return list of form answer audits
+     */
     List<FormAnswerAudit> findAllByInternalId(String internalId);
 }
