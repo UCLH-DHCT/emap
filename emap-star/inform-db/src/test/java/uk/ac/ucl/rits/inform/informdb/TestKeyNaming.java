90791dd9b4941d098e93b2f8638c6cb8d9752338
harveymannering <harveymannering@hotmail.com>
2023-02-20 11:49:26 +0000

    Added InformDB test to check that class and primary key names match

--- emap-star/inform-db/src/test/java/uk/ac/ucl/rits/inform/informdb/TestKeyNaming.java
+++ emap-star/inform-db/src/test/java/uk/ac/ucl/rits/inform/informdb/TestKeyNaming.java
@@ -7,8 +7,11 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import javax.persistence.JoinColumn;
+import javax.persistence.Id;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import static uk.ac.ucl.rits.inform.informdb.DBTestUtils.lowerCaseFirstCharacter;
 
@@ -17,10 +20,14 @@ import static uk.ac.ucl.rits.inform.informdb.DBTestUtils.lowerCaseFirstCharacter
  * Ensure that all entities use the convention that a foreign key to another entity is <entityClass>Id.
  * @author Tom Young
  */
-class TestForeignKeyNaming {
+class TestKeyNaming {
 
     boolean isAForeignKey(Field f) {
         return Arrays.stream(f.getDeclaredAnnotations()).anyMatch(a -> a.annotationType().equals(JoinColumn.class));
+    }
+
+    boolean isAPrimaryKey(Field f) {
+        return Arrays.stream(f.getDeclaredAnnotations()).anyMatch(a -> a.annotationType().equals(Id.class));
     }
 
     /**
@@ -41,6 +48,20 @@ class TestForeignKeyNaming {
         assertEquals(className + "Id", fieldName);
     }
 
+
+    /**
+     * Assert that a field has the same name as the class it belongs to, plus the "Id" suffix
+     * For example: A field in the ConditionType class should be called conditionTypeId.
+     * @param field  Class field
+     */
+    void assertHasClassNameAsPrefix(Field field) {
+
+        String declaringClass = field.getDeclaringClass().toString();
+        String fieldName = field.getName();
+        String capitalizedFieldName = fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+        assertTrue((declaringClass + "Id").endsWith(capitalizedFieldName));
+    }
+
     /**
      * Ensure that every foreign key has the correct naming convention.
      */
@@ -50,5 +71,13 @@ class TestForeignKeyNaming {
         Arrays.stream(entityClass.getDeclaredFields())
                 .filter(this::isAForeignKey)
                 .forEach(this::assertHasIdSuffix);
+    }
+
+    @ParameterizedTest
+    @MethodSource("uk.ac.ucl.rits.inform.informdb.DBTestUtils#findAllEntities")
+    void testPrimaryKeyNaming(Class<?> entityClass) {
+        Arrays.stream(entityClass.getDeclaredFields())
+                .filter(this::isAPrimaryKey)
+                .forEach(this::assertHasClassNameAsPrefix);
     }
 }
