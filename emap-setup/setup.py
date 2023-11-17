50b77ddeed015e7a4fcb9af2eb23b9c6e986037d
Stef Piatek <s.piatek@ucl.ac.uk>
2023-11-17 12:05:03 +0000

    Increment emap versions

--- emap-setup/setup.py
+++ emap-setup/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="emap_runner",
-    version="0.0.2",
+    version="0.1.0",
     packages=find_packages("."),
     author="Sarah Keating, Tom Young",
     url="https://github.com/inform-health-informatics/emap-setup",
