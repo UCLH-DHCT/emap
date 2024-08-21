ceafdae9fbbb60100ca130a6d352f09a14ac90a0
Jeremy Stein <j.stein@ucl.ac.uk>
2024-08-21 13:51:28 +0100

    Use the correct year in the date format!

--- waveform-generator/src/main/java/uk/ac/ucl/rits/inform/datasources/waveform_generator/Hl7Generator.java
+++ waveform-generator/src/main/java/uk/ac/ucl/rits/inform/datasources/waveform_generator/Hl7Generator.java
@@ -116,8 +116,7 @@ public class Hl7Generator {
                 OBX|${obxI}|${dataType}|${streamId}||${valuesAsStr}||||||F||20|${obsTimeStr}|\r\
                 """;
         ZoneId hospitalTimezone = ZoneId.of("Europe/London");
-        // XXX: shouldn't this be yyyy?
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYYMMddHHmmss.SSSZZ");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss.SSSZZ");
         // XXX: where to get message timestamp from? Just add a random bit maybe?
         // Will sampling rate have to be inferred at the other end? It's in the coding table I think...
         // message Id I think shouldn't be created here
